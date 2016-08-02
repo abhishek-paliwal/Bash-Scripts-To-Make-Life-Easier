3cca2022dbb5f211d3c7d9b703fadd79db7f75b4
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2016-08-02 16:20:57 +0300

    Files added. Image to HTML collage maker.

---
+++ _images2html-maker-with-names.sh
@@ -0,0 +1,102 @@
+#!/bin/bash;
+## THIS SCRIPT CREATES AN HTML FILE WITH...
+## ...ALL THE IMAGES IN WORKING DIRECTORY
+echo #Blank line
+pwd ##check the present working directory
+echo #Blank line
+
+echo '======== Current filename order ========'
+ls -1 *.{jpg,png,PNG,JPG} | nl ##sort output as 1 column
+
+echo '==================='
+echo `ls -1 *.{jpg,png,PNG,JPG} | head -1 | wc -m`
+echo "= Character length including file extension (jpg/JPG or png/PNG)"
+echo "So sort number to be used below could be this number minus 5 (or 6)."
+echo '==================='
+
+echo -n "Sort filenames at which character number (Enter '1' if you're not sure) [ENTER]:"
+read sortnumber
+
+echo '========== So this is the filename order that you want: ========='
+ls *.{jpg,png,PNG,JPG} | sort -n -k1.$sortnumber | nl
+
+echo -n "[REQUIRED FIELD] Enter the title for the HTML page; Spaces will be converted to hyphens; [ENTER]: "
+read titlename
+
+#ASSIGN FILENAME. AND NO SPACES BETWEEN VARNAME and '=' SIGN; else Bash throws error.
+totalfiles="`ls -l *.{jpg,png,PNG,JPG} | wc -l | tr -d '[[:space:]]'`"
+totalfiles+=" Images"
+echo "$totalfiles"
+echo "$titlename-$totalfiles.html" ##this output shows leading spaces, donno why. So SED is used below.
+
+filenamex=`echo "$titlename-$totalfiles.html" | sed -e 's/ /-/g'`
+
+echo -n "Enter the size of the DIV to use (300 works best) [ENTER]: "
+read imagesize
+imagesize+="px"; #Concatenating px at the end of the number
+
+echo "<html><head><title>$filenamex</title>" > $filenamex
+
+echo "<link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet'>
+<style>
+div {
+    float:left;
+    width: $imagesize;
+    /* height: $imagesize; */
+    padding: 5px;
+    margin: 5px;
+    color: black;
+    font-family: 'Raleway', sans-serif;
+    text-align: center;
+    font-size: 15px;
+    line-height:1;
+    /* text-transform: lowercase; */
+}
+
+.colored {
+  background-color:#00FF00;
+  color:white;
+  padding: 20px;
+  font-weight:700;
+}
+
+h1 {
+  font-family: 'Raleway', sans-serif;
+	text-align: center;
+	font-size: 20px;
+  font-weight:700;
+	line-height:1;
+	text-transform: capitalize;
+}
+
+hr {clear:both;}
+</style>" >> $filenamex
+
+echo "</head><body>" >> $filenamex
+echo "<h1>$titlename<br>(Total Image Files= $totalfiles)</h1>" >> $filenamex
+
+for x in `ls *.{jpg,png,PNG,JPG} | sort -n -k1.$sortnumber`
+	do
+		echo "<div><a href='$x'><img src='$x' width='100%' align='top'></img><br><br>$x</a></div>" >> $filenamex
+	done;
+
+
+#### OPTIONAL SECTION: Findind and listing Photoshop files ####
+totalcustomfiles="`find . -type f | egrep -i '\.(psd|ai|svg|eps)$' | wc -l | tr -d '[[:space:]]'`"
+
+echo "<hr><br><h1>DOWNLOADS: Total PSD, AI, SVG, EPS files = $totalcustomfiles)</h1>" >> $filenamex
+
+for y in `find . -type f | egrep -i '\.(psd|ai|svg|eps)$'`
+  do
+  	echo "<div class='colored'><a href='$y'>DOWNLOAD<br>$y</a></div>" >> $filenamex
+
+  done;
+
+##### OPTIONAL SECTION ENDS #####
+echo "<hr>" >> $filenamex
+
+echo "</body></html>" >> $filenamex
+
+#Open the newly created file in browser
+echo "DONE! File will now be opened in FIREFOX."
+open -a firefox $filenamex
---
+++ _images2html-maker.sh
@@ -0,0 +1,48 @@
+#!/bin/bash;
+## THIS SCRIPT CREATES AN HTML FILE WITH...
+## ...ALL THE IMAGES IN WORKING DIRECTORY
+echo #Blank line
+pwd ##check the present working directory
+echo #Blank line
+
+echo '======== Current filename order ========'
+ls -1 *.{jpg,png,PNG,JPG} | nl ##sort output as 1 column
+
+echo '==================='
+echo `ls -1 *.{jpg,png,PNG,JPG} | head -1 | wc -m`
+echo "= Character length including file extension (jpg/JPG or png/PNG)"
+echo "So sort number to be used below could be this number minus 5 (or 6)."
+echo '==================='
+
+echo -n "Sort filenames at which character number (Enter '1' if you're not sure) [ENTER]:"
+read sortnumber
+
+echo '========== So this is the filename order that you want: ========='
+ls *.{jpg,png,PNG,JPG} | sort -n -k1.$sortnumber | nl
+
+echo -n "[REQUIRED FIELD] Enter the title for the HTML page; Spaces will be converted to hyphens; [ENTER]: "
+read titlename
+
+#ASSIGN FILENAME. AND NO SPACES BETWEEN VARNAME and '=' SIGN; else Bash throws error.
+totalfiles="`ls -l *.{jpg,png,PNG,JPG} | wc -l | tr -d '[[:space:]]'`"
+totalfiles+=" Images"
+echo "$totalfiles"
+echo "$titlename-$totalfiles.html" ##this output shows leading spaces, donno why. So SED is used below.
+
+filenamex=`echo "$titlename-$totalfiles.html" | sed -e 's/ /-/g'` 
+
+echo -n "Enter the % size of images to use (99 works best) [ENTER]: "
+read imagesize
+
+echo "<html><head><title>$filenamex</title></head><body>" > $filenamex
+
+for x in `ls *.{jpg,png,PNG,JPG} | sort -n -k1.$sortnumber`
+	do
+		echo "<img src='$x' width='$imagesize%' align='top'></img>" >> $filenamex
+	done;
+
+echo "</body></html>" >> $filenamex
+
+#Open the newly created file in browser
+echo "DONE! File will now be opened in FIREFOX."
+open -a firefox $filenamex
