49c1359f68f15191ecd4baf967e93a49728253ba
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2017-03-15 20:25:01 +0200

    File added.

---
+++ 11-sitemap-for-website-generator.sh
@@ -0,0 +1,27 @@
+#!bin/bash
+# This script creates sitemaps in directories specified as function arguments
+# by passing arguments to a function
+create_sitemap_in_this_directory () {
+  cd $1 ;
+  # sitemap creation begins
+  echo '++++++++++++++++++++++++++++++++++++++++++++++++++'
+  echo '===>>>>>>>> BEGINNING : Sitemap Creation at '$1
+  echo "<h1>SITEMAP</h1>" > sitemap.html
+
+  for y in `ls -c $1` ;  #Only lists diretories and files in root folder
+    do
+        echo "<div style='display : block ; padding : 5px ; '>&bull; <a href='$y'>$y</a></div>" ;
+    done >> sitemap.html
+
+  echo "<hr><h3>This sitemap is automatically created on :"`date`"</h3>" >> sitemap.html
+
+  echo '===>>>>>>>> DONE : Sitemap Creation at '$1
+
+  ### Opening directory
+  echo 'Opening directory...'
+  open $1 # Works only on Mac
+}
+
+## Actual magic by calling the fuctions with required directories as arguments
+create_sitemap_in_this_directory $HOME/Dropbox/Public/_TO_SYNC_cdn.mygingergarlickitchen.com
+create_sitemap_in_this_directory $HOME/Dropbox/Public/_TO_SYNC_downloads.concepro.com
