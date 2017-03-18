3bb582120b00203c554e8bbcc048449a8ab9f390
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2017-03-18 17:04:29 +0200

    SiteName added in the title.

--- 11-sitemap-for-website-generator.sh
+++ 11-sitemap-for-website-generator.sh
@@ -3,17 +3,18 @@
 # by passing arguments to a function
 create_sitemap_in_this_directory () {
   cd $1 ;
+  siteName=`basename $1`
   # sitemap creation begins
   echo '++++++++++++++++++++++++++++++++++++++++++++++++++'
   echo '===>>>>>>>> BEGINNING : Sitemap Creation at '$1
-  echo "<h1>SITEMAP</h1>" > sitemap.html
+  echo "<h1>SITEMAP for $siteName</h1>" > sitemap.html
 
   for y in `ls -c $1` ;  #Only lists diretories and files in root folder
     do
         echo "<div style='display : block ; padding : 5px ; '>&bull; <a href='$y'>$y</a></div>" ;
     done >> sitemap.html
 
-  echo "<hr><h3>This sitemap is automatically created on :"`date`"</h3>" >> sitemap.html
+  echo "<hr><h3>This sitemap is automatically created on : "`date`"</h3>" >> sitemap.html
 
   echo '===>>>>>>>> DONE : Sitemap Creation at '$1
 
