1a7ce786cdbc2fd632c765040efdab21cf63df73
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2017-04-03 04:17:45 +0300

    File renamed.

--- 10B_create_SOCIAL_MEDIA_image_indexes.sh
+++ 10B_create_SOCIAL_MEDIA_image_indexes.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-## THIS SCRIPT CREATES AN HTML FILE WITH...
+## BOOTSTRAP FRAMEWORK : THIS SCRIPT CREATES AN HTML FILE WITH...
 ## ...ALL THE IMAGES IN WORKING DIRECTORY + RECUSRSIVELY
 echo "#################" #Blank line
 ROOT="$HOME/Dropbox/Public/_TO_SYNC_downloads.concepro.com/_BRAND-CONCEPRO/concepro-08-social/sharing"
 echo "CURRENT WORKING DIRECTORY: " $ROOT ##check the present working directory
 echo "#################" #Blank line
 
-FINALFILENAME="Index-of-SocialMedia-files.html"
+FINALFILENAME="Index-of-SocialMedia-files-BS.html"
 OUTPUT="$ROOT/$FINALFILENAME" ##Output filename
 SITEURL="https://downloads.concepro.com/_BRAND-CONCEPRO/concepro-08-social/sharing/$FINALFILENAME"
 
@@ -15,76 +15,54 @@ SITEURL="https://downloads.concepro.com/_BRAND-CONCEPRO/concepro-08-social/shari
 # read imagesize
 # imagesize+="px"; #Concatenating px at the end of the number
 
-echo "<html><head><title>Library of Social Media Images // Concepro Agency</title>" > $OUTPUT
+echo "<!DOCTYPE html>" > $OUTPUT ## FIRST OUTPUT LINE TO HTML FILE
 
-echo "<link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900' rel='stylesheet'>
+echo "<html lang='en'><head><meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
 
-<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+    <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+    <link rel='icon' href='https://v4-alpha.getbootstrap.com/favicon.ico'>
 
-<!-- LAZY-LOADER OF IMAGES -  JQUERY Plugin -->
-<script type='text/javascript' src='https://abhishek-paliwal.github.io/wallpaper_creators/js/JQUERY.LAZY.MIN.js'></script>
+    <title>Library of Social Media Images // Concepro Agency</title>
 
-<script>
+    <!-- Bootstrap core CSS -->
+    <link href='https://abhishek-paliwal.github.io/wallpaper_creators/bootstrap-alpha6-js-css/bootstrap.min.css' rel='stylesheet'>
 
-  \$(document).ready(function(){
-        // ENABLING LAZY LOADING
-        \$('.lazy').Lazy();
-  });
+    <!-- Custom styles for this template -->
+    <link href='https://abhishek-paliwal.github.io/wallpaper_creators/bootstrap-alpha6-js-css/starter-template.css' rel='stylesheet'>
 
-</script>
 
 <style>
-div {
-    float:left;
-    /* width: $imagesize; */
-    /* height: $imagesize; */
-    width: 200px;
+
+body {
+  background-color: rgb(245,245,245) ;
+}
+
+.pali {
+    height: 100%;
     padding: 5px;
-    margin: 5px;
-    color: black;
-    font-family: 'Roboto', sans-serif;
-    text-align: center;
-    font-size: 15px;
-    line-height:1.5;
-    /* text-transform: lowercase; */
-    background-color: rgb(245,245,245) ;
-    border : 2px solid white ;
+    margin-bottom : 20px;
+    text-align: left;
+    background-color: white ;
+    border : 1px solid white ;
 		box-shadow: 0px 0px 5px #999 ;
-		border-radius : 2px ;
+		border-radius : 1px ;
+    color : #999 ;
 }
 
 div.heading {
     display: block ;
-    padding: 0px;
+    width: 100% ;
+    padding: 20px;
     margin: 0px;
     margin-bottom : 20px ;
-    clear: both ;  /* Clearing floats if any */
-    width: 100% ;
     height: auto;
+    text-align: center;
+    color : white;
     /* Concepro Colored Background Gradient */
-    background : linear-gradient( 45deg, #007fff, #ff8700 ) ;
+    background : linear-gradient( 185deg, #007fff, #ff8700 ) ;
     border : 0px solid white ;
 		box-shadow: 0px 0px 0px #999 ;
 		border-radius : 0px ;
-}
-
-.colored {
-  background-color:#00FFFF;
-  color:white;
-  padding: 20px;
-  font-weight:700;
-}
-
-h1 {
-  display : block ;
-  padding : 10px ;
-  color : white ;
-  font-family: 'Roboto', sans-serif;
-	text-align: center;
-	font-size: 30px;
-  font-weight: 100;
-	line-height: 1.3;
-	/* text-transform: uppercase ; */
 }
 
 h1.heading {
@@ -95,25 +73,13 @@ h1.heading {
    text-transform: uppercase ;
 }
 
-h2 {
-  font-family: 'Roboto', sans-serif;
-  text-align: center;
-  color: black ;
-  font-weight: 700;
-  font-size: 22px;
-}
-
 h2.heading {
   font-weight: 100;
   font-size: 40px;
 }
 
-h3 {
-  font-family: 'Roboto', sans-serif;
-  text-align: center;
-  color: #999 ;
-  font-weight: 900;
-  font-size: 16px;
+h1,h2,h3,h4,h5,h6 {
+  text-align : center;
 }
 
 hr {
@@ -124,7 +90,20 @@ hr {
 }
 
 .thin {
-  font-weight : 300 ;
+  font-size: 0.8em ;
+}
+
+.toc {
+  text-align: center ;
+  font-size: 1.2em ;
+  font-weight: 900 ;
+}
+
+.theEndFooter {
+  padding: 10px ;
+  background-color: #444;
+  color: white;
+  text-align: center ;
 }
 
 .p1 {background : linear-gradient( 45deg, #3498db, #ff8700 ) ; color: white ; text-transform: uppercase ; padding : 10px ; }
@@ -132,33 +111,36 @@ hr {
 hr {clear:both;}
 </style>" >> $OUTPUT
 
-echo "</head><body style='background-color: rgb(230,230,230) ;'>" >> $OUTPUT
+echo "</head><body>" >> $OUTPUT
+
+echo "<nav class='navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top'>
+  <a class='navbar-brand' href='http://www.concepro.com'><span style='font-weight: 700; color: #3498db;'>concepro</span> | a digital marketing agency</a>
+</nav>
+
+<div class='container' style='width: 100% ;'>" >> $OUTPUT
 
 #### OPTIONAL SECTION: Finding and listing all Image files Recursively. ####
 totalimagefiles="`find $ROOT -type f | egrep -i '\.(jpg|png|PNG|JPG|gif|GIF)$' | wc -l | tr -d '[[:space:]]'`"
 
-echo "<!-- HEADING DIV STARTS --> <div class='heading'>"  >> $OUTPUT
+echo "<div class='container' style='width: 100% ;'>" >> $OUTPUT
 
-echo "<!-- TOP LOGO --> <img src='http://downloads.concepro.com/dropbox-public-files/logos/4-logos-concepro/Concepro-Shadow.png' style='width: 300px; '>" >> $OUTPUT
+echo "<!-- HEADING DIV STARTS --> <div class='heading'>
+<!-- TOP LOGO --> <img src='http://downloads.concepro.com/dropbox-public-files/logos/4-logos-concepro/Concepro-Shadow.png' style='width: 300px; '>
+<h1 class='heading'>Full Library Of All Social Media Images</h1>
 
-echo "<h1 class='heading'>Full Library Of All Social Media Images</h1>" >> $OUTPUT
+<h2 class='heading'>Designed by <a href='http://www.AbhishekPaliwal.com'>Abhishek Paliwal</a> for <a href='http://www.concepro.com'>Concepro Digital Marketing Agency</a></h2>
+<!-- HEADING DIV ENDS --> </div>"  >> $OUTPUT
 
-echo "<hr>" >> $OUTPUT
-
-echo "<h2 class='heading'>Designed by <a href='http://www.AbhishekPaliwal.com'>Abhishek Paliwal</a> for <a href='http://www.concepro.com'>Concepro Digital Marketing Agency</a></h2>" >> $OUTPUT
-
-echo "<!-- HEADING DIV ENDS --> </div>"  >> $OUTPUT
-
-echo "<h2 class='thin'>Total Images On This Page (Recursive) = $totalimagefiles</h2>" >> $OUTPUT
+echo "<h3 class='thin'>Total Images On This Page (Recursive) = $totalimagefiles</h3>" >> $OUTPUT
 echo "<h3 class='thin'>Page last updated: "`date`"</h3>" >> $OUTPUT
 
 ##### CREATING THE HREF-NAME LINKS FOR ALL THE FOLDERS #####
 echo "<hr>" >> $OUTPUT
-echo "<h2 style='padding: 10px; border: 2px solid #000 ;'>&bull; Table of Contents &bull;</h2>" >> $OUTPUT
+echo "<div class='toc'>&bull; Table of Contents &bull;</div>" >> $OUTPUT
 
 for foldername in `find "$ROOT" -maxdepth 1 -mindepth 1 -type d| sort`; do
   folder=`basename "$foldername"`
-  echo "<h2 style='text-transform: uppercase; letter-spacing: 2px;'><a href='#$folder'> $folder</a><h2> " >> $OUTPUT
+  echo "<div class='toc'><a href='#$folder'> $folder</a></div> " >> $OUTPUT
 done
 #### Link Creation Ends #####
 
@@ -167,6 +149,8 @@ x=0
 for filepath in `find "$ROOT" -maxdepth 1 -mindepth 1 -type d| sort`; do
   path=`basename "$filepath"`
   echo " <hr> <h2 class='p1'> <a name='$path'>$path</a> <a href='#'>( &uarr; Back to top )</a></h2>" >> $OUTPUT
+
+  echo "<div class='row'> <!-- ROW BEGINS --> " >> $OUTPUT
 
   for x in `find "$filepath" -type f| sort | egrep -i '\.(jpg|png|PNG|JPG|gif|GIF)$'`; do
     file=`basename "$x"`
@@ -181,20 +165,47 @@ for filepath in `find "$ROOT" -maxdepth 1 -mindepth 1 -type d| sort`; do
 
 ## Printing the image dimensions for everything, except GIFs because they produce LOOOOONG outputs for all GIF frames. ##
     if [ "$filetype" != 'GIF' ]; then
-      echo "<div><a href='$path/$file'><img class='lazy' data-src='$path/$file' width='100%' align='top'></img><br><br><span class='thin'>$file</a></span><br>$imagedimen</div>" >> $OUTPUT
+      echo "<div class='col-md-4 col-lg-2 col-sm-6'>" >> $OUTPUT
+      echo "<div class='pali'><a href='$path/$file'><img class='lazy' data-src='$path/$file' width='100%'></img><span class='thin'>$file</a></span><br><span class='thin'>$imagedimen</span></div>" >> $OUTPUT
+      echo "</div>" >> $OUTPUT
     else
-      echo "<div><a href='$path/$file'><img class='lazy' data-src='$path/$file' width='100%' align='top'></img><br><br><span class='thin'>$file</span></a></div>" >> $OUTPUT
+      echo "<div class='col-md-4 col-lg-2 col-sm-6'>" >> $OUTPUT
+      echo "<div class='pali'><a href='$path/$file'><img class='lazy' data-src='$path/$file' width='100%'></img><span class='thin'>$file</span></a></div>" >> $OUTPUT
+      echo "</div>" >> $OUTPUT
     fi
 
   done
+
+  echo "</div> <!-- ROW DIV ENDS --> " >> $OUTPUT
+
 done
 
 #### Calculations end ####
-echo "<hr>" >> $OUTPUT
+echo "<footer class='theEndFooter'><p>&copy; Concepro Digital Marketing Agency 2017</p></footer>" >> $OUTPUT
+
+echo "</div><!-- Container DIV ENDS -->" >> $OUTPUT
+
+echo "<!-- Bootstrap core JavaScript ================================ -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src='https://abhishek-paliwal.github.io/wallpaper_creators/bootstrap-alpha6-js-css/jquery-3.1.1.slim.min.js' crossorigin='anonymous'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js' crossorigin='anonymous'></script>
+<script src='https://abhishek-paliwal.github.io/wallpaper_creators/bootstrap-alpha6-js-css/bootstrap.min.js'></script>
+
+<!-- LAZY-LOADER OF IMAGES -  JQUERY Plugin -->
+<script type='text/javascript' src='https://abhishek-paliwal.github.io/wallpaper_creators/js/JQUERY.LAZY.MIN.js'></script>
+
+<script>
+  \$(document).ready(function(){
+          // ENABLING LAZY LOADING
+          \$('.lazy').Lazy();
+  });
+</script>" >> $OUTPUT
+
+
 echo "</body>" >> $OUTPUT
 echo "</html>" >> $OUTPUT
 
-echo "######## LOGOS Index Successfully created. ######### ";
+echo "######## SM IMAGES Index Successfully created. ######### ";
 echo "####### DONE! File will now be opened in SAFARI. ########"
 open -a Safari $SITEURL
 open -a Safari $OUTPUT
