17d647ff46edeb4167ddae4f748e0c3dd3f0454d
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2018-08-15 21:31:22 +0300

    File added.

---
+++ 1001a-rename-files-with-first-N-words.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+## THIS PROGRAM RENAMES ALL FILES IN A FOLDER BY KEEPING THE FIRST N-WORDS, REMOVING EVERYTHING AFTER THAT
+
+PWD=`pwd` ;
+cd $PWD ;
+echo "Present working directory: $PWD" ; echo ;
+
+mkdir _TMP_DIR ;
+
+echo "How many words from beginning do you want to keep in your new filename: " ;
+read num_words ;
+
+## CONVERTING IT TO AN INTEGER
+num_words_new=`echo $num_words | bc` ;
+
+echo "$num_words = NUMBER OF WORDS TO KEEP FROM BEGINNING " ;
+
+for x in *.* ;
+do
+    filename=$(basename -- "$x") ;
+    extension="${filename##*.}" ;
+    filename="${filename%.*}" ;
+
+    ## NEW VARIABLE WHICH HOLDS WHOLE FILENAME WITH FIRST N-WORDS
+    new_name=`echo $filename | cut -d "-" -f1-$num_words_new` ;
+
+    cp $x _TMP_DIR/$new_name.$extension ;
+    echo "RENAMING DONE: $x -> $new_name.$extension " ;
+
+done
