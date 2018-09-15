e003db3b353967aadf7a08ec01df409f7445e487
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2018-09-15 19:07:44 +0300

    Code for automatic calculations added
    Code for automatic calculations added for collage-block dimensions and rows x columns calculations

--- ffmpeg-commands/114_make_imagemagick_collage_with_title.sh
+++ ffmpeg-commands/114_make_imagemagick_collage_with_title.sh
@@ -17,6 +17,9 @@ cd _TMP_COLLAGE_DIR ;
 NEW_PWD=`pwd` ;
 echo "CURRENT COLLAGE WORKING DIRECTORY: $NEW_PWD" ;
 
+echo ; echo "CURRENT FILES IN DIRECTORY: "
+ls -1 *.*g | nl ## chooses all the png/jpg images
+
 ## PRINTING ALL IMAGES DIMENSIONS OF FINAL FILE
 echo; echo "Printing all image sizes (sorted from small to big):";
 identify -format "%wx%h : %f\n" *.* | sort -r | nl ;
@@ -26,27 +29,55 @@ echo; echo "Now printing the dimensions of all images with how many images they 
 identify -format "%wx%h\n" *.* | sort -nr > _TMP_LIST.TXT ;
 cat _TMP_LIST.TXT | sort -n | uniq -c | sort -k2nr ;
 
+########################################################
 
 NEW_LINE_VAR="\\\\\\\\n" ; ## THIS WILL PRINT OUT \\n
 echo; echo "USE THIS FOR NEW LINES: $NEW_LINE_VAR" ;
 
-echo "ENTER THE TITLE OF THE COLLAGE [ use $NEW_LINE_VAR for new-lines ]: " ;
-echo ; echo "(NOTE: KEEP IT EMPTY IF YOU DON'T WANT ANY TITLE SLIDE) " ;
+echo "ENTER THE TITLE OF THE COLLAGE [ use $NEW_LINE_VAR for new-lines ] [NOTE: KEEP IT EMPTY IF YOU DON'T WANT ANY TITLE SLIDE] : " ;
 echo ;
 read collage_title ;
 
-echo "ENTER THE DIMENSIONS OF THE TITLE AND EACH IMAGE IN COLLAGE AS WxH [eg., 1920x1080]: " ;
+echo "ENTER THE DIMENSIONS OF THE TITLE AND EACH IMAGE IN COLLAGE AS WxH [eg., 1920x1080] [OR leave empty for automatic calculations]: " ;
 echo ;
 read collage_dimensions ;
 
-echo "ENTER THE TILE VALUE AS ROWSxCOLUMNS [eg., 4x5]: " ;
+echo "ENTER THE TILE VALUE AS ROWSxCOLUMNS [eg., 4x5] [OR leave empty for automatic choice]: " ;
 echo ;
 read tile_value ;
+
+########################################################
+total_num_images=`cat _TMP_LIST.TXT | wc -l | sed 's/ //g' ` ;
+echo "======> TOTAL NUMBER OF IMAGES: $total_num_images " ;
+
+## FOLLOWING ARE THE CALCUATIONS DONE FOR 15 MEGAPIXEL COLLAGE = 5000x3125 (change if you want to)
+height_auto=`echo "scale=0; sqrt(5000*3125/$total_num_images/1.5)" | bc -l ` ;
+width_auto=`echo "scale=0; $height_auto*3/2" | bc -l ` ;
+sep="x" ;
+
+echo "Automatic dimensions have been calculated for TOTAL IMAGES: $total_num_images" ;
+echo "Automatic dimensions : $width_auto$sep$height_auto" ;
+
+########################################################
+## CHECKING IF $collage_dimensions WAS ENTERED OR NOT
+if [ -z "$collage_dimensions" ]
+then
+      echo "\$collage_dimensions is empty. Thus, COLLAGE_DIMENSIONS will be calculated automatically." ;
+      collage_dimensions="$width_auto$sep$height_auto" ;
+      echo "collage_dimensions: $collage_dimensions" ;
+else
+      echo "\$collage_dimensions is NOT empty. COLLAGE_DIMENSIONS will be made using tile value = $tile_value " ;
+      montage *.*g -tile $tile_value -geometry $collage_dimensions+5+5 0_my_collage.jpg ;
+fi
+########################################################
 
 echo ;
 echo "collage_title: $collage_title" ;
 echo "collage_dimensions: $collage_dimensions" ;
+echo "tile_value: $tile_value" ;
 echo ;
+
+################################################################
 
 ## Now generating random HEX colors for title
 random_color_background=`echo "#$(openssl rand -hex 3)"` ;
@@ -69,18 +100,25 @@ else
       convert -background "$random_color_background" -fill "$random_color_text" -font /Users/abhishek/Library/Fonts/BebasNeue\ Book.ttf -size "$collage_dimensions" -gravity east label:"$collage_title" 0_my_collage_title.jpg ;
 fi
 
-echo ; echo "CURRENT FILES IN DIRECTORY: "
-ls -1 *.*g | nl ## chooses all the png/jpg images
 
 ## STEP 2: Using IMAGEMAGICK to resize ALL images to Full-HD with padding to keep FULL-HD aspect ratio
 echo "=======> ImageMagick currently resizing all images to $collage_dimensions ...."
 mogrify -resize $collage_dimensions -background black -gravity center -extent $collage_dimensions *
 echo "=======> ImageMagick resizing done ...."
 
-## STEP 3: MAKING COLLAGE USING ALL IMAGES IN PWD
-echo ; echo "Now creating collage ....."
-montage *.*g -tile $tile_value -geometry $collage_dimensions+5+5 0_my_collage.jpg
+## STEP 3: MAKING COLLAGE USING ALL IMAGES IN PWD (ALSO CHECKS IF TILE VAR IS EMPTY)
+echo ; echo "Now creating collage ....." ;
 
+if [ -z "$tile_value" ]
+then
+      echo "\$tile_value is empty. Collage will be made automatically." ;
+      montage *.*g -geometry $collage_dimensions+5+5 0_my_collage.jpg ;
+else
+      echo "\$tile_value is NOT empty. Collage will be made using tile value = $tile_value " ;
+      montage *.*g -tile $tile_value -geometry $collage_dimensions+5+5 0_my_collage.jpg ;
+fi
+
+#######################################################################################
 ## RENAMING COLLAGE FILE BASED UPON title
 FINAL_FILENAME=`echo "$collage_title-collage" | sed 's/\\\\n/-/g' | sed 's/ /-/g'` ;
 cp 0_my_collage.jpg $FINAL_FILENAME.jpg ;
