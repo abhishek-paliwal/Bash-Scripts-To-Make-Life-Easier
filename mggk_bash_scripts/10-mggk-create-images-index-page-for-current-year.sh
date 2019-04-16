b52078e83dd4d7b75b02de3d4c1872cb9f2b72b7
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2019-04-16 15:19:21 +0300

    Some major changes in the layout of images.
    Packery styles are no longer used. Instead vanilla bootstrapy grid layout is used.

--- mggk_bash_scripts/10-mggk-create-images-index-page-for-current-year.sh
+++ mggk_bash_scripts/10-mggk-create-images-index-page-for-current-year.sh
@@ -2,7 +2,7 @@
 CAT << EOF
     ###############################################################################
     ## BOOTSTRAP FRAMEWORK : THIS SCRIPT CREATES AN HTML FILE WITH
-    ## ALL THE IMAGES IN WORKING DIRECTORY + RECURSIVELY
+    ## ALL THE IMAGES IN WORKING DIRECTORY + NON-RECURSIVELY
     ###############################################################################
     ## Coded by: PALI
     ## On: April 13, 2019
@@ -11,20 +11,22 @@ EOF
 
 echo "#################################################" #Blank line
 
-DIRPATH="$HOME/GitHub/2019-HUGO-MGGK-WEBSITE-OFFICIAL/static/wp-content/uploads/2019"
+CURRENT_YEAR="$(date +%Y)"
 CURRENT_MONTH="$(date +%m)"
+DIRPATH="$HOME/GitHub/2019-HUGO-MGGK-WEBSITE-OFFICIAL/static/wp-content/uploads/$CURRENT_YEAR"
 
 MAIN_IMAGES_FOLDER="$CURRENT_MONTH" ## the current month is the main_images_folder
 ROOT="$DIRPATH/$MAIN_IMAGES_FOLDER"
+
+FINALFILENAME="index-of-mggk-images-for-current-year.html"
+OUTPUT="$DIRPATH/$FINALFILENAME" ##Output filename
+SITEURL="https://www.MyGingerGarlicKitchen.com/wp-content/uploads/$CURRENT_YEAR/$FINALFILENAME"
 
 #####################
 cd $ROOT
 echo "CURRENT WORKING DIRECTORY [ROOT]: " $ROOT ##check the present working directory
 echo "#################################################" #Blank line
 
-FINALFILENAME="index-of-mggk-images-for-current-year.html"
-OUTPUT="$DIRPATH/$FINALFILENAME" ##Output filename
-SITEURL="https://www.MyGingerGarlicKitchen.com/wp-content/uploads/$FINALFILENAME"
 
 ##### UNCOMMENT the following three lines for custom image size #######
 # echo -n "Enter the size of the DIV to use (300 works best) [ENTER]: "
@@ -60,11 +62,7 @@ echo "<html lang='en'>
     .grid-item--width2 { width: 45%; border: 1px solid green ;}
 
     .grid-item {
-        /* float:left; */
         width: 16% ;
-        /* height: 220px ;  */
-        /* padding: 5px; */
-        /* margin: 0px; */
     }
 
     .grid {
@@ -106,8 +104,6 @@ echo "<html lang='en'>
             border : 7px solid white ;
           }
       }
-
-
 
 /* PACKERY STYLES END */
 
@@ -200,15 +196,16 @@ echo "<nav class='navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-to
   </a>
 </nav>" >> $OUTPUT
 
-#### OPTIONAL SECTION: Finding and listing all Image files Recursively. ####
-totalimagefiles=$(find $ROOT -type f | egrep -i '\.(jpg|png|PNG|JPG|gif|GIF)$' | wc -l | tr -d '[[:space:]]')
+#### OPTIONAL SECTION: Finding and listing all Image files Non-Recursively. ####
+#totalimagefiles=$(find $ROOT -type f | egrep -i '\.(jpg|png|PNG|JPG|gif|GIF)$' | wc -l | tr -d '[[:space:]]')
+totalimagefiles=$(ls -1 $ROOT | egrep -i '\.(jpg|png|PNG|JPG|gif|GIF)$' | wc -l | tr -d '[[:space:]]')
 
 echo "<div class='site-wrapper'>
 <div class='site-wrapper-inner'>
 
 <!-- HEADING DIV STARTS --> <div class='heading'>
 <!-- TOP LOGO --> <img src='https://www.mygingergarlickitchen.com/wp-content/uploads/2015/02/mggk-new-logo-transparent-150px.png' style='width: 100px; '>
-<h1 class='heading'>MGGK - IMAGES INDEX<br>(FOR CURRENT MONTH)<br>&bull;&bull;&bull;&bull;&bull;</h1>
+<h1 class='heading'>MGGK - IMAGES INDEX<br>FOR CURRENT MONTH<br>// $CURRENT_YEAR/$CURRENT_MONTH //<br>&bull;&bull;&bull;&bull;&bull;</h1>
 
 <h2 class='heading'><font color='#FFFF00'>Designed by</font> <a href='https://www.MyGingerGarlicKitchen.com'><font color='#50c878'>My Ginger Garlic Kitchen</font></a></h2>
 <!-- HEADING DIV ENDS --> </div>
@@ -218,8 +215,9 @@ echo "<div class='site-wrapper'>
 
 echo "<div class='container-fluid'>" >> $OUTPUT
 
-echo "<h3 class='thin'>Total Images On This Page (Recursive) = $totalimagefiles</h3>" >> $OUTPUT
-echo "<h3 class='thin'>Page last updated: "`date`"</h3>" >> $OUTPUT
+echo "<h3 class='thin'>Total Images On This Page (Non-Recursive) = $totalimagefiles</h3>" >> $OUTPUT
+echo "<h3 class='thin'>Page last updated: "$(date)"</h3>" >> $OUTPUT
+echo "<h3 style='color: #cd1c62;'>&bull; &bull; &bull;<br> IMAGES ARE SORTED BY LATEST FIRST <br>&bull; &bull; &bull;</h3>" >> $OUTPUT
 
 ##### CREATING THE HREF-NAME LINKS FOR ALL THE FOLDERS #####
 echo "<hr>" >> $OUTPUT
@@ -227,6 +225,7 @@ echo "<div class='toc'>&bull; Table of Contents &bull;</div>" >> $OUTPUT
 
 for foldername in `find "$ROOT" -maxdepth 0 -mindepth 0 -type d| sort -nr`; do
   folder=`basename "$foldername"`
+  echo ">>>> FOLDER-NAME (For TOC) = $folder" ;
   echo "<div class='toc'><a href='#$folder'>MONTH: $folder</a></div> " >> $OUTPUT
 done
 #### Link Creation Ends #####
@@ -235,11 +234,24 @@ done
 x=0
 for filepath in `find "$ROOT" -maxdepth 0 -mindepth 0 -type d| sort -nr`; do
   path=`basename "$filepath"`
-  echo " <hr> <h2 class='p1'> <a name='$path'>$path</a> <a href='#' style='color: lime ;'>( &uarr; Go to top )</a></h2>" >> $OUTPUT
 
-  echo "<div class='grid'> <!-- PACKERY MASONRY DIV BEGINS -->" >> $OUTPUT
+  echo ">>> FILEPATH = $filepath" ;
+  echo ">>> PATH = BASENAME(FILEPATH) = $path" ;
+  echo;
 
-  for x in $(find "$filepath" -type f| sort -nr | egrep -i '\.(jpg|png|PNG|JPG|gif|GIF)$') ; do
+  echo " <hr> <h2 class='p1'> <a name='$path'>MONTH: $path</a> <a href='#' style='color: lime ;'>( &uarr; Go to top )</a></h2>" >> $OUTPUT
+
+  ## UNCOMMENT THE FOLLOWING LINE IF YOU WANT PACKERY TYPE LOADING OF IMAGES
+  #### AND INSTEAD OF USING <div class='col-6 col-sm-2'> FOR INDIVIDUAL IMAGES,
+  #### USE THIS: <div class='grid-item'>
+  ####
+  #echo "<!-- PACKERY MASONRY DIV BEGINS --> <div class='grid'> " >> $OUTPUT
+  ####
+  ## COMMENT THE FOLLOWING LINE IF YOU ARE USING THE PACKERY STYLE ABOVE
+  echo "<!-- MAIN BOOTSTRAP DIV ROW BEGINS --> <div class='row'> " >> $OUTPUT
+
+  ## IN THE FOLLOWING, THE LS COMMAND SORTS BY LAST MODIFICATION DATE (latest first)
+  for x in $( ls -1t "$filepath"/*.* | egrep -i '\.(jpg|png|PNG|JPG|gif|GIF)$' ) ; do
     file=`basename "$x"`
 
     ## Finding the image dimensions using ImageMagick's identify command.
@@ -256,9 +268,9 @@ for filepath in `find "$ROOT" -maxdepth 0 -mindepth 0 -type d| sort -nr`; do
 
 ## Printing the image dimensions for everything, except GIFs because they produce LOOOOONG outputs for all GIF frames. ##
     if [ "$filetype" != 'GIF' ]; then
-      echo "<div class='grid-item'><div class='pali'><a href='$path/$file'><img src='$path/$file' width='100%'></img><span class='thin'>$file</span></a><br><span class='thin'>$imagedimen</span><br><br><strong style='background-color: deeppink; padding: 5px ; '><a style='color: white ;' href='$MAIN_IMAGES_FOLDER/$path/$file'>Enlarge</a></strong></div></div>" >> $OUTPUT
+      echo "<div class='col-6 col-sm-4 col-md-3 col-lg-2'><div class='pali'><a href='$path/$file'><img src='$path/$file' width='100%'></img><span class='thin'>$file</span></a><br><span class='thin'>$imagedimen</span><br><br><strong style='background-color: deeppink; padding: 5px ; '><a style='color: white ;' href='$MAIN_IMAGES_FOLDER/$path/$file'>Enlarge</a></strong></div></div>" >> $OUTPUT
     else
-      echo "<div class='grid-item'><div class='pali'><a href='$path/$file'><img src='$path/$file' width='100%'></img><span class='thin'>$file</span></a><br><br><strong style='background-color: deeppink ; padding: 5px ; '><a style='color: white ;' href='$MAIN_IMAGES_FOLDER/$path/$file'>Enlarge</a></strong></div></div>" >> $OUTPUT
+      echo "<div class='col-6 col-sm-4 col-md-3 col-lg-2'><div class='pali'><a href='$path/$file'><img src='$path/$file' width='100%'></img><span class='thin'>$file</span></a><br><br><strong style='background-color: deeppink ; padding: 5px ; '><a style='color: white ;' href='$MAIN_IMAGES_FOLDER/$path/$file'>Enlarge</a></strong></div></div>" >> $OUTPUT
     fi
 
   done
