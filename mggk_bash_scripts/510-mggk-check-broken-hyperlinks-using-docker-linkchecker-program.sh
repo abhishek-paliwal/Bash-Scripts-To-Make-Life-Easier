b2c93cd172854195cd3949148ec62ade4e882ec8
abhishek <abhiitbhu@gmail.com>
2019-04-24 14:39:31 +0300

    Script updated.

--- mggk_bash_scripts/510-mggk-check-broken-hyperlinks-using-docker-linkchecker-program.sh
+++ mggk_bash_scripts/510-mggk-check-broken-hyperlinks-using-docker-linkchecker-program.sh
@@ -2,9 +2,19 @@
 ###############################################################################
 cat <<EOF
   ###############################################################################
-  ## This script checks broken hyperlinks in any given site
-  ## THIS SCRIPT USES DOCKER
+  ## THIS SCRIPT CHECKS BROKEN HYPERLINKS IN ANY GIVEN SITE.
+  ## THIS SCRIPT USES DOCKER. SO, RUN DOCKER BEFORE RUNNING IT.
   ## GET HELP: https://github.com/linkchecker/linkchecker
+  ###############################################################################
+  ## USAGE:
+  ## > docker run --rm -it -u \$(id -u):\$(id -g) -v "\$PWD":/mnt linkchecker/linkchecker --verbose www.mygingergarlickitchen.com
+  ###############################################################################
+  ## CODED ON: Wednesday April 24, 2019
+  ## BY: PALI
+  ###############################################################################
+  ## FOR HELP WITH LINKCHECKER PROGRAM: RUN THE FOLLOWING COMMAND:
+  ## >>>>>>>>> PRINTS HELP MESSAGE ABOUT HOW TO RUN THIS IMAGE <<<<<<<<<<<
+  ## docker run --rm -it -u \$(id -u):\$(id -g) -v "\$PWD":/mnt linkchecker/linkchecker --help
   ###############################################################################
 EOF
 ###############################################################################
@@ -13,8 +23,19 @@ EOF
 MY_SITE="https://www.mygingergarlickitchen.com" ;
 HTML_OUTPUT_FILE="_tmp_linkchecker_output_file.html" ;
 
-## PRINTS HELP MESSAGE ABOUT HOW TO RUN THIS IMAGE
-docker run --rm -it -u $(id -u):$(id -g) -v "$PWD":/mnt linkchecker/linkchecker --help
+PWD=$(pwd) ;
+echo; echo "Current working directory: $PWD" ; echo;
+
+###############################################################################
+## USER CONFIRMATION
+echo "You are about to run the following command, are you sure????" ;
+
+echo ">>>>> COMMAND >>>>> docker run --rm -it -u \$(id -u):\$(id -g) -v '\$PWD\':/mnt linkchecker/linkchecker --verbose -o html r0 $MY_SITE > $HTML_OUTPUT_FILE"
+
+read -p ">>>>>> If your sure sure, press ENTER key ..." ;
+echo; 
+###############################################################################
 
 ## RUNS THE ACTUAL COMMAND FOR $MY_SITE
+## docker run --rm -it -u $(id -u):$(id -g) -v "$PWD":/mnt linkchecker/linkchecker --verbose www.mygingergarlickitchen.com
 docker run --rm -it -u $(id -u):$(id -g) -v "$PWD":/mnt linkchecker/linkchecker --verbose -o html r0 $MY_SITE > $HTML_OUTPUT_FILE
