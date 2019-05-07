e4b90a6f8e9ba3269157ae88c0bdca7912a56ea7
abhishek <abhiitbhu@gmail.com>
2019-05-07 21:02:54 +0300

    New bash script added for MGGK sitemap.xml

---
+++ mggk_bash_scripts/512-mggk-get-all-page-links-from-local-sitemap-xml-file.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+################################################################################
+THIS_FILENAME="512-mggk-get-all-page-links-from-local-sitemap-xml-file.sh"
+TMP_OUTPUT_FILE="_512_TMP_OUTPUT.TXT"
+REQUIRED_FILE="sitemap.xml"
+################################################################################
+cat <<EOF
+  #################################################################################
+  ## THIS FILENAME = $THIS_FILENAME
+  ## USAGE: > sh $THIS_FILENAME
+  #################################################################################
+  ## THIS SCRIPT OUTPUTS A TEXT FILE in PWD = $TMP_OUTPUT_FILE , WHICH CONTAINS
+  ## ALL NON-TAGS AND NON-CATEGORIES LINKS FROM THE SITEMAP.XML FILE DOWNLOADED from
+  ## ANY WEBSITE.
+  #################################################################################
+  ## REQUIREMENT: ###########################
+  #### For this script, you need to have a $REQUIRED_FILE file locally stored
+  #### in the Present Working Directory.
+  #################################################################################
+  ###############################################################################
+  ## CODED ON: MAY 7, 2019
+  ## CODED BY: PALI
+  ###############################################################################
+EOF
+
+################################################################################
+PWD=$(pwd) ;
+echo;
+echo "Current working directory = $PWD" ;
+echo;
+
+## USER CONFIRMATION
+read -p "If this working directory is OK, please press ENTER key to continue ..." ;
+echo;
+
+################################################################################
+## REAL MAGIC HAPPENS BELOW
+
+## Running command and printing output on CLI
+cat $REQUIRED_FILE | grep '<loc>' | grep -iv 'tags' | grep -iv 'categories' | sed 's|<loc>||g' | sed 's|</loc>||g' | sort
+
+## Saving output to text file
+echo; echo ">>>> Saving output to output text file = $TMP_OUTPUT_FILE " ;
+cat $REQUIRED_FILE | grep '<loc>' | grep -iv 'tags' | grep -iv 'categories' | sed 's|<loc>||g' | sed 's|</loc>||g' | sort > $TMP_OUTPUT_FILE
+
+## OPENING PWD
+echo; echo ">>>> Opening PWD = $PWD" ;
+open $PWD
