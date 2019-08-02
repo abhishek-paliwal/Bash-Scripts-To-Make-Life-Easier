0797a51488b45b4d205b1adca3d71ffa2e439251
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2019-08-02 15:50:01 +0300

    Now witdh is made to be 100% of screen width.

--- 0D_images2html-very-simple-photoindex-maker.sh
+++ 0D_images2html-very-simple-photoindex-maker.sh
@@ -2,7 +2,7 @@
 ##################################################
 ## THIS PROGRAM MAKES AN INDEX HTML PAGE WITH ALL THE IMAGES
 ## IN THE PRESENT WORKIND DIRECTORY
-## DATE: Friday August 31, 2018
+## DATE: Friday August 01, 2019
 ## By: Pali
 ##################################################
 PWD=`pwd` ;
@@ -10,31 +10,73 @@ cd $PWD ;
 echo "Present working directory: $PWD" ;
 ##################################################
 
-x="index-of-all-photos.html" ;
+DIRNAME=$(basename $PWD) ;
+
+x="index-of-all-photos-$DIRNAME.html" ;
 touch $x
 rm $x
 
 TOTAL_IMAGES=`ls *.*g | wc -l | sed 's/ //g'` ;
 
-echo "<body bgcolor=white><center>" >> $x
-echo "<font color=red><h1>DIRECTORY: $PWD</h1></font>" >> $x
-echo "<font color=blue><h2>TOTAL IMAGES IN FOLDER: $TOTAL_IMAGES</h2></font>" >> $x
+echo "<!DOCTYPE html>
+<html>
+<head>
+<style>
+html, body {
+  height: 100%;
+}
+
+img.one {
+  height: auto;
+  width: auto;
+}
+
+img.two {
+  height: 100%;
+  width: 100%;
+}
+</style>
+
+</head>" >> $x
+
+## THE USE OF oncontextmenu=\"return false;\" BELOW WILL DISABLE RIGHT-CLICKS ON WEBPAGE
+
+echo "<body bgcolor=white oncontextmenu=\"return false;\"><center>" >> $x
+echo "<h2 style='color: rgb(240,240,240) '; >DIRECTORY: $PWD</h2>" >> $x
+echo "<font color=blue><h2>TOTAL IMAGES IN THIS FOLDER: $TOTAL_IMAGES</h2></font>" >> $x
+
+echo "<p>(Images are sized as 100% of window-width // Clicks disabled on webpage)</p>" >>  $x
+
 echo "<hr>" >> $x
+echo "<h1 style='font-size: 14vw ; line-height: 0.8 '; >$DIRNAME</h1>"  >> $x
+
 ###################################################
 
 ## Taking all jpg/png
 for p in *.*g
 do
-echo "<a href=\"$p\"> <img src=\"$p\" alt=\"$p\" height=20%> </a>" >> $x
+  # WITH HYPERLINKS
+  #echo "<a href=\"$p\"> <img class=\"two\" src=\"$p\" alt=\"$p\"> </a>" >> $x
+  # WITHOUT HYPERLINKS
+  echo "<img class=\"two\" src=\"$p\" alt=\"$p\">" >> $x
 done
 
 ## Taking all JPG/PNG
 for p in *.*G
 do
-echo "<a href=\"$p\"> <img src=\"$p\" alt=\"$p\" height=20%> </a>" >> $x
+  # WITH HYPERLINKS
+  #echo "<a href=\"$p\"> <img class=\"two\" src=\"$p\" alt=\"$p\"> </a>" >> $x
+  # WITHOUT HYPERLINKS
+  echo "<img class=\"two\" src=\"$p\" alt=\"$p\">" >> $x
 done
 
 ###################################################
 ## OPENING DIRECTORY
 echo "Opening directory: $PWD" ;
 open $PWD ;
+echo "Opening HTML file: $PWD" ;
+open $x ;
+
+## IN CASE OF ERRORS
+echo ">>>> In case of ERRORS:"
+echo ">>>> NOTE: Make sure that the directory name does not have any spaces. If so, then convert spaces to hyphens."
