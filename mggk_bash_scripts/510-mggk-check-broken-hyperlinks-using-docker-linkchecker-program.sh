32760faa38203b78cde8e13e6136b6222cd18b6d
abhishek <abhiitbhu@gmail.com>
2019-10-21 14:31:58 +0300

    Update 510-mggk-check-broken-hyperlinks-using-docker-linkchecker-program.sh

--- mggk_bash_scripts/510-mggk-check-broken-hyperlinks-using-docker-linkchecker-program.sh
+++ mggk_bash_scripts/510-mggk-check-broken-hyperlinks-using-docker-linkchecker-program.sh
@@ -6,8 +6,10 @@ cat <<EOF
   ## THIS SCRIPT USES DOCKER. SO, RUN DOCKER BEFORE RUNNING IT.
   ## GET HELP: https://github.com/linkchecker/linkchecker
   ###############################################################################
-  ## USAGE:
+  ## USAGE (if run from command line interactive terminal):
   ## > docker run --rm -it -u \$(id -u):\$(id -g) -v "\$PWD":/mnt linkchecker/linkchecker --verbose --check-extern www.mygingergarlickitchen.com
+  ## USAGE (if run from crontab by root):
+  ## > docker run --rm -u \$(id -u):\$(id -g) -v "\$PWD":/mnt linkchecker/linkchecker --verbose --check-extern www.mygingergarlickitchen.com
   ###############################################################################
   ## CODED ON: Wednesday April 24, 2019
   ## BY: PALI
@@ -37,7 +39,11 @@ echo ;
 ###############################################################################
 
 ## RUNS THE ACTUAL COMMAND FOR \$MY_SITE (r0 = link recursion depth)
+#### Running from the interactive shell via terminal application, CLI version
 sudo docker run --rm -it -u $(id -u):$(id -g) -v "$PWD":/mnt linkchecker/linkchecker --verbose --check-extern -F html $MY_SITE
+#### Running from the crontab by root (Don't forget to remove -it from the docker command )
+#sudo docker run --rm -u $(id -u):$(id -g) -v "$PWD":/mnt linkchecker/linkchecker --verbose --check-extern -F html $MY_SITE
+
 
 ###############################################################################
 ###############################################################################
