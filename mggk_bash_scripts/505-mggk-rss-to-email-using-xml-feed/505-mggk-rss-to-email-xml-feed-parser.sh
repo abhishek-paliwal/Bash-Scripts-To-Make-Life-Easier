1c66a5d95bab75dbff6e2c61fccf93ce0b8bd1c9
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2019-10-25 19:49:36 +0300

    Update 505-mggk-rss-to-email-xml-feed-parser.sh

--- mggk_bash_scripts/505-mggk-rss-to-email-using-xml-feed/505-mggk-rss-to-email-xml-feed-parser.sh
+++ mggk_bash_scripts/505-mggk-rss-to-email-using-xml-feed/505-mggk-rss-to-email-xml-feed-parser.sh
@@ -29,8 +29,23 @@ echo ;
 ## this particular rss feed file contains only 8 entries)
 echo "Initializing the HTML file : $OUTPUT_HTML_FILE " ;
 
+echo "<!doctype html>
+<html lang='en'>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+
+    <!-- Bootstrap CSS -->
+    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' crossorigin='anonymous'>
+		<title>Latest delicious recipes from My Ginger Garlic Kitchen</title>
+	</head>
+	<body>
+	<div class='container'>" > $OUTPUT_HTML_FILE ;
+
+
 echo "<p>Hello [Name,fallback=]</p><p><strong>🍴Please find the latest food posts below published on the website:</strong></p><p>Simply click the buttons below to watch the how-to videos and to download recipes.</p>
-<p style='text-align: center; color: #cd1c62; '>&bull; &bull; &bull; &bull; &bull; &bull; &bull; &bull;</p>" > $OUTPUT_HTML_FILE ; ## WRITING THE FIRST EMPTY LINE, THEN APPENDING LATER
+<p style='text-align: center; color: #cd1c62; '>&bull; &bull; &bull; &bull; &bull; &bull; &bull; &bull;</p>" >> $OUTPUT_HTML_FILE ; ## WRITING THE FIRST EMPTY LINE, THEN APPENDING LATER
 
 echo "<center>" >> $OUTPUT_HTML_FILE ;
 
@@ -42,7 +57,7 @@ do
 	LINK=$( xmlstarlet sel -t -v "/rss/channel/item[$x]/link" $OUTPUT_RSSFEED_FILE ) ;
 	DESCRIPTION=$( xmlstarlet sel -t -v "/rss/channel/item[$x]/description" $OUTPUT_RSSFEED_FILE ) ;
 
-	echo "<h2><a href='$LINK'>$x.) $TITLE</a></h2>" >> $OUTPUT_HTML_FILE;
+	echo "<h3><a style='color: #cd1c62;' href='$LINK'>$x.) $TITLE</a></h3>" >> $OUTPUT_HTML_FILE;
 	echo "<p>Published: $PUBDATE</p>" >> $OUTPUT_HTML_FILE ;
 	echo "<p>$DESCRIPTION</p>" >> $OUTPUT_HTML_FILE ;
 	echo "<div style='background-color: #cd1c62;
@@ -74,6 +89,14 @@ echo "<webversion>View web version</webversion> // <unsubscribe>Unsubscribe</uns
 echo "<p><a href='https://www.mygingergarlickitchen.com' target='_blank'><img src='https://www.mygingergarlickitchen.com/wp-content/uploads/2015/02/mggk-new-logo-transparent-150px.png' width='100px' ;></img></a></p>" >> $OUTPUT_HTML_FILE ;
 
 echo "</center>" >> $OUTPUT_HTML_FILE ; ## WRITING THE FIRST EMPTY LINE, THEN APPENDING LATER
+
+echo "<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' crossorigin='anonymous'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' crossorigin='anonymous'></script>
+<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' crossorigin='anonymous'></script>
+</body>
+</html>" >> $OUTPUT_HTML_FILE ;
 
 ##########################################
 ## Replacing strange characters to formal html symbols and saving to final html file
