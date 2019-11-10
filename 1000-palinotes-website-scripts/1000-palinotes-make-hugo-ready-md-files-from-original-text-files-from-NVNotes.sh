d55b0c1a81a3e4bab4b0c8ddaf2e5a9f9e78975e
abhishek <abhiitbhu@gmail.com>
2019-11-10 22:09:36 +0200

    Create 1000-palinotes-make-hugo-ready-md-files-from-original-text-files-from-NVNotes.sh

---
+++ 1000-palinotes-website-scripts/1000-palinotes-make-hugo-ready-md-files-from-original-text-files-from-NVNotes.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+################################################################################
+MY_DIR="/Users/anu/Desktop/X/notes_nv"
+################################################################################
+OUTPUT_DIR="$MY_DIR/_TMP_OUTPUTS"
+mkdir $OUTPUT_DIR ;
+################################################################################
+
+for x in *.txt ;
+do
+  ##
+  ## Find file modification time of current file
+  MOD_TIME=$(stat -s $x | awk '{print $10}' | sed 's/st_mtime=//g')
+
+  ## Get date prefix based upon above modification time
+  DATE_PREFIX=$(date -r $MOD_TIME +%Y-%m-%dT%H:%M:%S)
+  DATE_PREFIX_YMD=$(date -r $MOD_TIME +%Y-%m-%d)
+  NEW_FILENAME=$(echo $DATE_PREFIX_YMD-$x | sed 's/_/-/g' | sed 's/.txt/.md/g')
+
+  ## Extracting URL and TITLE from original filename
+  TITLE=$(echo $x | sed 's/_/ /g' |sed 's/.txt//g')
+  URL=$(echo $TITLE | sed 's/ /-/g' | tr 'A-Z' 'a-z')
+
+  ## Printing all variables before anything
+  echo "CURRENT FILE: $x"
+  echo "MOD_TIME = $MOD_TIME"
+  echo "DATE_PREFIX: $DATE_PREFIX"
+  echo "DATE_PREFIX_YMD: $DATE_PREFIX_YMD"
+  echo "TITLE = $TITLE"
+  echo "URL = $URL"
+  echo "NEW_FILENAME: $NEW_FILENAME"
+  echo;
+
+## Writing to a new file and saving to _output
+echo "---" > $OUTPUT_DIR/$NEW_FILENAME ## Initializing by writing the first line
+
+echo "title: $TITLE
+url: /$URL/
+date: $DATE_PREFIX
+publishdate: $DATE_PREFIX_YMD
+lastmod: $DATE_PREFIX_YMD
+draft: false
+tags: ["tag1", "tag2"]
+---
+
+" | tee -a $OUTPUT_DIR/$NEW_FILENAME
+
+  ## Appending original file content to the new file
+  cat "$x" >> $OUTPUT_DIR/$NEW_FILENAME
+
+done
