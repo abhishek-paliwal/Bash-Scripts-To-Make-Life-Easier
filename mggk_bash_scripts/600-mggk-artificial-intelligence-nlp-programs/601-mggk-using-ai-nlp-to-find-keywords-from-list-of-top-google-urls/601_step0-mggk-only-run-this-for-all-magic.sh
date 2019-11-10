2d702f705c786c3c980586207acf675d732ce88c
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2019-11-10 14:11:57 +0200

    Update 601_step0-mggk-only-run-this-for-all-magic.sh

--- mggk_bash_scripts/600-mggk-artificial-intelligence-nlp-programs/601-mggk-using-ai-nlp-to-find-keywords-from-list-of-top-google-urls/601_step0-mggk-only-run-this-for-all-magic.sh
+++ mggk_bash_scripts/600-mggk-artificial-intelligence-nlp-programs/601-mggk-using-ai-nlp-to-find-keywords-from-list-of-top-google-urls/601_step0-mggk-only-run-this-for-all-magic.sh
@@ -17,7 +17,7 @@ else
   echo "USER = $USER // USER is not ubuntu. Hence, MY_PWD will be: $MY_PWD " ;
 fi
 
-cd $MY_DIR ;
+cd $MY_PWD ;
 ################################################################################
 
 BASH_SCRIPT_TO_DOWNLOAD="601-step1-run-this-bash-script-if-website-subfolders-are-present-for-AI-NLP-mggk.sh"
@@ -26,7 +26,7 @@ echo ">>>> DOWNLOADING => $BASH_SCRIPT_TO_DOWNLOAD" ; echo;
 curl -O https://raw.githubusercontent.com/abhishek-paliwal/Bash-Scripts-To-Make-Life-Easier/master/mggk_bash_scripts/600-mggk-artificial-intelligence-nlp-programs/601-mggk-using-ai-nlp-to-find-keywords-from-list-of-top-google-urls/$BASH_SCRIPT_TO_DOWNLOAD
 
 ## Moving to correct directory
-mv $BASH_SCRIPT_TO_DOWNLOAD $MY_DIR/$BASH_SCRIPT_TO_DOWNLOAD
+mv $BASH_SCRIPT_TO_DOWNLOAD $MY_PWD/$BASH_SCRIPT_TO_DOWNLOAD
 
 echo ">>>> RUNNING => $BASH_SCRIPT_TO_DOWNLOAD" ; echo;
-bash $MY_DIR/$BASH_SCRIPT_TO_DOWNLOAD
+bash $MY_PWD/$BASH_SCRIPT_TO_DOWNLOAD
