053da187600c595dd096f1ed69511a5c31edacd2
abhishek <abhiitbhu@gmail.com>
2019-11-10 22:13:53 +0200

    Update 1000-palinotes-make-hugo-ready-md-files-from-original-text-files-from-NVNotes.sh

--- 1000-palinotes-website-scripts/1000-palinotes-make-hugo-ready-md-files-from-original-text-files-from-NVNotes.sh
+++ 1000-palinotes-website-scripts/1000-palinotes-make-hugo-ready-md-files-from-original-text-files-from-NVNotes.sh
@@ -1,39 +1,50 @@
 #!/bin/bash
 ################################################################################
-MY_DIR="/Users/anu/Desktop/X/notes_nv"
+## THIS SCRIPT PARSES EXPORTED TXT FILES FROM NVNOTES IN A FORMAT READY TO BE
+## USED IN HUGO STATIC SITE GENERATOR. IT GENERATES MARKDOWN (MD) FILES AT A RESULT
+## FOR EACH TXT FILE ENCOUNTERED. JUST PUT THOSE OUTPUT MD FILES INTO
+## HUGO'S CONTENT/POSTS/ DIRECTORY.
+################################################################################
+################################################################################
+
+####################### REAL MAGIC BELOW #######################################
+################################################################################
+MY_DIR="$HOME/Desktop/X/notes_nv"
 ################################################################################
 OUTPUT_DIR="$MY_DIR/_TMP_OUTPUTS"
 mkdir $OUTPUT_DIR ;
 ################################################################################
 
+## BEGIN : MAIN FOR LOOP #######################################################
 for x in *.txt ;
 do
-  ##
-  ## Find file modification time of current file
-  MOD_TIME=$(stat -s $x | awk '{print $10}' | sed 's/st_mtime=//g')
+##
+## Find file modification time of current file
+MOD_TIME=$(stat -s $x | awk '{print $10}' | sed 's/st_mtime=//g')
 
-  ## Get date prefix based upon above modification time
-  DATE_PREFIX=$(date -r $MOD_TIME +%Y-%m-%dT%H:%M:%S)
-  DATE_PREFIX_YMD=$(date -r $MOD_TIME +%Y-%m-%d)
-  NEW_FILENAME=$(echo $DATE_PREFIX_YMD-$x | sed 's/_/-/g' | sed 's/.txt/.md/g')
+## Get date prefix based upon above modification time
+DATE_PREFIX=$(date -r $MOD_TIME +%Y-%m-%dT%H:%M:%S)
+DATE_PREFIX_YMD=$(date -r $MOD_TIME +%Y-%m-%d)
+NEW_FILENAME=$(echo $DATE_PREFIX_YMD-$x | sed 's/_/-/g' | sed 's/.txt/.md/g')
 
-  ## Extracting URL and TITLE from original filename
-  TITLE=$(echo $x | sed 's/_/ /g' |sed 's/.txt//g')
-  URL=$(echo $TITLE | sed 's/ /-/g' | tr 'A-Z' 'a-z')
+## Extracting URL and TITLE from original filename
+TITLE=$(echo $x | sed 's/_/ /g' |sed 's/.txt//g')
+URL=$(echo $TITLE | sed 's/ /-/g' | tr 'A-Z' 'a-z')
 
-  ## Printing all variables before anything
-  echo "CURRENT FILE: $x"
-  echo "MOD_TIME = $MOD_TIME"
-  echo "DATE_PREFIX: $DATE_PREFIX"
-  echo "DATE_PREFIX_YMD: $DATE_PREFIX_YMD"
-  echo "TITLE = $TITLE"
-  echo "URL = $URL"
-  echo "NEW_FILENAME: $NEW_FILENAME"
-  echo;
+## Printing all variables before anything
+echo "CURRENT FILE: $x"
+echo "MOD_TIME = $MOD_TIME"
+echo "DATE_PREFIX: $DATE_PREFIX"
+echo "DATE_PREFIX_YMD: $DATE_PREFIX_YMD"
+echo "TITLE = $TITLE"
+echo "URL = $URL"
+echo "NEW_FILENAME: $NEW_FILENAME"
+echo;
 
 ## Writing to a new file and saving to _output
 echo "---" > $OUTPUT_DIR/$NEW_FILENAME ## Initializing by writing the first line
 
+########################################
 echo "title: $TITLE
 url: /$URL/
 date: $DATE_PREFIX
@@ -44,8 +55,12 @@ tags: ["tag1", "tag2"]
 ---
 
 " | tee -a $OUTPUT_DIR/$NEW_FILENAME
+########################################
 
-  ## Appending original file content to the new file
-  cat "$x" >> $OUTPUT_DIR/$NEW_FILENAME
+## Appending original file content to the new file
+cat "$x" >> $OUTPUT_DIR/$NEW_FILENAME
 
 done
+## END : MAIN FOR LOOP #######################################################
+
+################################################################################
