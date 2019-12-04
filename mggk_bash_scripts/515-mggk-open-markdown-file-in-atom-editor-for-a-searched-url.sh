d6817dad1f0f3e8116a98df2ace2f002f5ff9c01
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2019-12-04 23:59:13 +0200

    Update 515-mggk-open-markdown-file-in-atom-editor-for-a-searched-url.sh

--- mggk_bash_scripts/515-mggk-open-markdown-file-in-atom-editor-for-a-searched-url.sh
+++ mggk_bash_scripts/515-mggk-open-markdown-file-in-atom-editor-for-a-searched-url.sh
@@ -31,12 +31,12 @@ echo;
 read -p "Enter Your URL to search the MARKDOWN file: "  TMPURL
 
 ##------------------------------------------------------------------------------
-## CREATING SEARCH_URL FROM THE USER 1INPUT
+## CREATING SEARCH_URL FROM THE USER INPUT
 SEARCHURL=$(echo $TMPURL | sed 's|https://www.mygingergarlickitchen.com||g')
 
 ## EXIT THE SCRIPT IF THE ENTERED URL IS NOT PROPER
 if [[ "$SEARCHURL" == "/" ]] || [[ "$SEARCHURL" == "" ]] ; then
-  echo; echo ">> ERROR: INVALID SEARCH URL. Please don't enter the URL of homepage. Any other page is okay." ;
+  echo; echo ">> ERROR: INVALID SEARCH URL. Please don't enter the URL of homepage, or an empty URL. Any other mggk URL is okay." ;
   exit 1; ## Exit this script if invalid url.
 fi
 ##------------------------------------------------------------------------------
@@ -48,20 +48,24 @@ echo "CURRENT SEARCHURL = $SEARCHURL" ;
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" ;
 
 ## COUNT THE NUMBER OF FILES WITH CURRENT URL IN YAML FRONTMATTER.
-## THE ANSWER SHOULD ONLY BE ONE. BUT JUST TO MAKE SURE, RUN THE FOLLOWING COMMAND.
-NUM_FILES=$(grep -irl "url: $SEARCHURL" $SEARCHDIR | wc -l) ;
-echo;
+## THE ANSWER SHOULD ONLY BE 1. BUT JUST TO MAKE SURE, RUN THE FOLLOWING COMMAND.
+NUM_FILES=$(grep -irl "url: $SEARCHURL" $SEARCHDIR | wc -l | tr -d '[:space:]') ;
+echo ">>>> NUMBER OF FILES FOUND: $NUM_FILES" ;
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" ;
-ECHO ">>>> NUMBER OF FILES FOUND: $NUM_FILES" ;
-ECHO ">>>>>>> IMPORTANT NOTE: Make sure that the number of files found in the above output is equal to 1. Else, it means that there are more than one markdown files with the same url in the YAML frontmatter, which should never happen. DOUBLE-CHECK THAT." ;
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" ;
+
+## Check, how many files with this url are returned.
+if [[ "$NUM_FILES" -eq 0 ]] || [[ "$NUM_FILES" -gt 1 ]] ; then
+  echo ">>>>>> ERROR: INVALID MGGK URL. Program found either zero, or more than 1 md files containing this URL." ;
+  echo ">>>>>> ERROR NOTE: Make sure that the number of files found in the above output is exactly equal to 1. Else, it means that there are more than one markdown files with the same url in the YAML frontmatter, which should never happen. DOUBLE-CHECK THAT." ;
+  exit 1; ## Exit this script
+else
+  echo ">>>>>> SUCCESS! Only 1 valid file is found with this URL. Good work!"
+fi
 
 ## SEARCH FOR THE MD FILE WHERE THE URL LIES IN THE YAML FRONTMATTER
 MY_COMMAND="grep -irl \"url: $SEARCHURL\" $SEARCHDIR | head -1" ;
 echo;
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" ;
 echo ">> BASH COMMAND TO RUN = $MY_COMMAND" ;
-echo;echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" ;
 
 ## EXAMPLE SEARCH BELOW
 echo;echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" ;
