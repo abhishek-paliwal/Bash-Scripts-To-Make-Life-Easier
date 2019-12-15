43385d2ea1d8118949244e433552844533d102b8
abhishek <abhiitbhu@gmail.com>
2019-12-15 16:13:41 +0200

    Update 596_step0-only-run-this-to-CREATE-RECIPE-JSON-AND-CSV-FOR-ALL-MD-FILES.sh

--- mggk_bash_scripts/596-mggk-pyhon-create-recipe-json-n-csv-using-frontmatter/596_step0-only-run-this-to-CREATE-RECIPE-JSON-AND-CSV-FOR-ALL-MD-FILES.sh
+++ mggk_bash_scripts/596-mggk-pyhon-create-recipe-json-n-csv-using-frontmatter/596_step0-only-run-this-to-CREATE-RECIPE-JSON-AND-CSV-FOR-ALL-MD-FILES.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
-
+################################################################################
+THIS_SCRIPT_NAME="596_step0-only-run-this-to-CREATE-RECIPE-JSON-AND-CSV-FOR-ALL-MD-FILES.sh"
+################################################################################
 MY_PWD="$HOME/Desktop/X/"
-#cd $MY_PWD ;
+cd $MY_PWD ;
 echo "Current working directory = $MY_PWD" ;
-
+################################################################################
+## VARIABLE DEFINITIONS
 HUGO_CONTENT_DIR="$HOME/GitHub/2019-HUGO-MGGK-WEBSITE-OFFICIAL/content" ;
 HUGO_CONTENT_SUBDIR="$HOME/GitHub/2019-HUGO-MGGK-WEBSITE-OFFICIAL/content/popular-posts" ;
 
 PYTHON_SCRIPT_NAME="$HOME/Github/Bash-Scripts-To-Make-Life-Easier/mggk_bash_scripts/596-mggk-pyhon-create-recipe-json-n-csv-using-frontmatter/596_step1-mggk-CREATE-RECIPE-JSON-AND-RECIPE-CSV-FROM-FRONTMATTER-USING-PYTHON.py" ;
+
+################################################################################
+cat << EOF
+  ################################################################################
+  ## THIS BASH SCRIPT PREPROCESSES USER INPUT DATA AND THEN INVOKES
+  ## THIS PYTHON SCRIPT => $PYTHON_SCRIPT_NAME
+  ##------------------------------------------------------------------------------
+  ## DETAILS:
+  ## THIS BASH SCRIPT READS A USER INPUT URL, AND
+  ## THEN BASED UPON THAT FINDS THE CORRESPONDING MD FILE IN HUGO_CONTENT_DIR, AND PASSES
+  ## THAT MD FILENAME TO THE PYTHON SCRIPT.
+  ## THAT PYTHON SCRIPT THEN EXTRACTS THE mggk_json_recipe YAML FRONTMATTER CONTENT,
+  ## PARSES IT AND FINALLY CREATES A RECIPE JSON AND A RECIPE CSV FILE. THIS CSV FILE
+  ## IS THEN TO BE READ BY 513B SCRIPT FOR MAKING THE RECIPE JSON AND HTML BLOCKS.
+  ##------------------------------------------------------------------------------
+  ## IMPORTANT NOTE: ALL OUTPUT JSON AND CSV FILES ARE PRODUCES IN $MY_PWD
+  ##------------------------------------------------------------------------------
+  ## USAGE:
+  ## bash $THIS_SCRIPT_NAME
+  ##------------------------------------------------------------------------------
+  ## CREATED ON: Sunday December 15, 2019
+  ## CREATED BY: Pali
+  ################################################################################
+EOF
+################################################################################
 
 ################################################################################
 #### BEGIN: DEFINING MAIN FUNCTIONS ############################################
@@ -72,10 +100,16 @@ FUNC_CREATE_JSON_N_CSV_FILES_FOR_THIS_URL_ONLY () {
 ################################################################################
 
 ##------------------------------------------------------------------------------
-## RUNNING THE FUNCTION (UNCOMMENT IF NECESSARY)
-#FUNC_CREATE_JSON_N_CSV_FILES_FOR_ALL_MD_FILES_IN_HUGO_CONTENT_DIR
+## RUNNING THE FUNCTION(s)
 FUNC_CREATE_JSON_N_CSV_FILES_FOR_THIS_URL_ONLY
+## UNCOMMENT THE FOLLOWING SUBDIR FUNCION RUN LINE IF DESIRED
+#FUNC_CREATE_JSON_N_CSV_FILES_FOR_ALL_MD_FILES_IN_HUGO_CONTENT_SUBDIR
+
 ##------------------------------------------------------------------------------
 
 ## PRETTY PRINTING (ON CLI) OF ALL JSON FILES THUS PRODUCED BY ABOVE PYTHON SCRIPT
 for jsonfile in *.json ; do cat $jsonfile | jq ; done
+
+## FINALLY OPENING $MY_PWD
+echo "Opening DIRECTORY: $MY_PWD ..." ;
+open $MY_PWD
