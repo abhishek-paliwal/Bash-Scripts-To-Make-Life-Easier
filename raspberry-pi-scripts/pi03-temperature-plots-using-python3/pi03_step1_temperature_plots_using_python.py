c97883aa93d57c6d9ffae11d0507057888a15879
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2020-01-16 03:06:43 +0200

    Update pi03_step1_temperature_plots_using_python.py

--- raspberry-pi-scripts/pi03-temperature-plots-using-python3/pi03_step1_temperature_plots_using_python.py
+++ raspberry-pi-scripts/pi03-temperature-plots-using-python3/pi03_step1_temperature_plots_using_python.py
@@ -1,8 +1,8 @@
 ################################################################################
 """
-## THIS PROGRAM USES CSV OUTPUTS CREATED FROM 601... PYTHON SCRIPT, AND CREATE
+## THIS PROGRAM USES CSV OUTPUTS CREATED FROM PI03... PYTHON SCRIPT, AND CREATE
 ## VARIOUS PLOTS USING PYTHON pandas AND matplotlib packages.
-## Created on: Monday November 18, 2019
+## Created on: January 16, 2020
 ## Created by: Pali
 """
 ################################################################################
@@ -26,8 +26,11 @@ myfilename, myfilename_extension = os.path.splitext(CSV_FILE)
 MYPLOT_FILENAME = myfilename + '.png'
 ################################################################################
 
+## READING TIMESERIES DATA FROM CSV FILE
 series = read_csv(CSV_FILE, header=0, index_col=0, parse_dates=True, squeeze=True)
+print(">>>> PRINTING SOME HEADER ROWS ...")
 print(series.head())
+print(">>>> PRINTING DESCRIPTION FOR THE DATA SERIES ...")
 print(series.describe())
 
 ############## FUNCION: CHOOSE RANDOM COLOR FOR PLOT #########################
@@ -41,13 +44,14 @@ def FUNCTION_CHOOSE_RANDOM_COLOR_FOR_PLOT():
 
 plt.close('all') ## IF already plot interface is open
 
-## CREATES A FIGURE WITH 1280x720 PIXELS DIMENSIONS
+## CREATES A FIGURE WITH 1200x800 PIXELS DIMENSIONS
 #### You can use html color names to use below
 chosen_random_color = FUNCTION_CHOOSE_RANDOM_COLOR_FOR_PLOT() ;
-myfig = series.plot(color=chosen_random_color, alpha=1, figsize=(12.8, 7.2))
+myfig = series.plot(color=chosen_random_color, alpha=1, figsize=(12, 8))
 fig = myfig.get_figure() ##  We need to break the np.arrary for plots, into elements.
 
 fig.suptitle('CSV DATA FILE USED = ' + CSV_FILE + ' // DIMENSIONS [#ROWS,#COLUMNS] = ' + str(series.shape) + '\n\n Left axis = Temperature in Degree Celsius // Right axis = Time of the day' )
 
 fig.savefig(MYPLOT_FILENAME)
 print(); print('>> SAVED, PNG FILE FOR PLOT FIGURE = ' + MYPLOT_FILENAME) ;
+################################################################################
