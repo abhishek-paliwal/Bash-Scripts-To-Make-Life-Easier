351c81d5a0a502a03c6e46d7531be2c8e472cba3
abhishek <abhiitbhu@gmail.com>
2020-01-17 16:10:03 +0200

    Update pi03_step1_temperature_plots_using_python.py

--- raspberry-pi-scripts/pi03-temperature-plots-using-python3/pi03_step1_temperature_plots_using_python.py
+++ raspberry-pi-scripts/pi03-temperature-plots-using-python3/pi03_step1_temperature_plots_using_python.py
@@ -47,7 +47,7 @@ plt.close('all') ## IF already plot interface is open
 ## CREATES A FIGURE WITH 1200x800 PIXELS DIMENSIONS
 #### You can use html color names to use below
 chosen_random_color = FUNCTION_CHOOSE_RANDOM_COLOR_FOR_PLOT() ;
-myfig = series.plot(color=chosen_random_color, alpha=1, figsize=(12, 8))
+myfig = series.plot(color=chosen_random_color, alpha=1, figsize=(12, 6))
 fig = myfig.get_figure() ##  We need to break the np.arrary for plots, into elements.
 
 fig.suptitle('CSV DATA FILE USED = ' + CSV_FILE + ' // DIMENSIONS [#ROWS,#COLUMNS] = ' + str(series.shape) + '\n\n Left axis = Temperature in Degree Celsius // Right axis = Time of the day' )
