30a7c519df2c4218510b6e3b4849d9b19e42234a
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2020-04-21 19:54:27 +0300

    2 bash scripts added

---
+++ 999-copy-n-random-files-in-folder.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+###########################################################
+## THIS PROGRAM COPIES N-RANDOM FILES TO TMP DIRECTORIES
+## FROM ALL THE FILES IN A GIVEN DIRECTORY USING 2 DIFFERENT APPROACHES
+## 1. BY SELECTING EVERY Nth FILE (eg., will select every 5th file after 'ls' from a list of 100 files, etc.)
+## 2. BY DOING A SHUFFLE COMMAND (TOTALLY RANDOM FILES)
+## MADE BY: PALI
+## MADE ON: Thursday September 27, 2018
+###########################################################
+
+PWD=`pwd` ;
+echo; echo "Current working directory: $PWD " ;
+cd $PWD ;
+
+##################################
+echo ; echo "How many files do you want to copy [enter an integer]: " ;
+read HOW_MANY_FILES_TO_SELECT
+##################################
+
+TMP_DIR="_TMP-$HOW_MANY_FILES_TO_SELECT-EQUIDISTANT_RANDOM_FILES_FOR_TIMESLICE" ;
+TMP_DIR_RANDOM="_TMP-$HOW_MANY_FILES_TO_SELECT-TOTALLY_RANDOM_FILES" ;
+
+mkdir $TMP_DIR $TMP_DIR_RANDOM ;
+
+###########################################################
+## 1. COMMAND BLOCK TO COPY EQUIDISTANT RANDOM FILES TO TMP_DIR
+## FINDING TOTAL FILES IN PWD
+NUM_FILES=$(scale=0; ls -1 | wc -l | bc -l) ;
+
+## NOW, FINDING HOW MANY FILES TO SKIP TO GET DESIRED NUMBER OF FINAL FILES
+NUM_RANDOM_FILES=$(echo "scale=0; $NUM_FILES/$HOW_MANY_FILES_TO_SELECT" | bc -l ) ;
+NUM_RANDOM_FILES=$(echo $NUM_RANDOM_FILES) ;
+
+echo;
+echo "<<<<<<<< NUM_FILES: $NUM_FILES // NUM_RANDOM_FILES: $NUM_RANDOM_FILES // HOW_MANY_FILES_TO_SELECT: $HOW_MANY_FILES_TO_SELECT >>>>>>>>>" ;
+echo ;
+
+MY_COMMAND="ls -1 *.* | awk 'NR == 1 || NR % $NUM_RANDOM_FILES == 0'" ;
+echo "MY_COMMAND: $MY_COMMAND" ;
+
+## ACTUAL COMMAND DOING ALL THE MAGIC
+echo "=======> EQUIDISTANT: NOW COPYING $HOW_MANY_FILES_TO_SELECT FILES (--TO-->) $TMP_DIR" ; echo ;
+cp $(ls -1 *.* | awk "NR == 1 || NR % $NUM_RANDOM_FILES == 0") $TMP_DIR/ ;
+
+###########################################################
+
+###########################################################
+## 2. COMMAND BLOCK TO COPY TOTALLY RANDOM FILES
+MY_COMMAND_RANDOM="ls -1 *.* | gshuf -n$HOW_MANY_FILES_TO_SELECT" ;
+echo "MY_COMMAND_RANDOM: $MY_COMMAND_RANDOM" ;
+
+## ACTUAL COMMAND DOING ALL THE MAGIC
+echo "=======> TOTALLY RANDOM: NOW COPYING $HOW_MANY_FILES_TO_SELECT FILES (--TO-->) $TMP_DIR" ; echo ;
+cp $(ls -1 *.* | gshuf -n$HOW_MANY_FILES_TO_SELECT) $TMP_DIR_RANDOM/ ;
+###########################################################
+
+## FINALLY OPENING PWD (works on MAC OS)
+echo "Opening ... $PWD ..."
+open $PWD ; echo ;
---
+++ 999-make-imagemagick-collage-vertically-and-horizontally.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+######################################################
+## THIS PROGRAM MAKES TWO COLLAGES FROM COMMAND LINE INPUTS
+## USING IMAGEMAGICK
+## MADE ON: Friday OCTOBER 5, 2018
+## MADE BY: PALI
+echo "THIS PROGRAM MAKES TWO COLLAGES FROM COMMAND LINE INPUTS USING IMAGEMAGICK" ; echo ;
+######################################################
+
+PWD=$(pwd);
+TMP_DIR="_TMP_COLLAGE_DIR_CONCATENATE" ;
+
+cd $PWD ;
+echo "Current working directory: $PWD " ;
+
+mkdir $TMP_DIR ;
+echo "DONE: Temporary directory made = $TMP_DIR " ;
+
+#######################################################
+echo "LIST OF IMAGES TO USE FOR COLLAGE: " ;
+ls -1 *.*g | nl ;
+#######################################################
+
+echo "Enter collage block WIDTH or HEIGHT (in px): " ;
+read WIDTH ;
+
+echo "Enter collage block PADDING (in px): " ;
+read PADDING ;
+
+NUMBER0=$(ls -1 *.*g | wc -l) ;
+NUMBER=$(echo $NUMBER0 | bc -l ) ;
+echo "TOTAL NUMBER of images to use for collage: $NUMBER" ;
+
+#######################################################
+## REAL MAGIC HAPPENS BELOW
+
+echo; echo "========> MAKING 4 COLLAGES...." ;
+sep="x" ;
+one="1" ;
+
+###########################
+echo "MAKING HORIZONTAL COLLAGES ..." ;
+mkdir _TMP_RESIZE_DIR1 ; mogrify -path _TMP_RESIZE_DIR1/ -resize $sep$WIDTH *.*g ;
+
+montage _TMP_RESIZE_DIR1/*.*g -tile $NUMBER$sep$one -mode concatenate $TMP_DIR/1a_HORZ_COLLAGE.jpg ;
+montage _TMP_RESIZE_DIR1/*.*g -tile $NUMBER$sep$one -mode concatenate -geometry ^$sep$WIDTH+$PADDING+$PADDING $TMP_DIR/1b_HORZ_COLLAGE_EQUAL_WIDTH.jpg ;
+
+echo "COMMAND USED: montage _TMP_RESIZE_DIR1/*.*g -tile $NUMBER$sep$one -mode concatenate -geometry $sep$WIDTH+$PADDING+$PADDING $TMP_DIR/1b_HORZ_COLLAGE_EQUAL_WIDTH.jpg " ;
+###########################
+
+###########################
+echo; echo "MAKING VERTICAL COLLAGES ..." ;
+mkdir _TMP_RESIZE_DIR2 ; mogrify -path _TMP_RESIZE_DIR2/ -resize $WIDTH$sep *.*g ;
+
+montage _TMP_RESIZE_DIR2/*.*g -tile $one$sep$NUMBER -mode concatenate $TMP_DIR/2a_VERT_COLLAGE.jpg ;
+montage _TMP_RESIZE_DIR2/*.*g -tile $one$sep$NUMBER -mode concatenate -geometry $WIDTH$sep+$PADDING+$PADDING $TMP_DIR/2b_VERT_COLLAGE_EQUAL_HEIGHT.jpg ;
+
+echo; echo "COMMAND USED: montage _TMP_RESIZE_DIR2/*.*g -tile $one$sep$NUMBER -mode concatenate -geometry $WIDTH$sep+$PADDING+$PADDING $TMP_DIR/2b_VERT_COLLAGE_EQUAL_HEIGHT.jpg " ;
+###########################
+
+
+#######################################################
+echo;
+echo "IN CASE OF ERRORS: =========> " ;
+echo "1. Make sure that the filename starts with a alphabet, such as a101.jpg/png, etc." ;
+
+
+#######################################################
+echo; echo "Opening $PWD " ;
+open $PWD ;
