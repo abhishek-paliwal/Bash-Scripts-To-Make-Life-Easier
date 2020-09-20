22c9defa6662d3f4de66f046a9f6f47274acae9a
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2020-09-20 19:07:09 +0300

    2x 701 scripts added

---
+++ mggk_bash_scripts/701-mggk-2020/701-script_bash.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+##############################
+
+MY_SCRIPT_PYTHON3="$DIR_GITHUB/Bash-Scripts-To-Make-Life-Easier/mggk_bash_scripts/701-mggk-2020/701-script_python.py" ;
+
+## Assigning working directory and going to it
+WORKDIR="$HOME_WINDOWS/Desktop/Y" ;
+cd $WORKDIR ;
+
+##
+## Removing all temporary files in Working directory
+rm $WORKDIR/_tmp_*.*
+##
+for x in *.json ;
+do
+    step1_json_file_input="_tmp_step1-$x" ;
+    step1_json_file_output="_tmp_step2-$x" ;
+    ####
+    cat $x | sed 's/\\"/"/g' | tr -d "\n\r" > $step1_json_file_input ;
+    cat $step1_json_file_input ;
+    python3 $MY_SCRIPT_PYTHON3 "$step1_json_file_input" "$step1_json_file_output" ;
+done
+
+##
+echo;
+echo "##---------------------------------------------" ;
+echo ">>>> PRINTING WORD COUNT IN ALL FILES - JSON: " ;
+echo "##---------------------------------------------" ;
+wc *.json
+echo "##---------------------------------------------" ;
+echo ">>>> PRINTING WORD COUNT IN ALL FILES - YAML: " ;
+echo "##---------------------------------------------" ;
+wc *.yaml
+##
---
+++ mggk_bash_scripts/701-mggk-2020/701-script_python.py
@@ -0,0 +1,97 @@
+import sys
+import yaml 
+import json 
+
+
+############ JSON 
+
+myfile_json = sys.argv[1] ;
+print ('>>> FILE = ' + myfile_json)
+
+with open(myfile_json,"r") as json_file:
+    data = json.load(json_file)
+
+## json_validate
+print(data) ;
+
+###############################
+###############################
+## Remove some unwanted elements from json
+ELEMENTS_TO_DELETE_FROM_JSON=("@context", "@type", "name", "image", "description", "datePublished", "author", "video" ,"url") ;
+##
+JSON_FILE_WITH_DELETED_ELEMENTS = sys.argv[2] ; 
+##
+with open(JSON_FILE_WITH_DELETED_ELEMENTS, 'w') as dest_file:
+    with open(myfile_json, 'r') as source_file:
+        for line in source_file:
+            #print(line.strip())
+            element = json.loads(line.strip())
+            ##
+            for delete_this in ELEMENTS_TO_DELETE_FROM_JSON:    
+                if delete_this in element:
+                    del element[delete_this]
+            ####
+            print (""); print('+++++++++++++++ NEW JSON DATA +++++++++++++++++++') ; print (""); 
+            print(json.dumps(element)) ;
+            ####        
+            dest_file.write(json.dumps(element))
+
+#################################
+#################################
+
+############ YAML 
+myfile_yaml = JSON_FILE_WITH_DELETED_ELEMENTS + '.yaml'
+##
+with open(JSON_FILE_WITH_DELETED_ELEMENTS,"r") as json_file:
+    newdata = json.load(json_file)
+    my_dict = newdata
+
+## json data formatting and taking only relevant fields
+def iterate_multidimensional(my_dict):
+    for k,v in my_dict.items():
+        if(isinstance(v,dict)):
+            print();
+            print(k+":::")
+            iterate_multidimensional(v)
+            continue
+        print(k+": "+str(v))
+
+iterate_multidimensional(my_dict)
+
+my_dict1 = newdata['recipeIngredient']
+print(my_dict1)
+for x in my_dict1:
+    print('==> ' + x)
+
+
+#############
+recipeInstructions = newdata['recipeInstructions']
+print(recipeInstructions)
+print("////////////////////////////////////////////////") ;
+for HowToSection in recipeInstructions:
+    #iterate_multidimensional(HowToSection)
+    nameOfSection = HowToSection['name']
+    print('XXXX ' + nameOfSection)
+    itemListElement = HowToSection['itemListElement']
+    for HowToStep in itemListElement:
+        #print(HowToStep) ; print() ;
+        HowToStepText = HowToStep['text']
+        print('======> ' + HowToStepText); print() ;
+
+    
+
+
+## DUMPING ALL JSON DATA TO YAML FILE
+#data_yaml = yaml.dump({"prepTime" : newdata['prepTime'], "cookTime" : newdata['cookTime']})
+data_yaml = yaml.dump(newdata)
+
+## yaml data validate
+yaml.safe_load(data_yaml)
+##
+f = open(myfile_yaml, "w")
+f.write(data_yaml)
+f.close()
+
+## yaml2json_pretty
+#print(json.dumps(yaml.safe_load(data_yaml), indent=2, sort_keys=False))
+
