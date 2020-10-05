8bf55510f3b299f5b658bb7024b043d2a1aa8893
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2020-10-05 19:16:28 +0300

    1x 703 bash script file added

---
+++ mggk_bash_scripts/703-mggk-2020-DELETE-RECIPE-JSON-HTML-VIDEO-BLOCKS-from-md-files.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## CREATING SCRIPT USAGE FUNCION AND CALLING IT VIA '--help'
+usage()
+{
+cat <<EOM
+USAGE: $(basename $0)
+    #############################################################################
+    ## THIS PROGRAM READS ALL MD FILES FROM A GIVEN CONTENT DIRECTORY AND DELETES
+    ## RECIPE JSON + HTML + HUGO VIDEO-SHORTCODES FROM EACH ONE OF THEM.
+    ## THESE BLOCKS ARE:
+    ## ... mggk_json_recipe JSON block
+    ## ... {{< mggkrecipeHTMLcode >}} HTML recipe block
+    ## ... {{< mggk-youtube-video-embed >}} HUGO VIDEO SHORTCODE
+    #############################################################################
+    ## CODED ON: October 5, 2020
+    ## BY: Pali
+    #############################################################################
+EOM
+
+exit 0 ## EXITING IF ONLY USAGE IS NEEDED
+}
+## Calling the usage function
+if [ "$1" == "--help" ] ; then usage ; fi
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+##################################################################################
+## CHOOSE YOUR CORRECT CONTENT DIRECTORY BEFORE RUNNING THIS SCRIPT
+##################################################################################
+
+#CONTENT_DIR="$DIR_GITHUB/ZZ-HUGO-TEST/content/top-01-50" ;
+#CONTENT_DIR="$DIR_GITHUB/ZZ-HUGO-TEST/content/top-51-100" ;
+#CONTENT_DIR="$DIR_GITHUB/ZZ-HUGO-TEST/content/top-101-150" ;
+#CONTENT_DIR="$DIR_GITHUB/ZZ-HUGO-TEST/content/top-151-200" ;
+#CONTENT_DIR="$DIR_GITHUB/ZZ-HUGO-TEST/content/top-201-300" ;
+#CONTENT_DIR="$DIR_GITHUB/ZZ-HUGO-TEST/content/top-301-400" ;
+#CONTENT_DIR="$DIR_GITHUB/ZZ-HUGO-TEST/content/top-401-500" ;
+#CONTENT_DIR="$DIR_GITHUB/ZZ-HUGO-TEST/content/top-501-END" ;
+CONTENT_DIR="$DIR_GITHUB/ZZ-HUGO-TEST/content/top-01-50" ;
+
+PWD="$HOME_WINDOWS/Desktop/Y" ;
+##################################################################################
+
+cd $PWD ;
+echo ;
+echo "###########################################" ;
+echo "Present working directory = $PWD" ;
+echo ">> Make REALLY sure that your chosen content directory is OKAY => $CONTENT_DIR" ;
+echo "###########################################" ;
+
+## USER CONFIRMATION
+read -p ">>>> Check if CONTENT DIRECTORY is OKAY. Press ENTER to continue ..."
+
+##################################################################################
+BASENAME_CONTENTDIR=$(basename $CONTENT_DIR) ;
+DIR_COPY="$PWD/_original_copied_$BASENAME_CONTENTDIR" ;
+mkdir $DIR_COPY
+
+##################################################################################
+for x in $CONTENT_DIR/* ; do 
+    echo; echo "READING CURRENT FILE => $x" ; 
+    file_basename="$(basename $x)" ;
+    file_copied="$DIR_COPY/$file_basename"
+
+    ## COPYING THE ORIGINAL MD FILE TO OUR PWD
+    cp $x $file_copied ;
+    echo "  >> ORIGINAL FILE COPIED AS => $file_copied" ;
+
+    ##################################################################################
+    ## DELETING ALL THE UNWANTED JSON + HTML + VIDEO-SHORTCODE ONE BY ONE
+    ##################################################################################
+
+    ## Delete mggk json recipe block from the frontmatter
+    echo "  >> DELETING = mggk json recipe JSON CODE in frontmatter" ;
+    sed '/mggk_json_recipe/,/BLOCK ABOVE THIS -->/d' $file_copied > _tmp1
+
+    ## Delete recipe html code from the main content
+    echo "  >> DELETING = mggk recipe HTML CODE from the main content" ;
+    sed '/{{< mggkrecipeHTMLcode >}}/,/{{< \/mggkrecipeHTMLcode >}}/d' _tmp1 > _tmp2
+
+    ## Delete mggk youtube shortcode 
+    echo "  >> DELETING =  mggk youtube shortcode " ;
+    sed '/{{< mggk-youtube-video-embed >}}/,/$/d' _tmp2 > $file_basename
+    ##################################################################################
+done 
+
+##################################################################################
+## PRINTING NEXT STEPS
+##################################################################################
+echo "##------------------------------------------------------------------------------" ;
+echo ">>>> NEXT STEPS: " ;
+echo "      If everything is okay so far, copy all the final files and replace them with the existing md files present in the original hugo directory." ;
+echo "      To do that, run this command => cp _TOCHECK*.md $CONTENT_DIR/"
+echo "##------------------------------------------------------------------------------" ;
