2a79f0d916a36eb925aa950368bff62af3eee0cd
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2020-10-10 18:13:23 +0300

    Update 999-mggk-MOVE-MD-FILES-to-PWD-based-on-URLs-from-CLI-text-file-argument.sh

--- mggk_bash_scripts/999-mggk-MOVE-MD-FILES-to-PWD-based-on-URLs-from-CLI-text-file-argument.sh
+++ mggk_bash_scripts/999-mggk-MOVE-MD-FILES-to-PWD-based-on-URLs-from-CLI-text-file-argument.sh
@@ -2,7 +2,7 @@
 
 ##################################################################################
 ## SOME VARIABLES (CHANGE THE SOURCE DIR IF NEEDED)
-#SOURCE_DIR="$DIR_GITHUB/ZZ-HUGO-TEST/content/top-501-END" ;
+#SOURCE_DIR="/Users/abhishek/GitHub/2019-HUGO-MGGK-WEBSITE-OFFICIAL/content/_FIXED/top-01-50" ;
 SOURCE_DIR="$DIR_GITHUB/2019-HUGO-MGGK-WEBSITE-OFFICIAL/content/594x" ;
 DIR_TO_MOVE="$HOME_WINDOWS/Desktop/Y/_TMP_md_files_moved-$(basename $SOURCE_DIR)" ;
 ##################################################################################
