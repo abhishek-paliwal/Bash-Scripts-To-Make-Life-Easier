f5cf8189ffd6a966b9a3e689c6726c3185b791b0
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2020-10-21 18:22:47 +0300

    Update 999-mggk-EXTRACT-VIDEO-SCREENSHOTS-EVERY-N-SECONDS.sh

--- mggk_bash_scripts/999-mggk-EXTRACT-VIDEO-SCREENSHOTS-EVERY-N-SECONDS.sh
+++ mggk_bash_scripts/999-mggk-EXTRACT-VIDEO-SCREENSHOTS-EVERY-N-SECONDS.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 ##################################################################################
-## SOME VARIABLES (CHANGE THE DIR IF NEEDED)
+## SOME VARIABLES (CHANGE THE DIRs IF NEEDED)
 WORKDIR="$HOME_WINDOWS/Desktop/Y" ;
+DIR_WHICH_YOUTUBE_VIDEOS="/Users/abhishek/GitHub/ZZ-HUGO-TEST/content/popular-posts" ;
 ##################################################################################
 
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -24,6 +25,12 @@ USAGE: $(basename $0)
   ## ARE NOT EMPTY.
   #### For renaming, it uses the 'rename' command. If your system does not have it ...
   #### Install rename command using homebrew by running > brew install raname 
+  ###########################################
+  ## IF YOU CHOOSE OPTION 3 WHEN ASKED FOR USER INPUT => 
+  ## THIS PROGRAM DOWNLOADS ALL VIDEOS FROM YOUTUBE FOUND IN AN ALREADY SPECIFIED DIRECTORY.
+  ## CURRENT SPECIFIED DIRECTORY = $DIR_WHICH_YOUTUBE_VIDEOS
+  ## IF NEEDED, CHANGE SPECIFIED DIRECTORY BY CHANGING THIS
+  ## VARIABLE => \$DIR_WHICH_YOUTUBE_VIDEOS.
   ###############################################################################
   ## Coded by: PALI
   ## On: October 13, 2020
@@ -92,6 +99,24 @@ function FUNCTION_step2_rename_steps_thumbnails () {
     ##--------------------------------------------------------------------
     done
 }
+##
+function FUNCTION_step3_download_videos_using_youtube-dl_program () {
+    echo "RUNNING STEP 3 = Downloading + Renaming Videos in chosen DIR_WHICH_YOUTUBE_VIDEOS = $DIR_WHICH_YOUTUBE_VIDEOS ..." ; echo; 
+    for mymdfile in $(grep -irl 'youtube_video_id:' $DIR_WHICH_YOUTUBE_VIDEOS/ ) ; do 
+        ## Extracting url without slashes to use for filename later
+        url_var=$(grep -irh 'url:' $mymdfile | tr -d ' ' | sed 's/url://g' | sed 's+/++g') ;
+        ## Extracting youtube video id without quotes
+        youtube_id=$(grep -irh 'youtube_video_id:' $mymdfile | tr -d ' ' | sed 's/youtube_video_id://g' | sed 's+/++g' | sed 's+"++g') ;
+        ## PRINTING
+        echo; echo "YOUTUBE_ID = $youtube_id // URL_VAR= $url_var" ;
+        ## DOWNLOADING YOUTUBE VIDEO USING youtube-dl program
+        youtube-dl --id $youtube_id ;
+        ## RENAMING YOUTUBE VIDEO WITH URL_VAR
+        mv $youtube_id.mkv  $url_var.mkv ;
+        mv $youtube_id.mp4  $url_var.mp4 ;
+        echo; echo ">> VIDEO FILE RENAMED AS // IF MKV => $url_var.mkv // IF MP4 => $url_var.mp4" ; 
+    done 
+}
 ##################################################################################
 
 ## CALLING THE FUNCTIONS, DEPENDING UPON THE OPTION CHOSEN BY THE USER
@@ -99,6 +124,7 @@ echo; echo;
 echo "What do you want to do? Select your option =======>
 1) FUNCTION_step1_video_thumbnails_extraction
 2) FUNCTION_step2_rename_steps_thumbnails
+3) FUNCTION_step3_download_videos_using_youtube-dl_program (from this DIR = $DIR_WHICH_YOUTUBE_VIDEOS)
 " ;
 
 echo "Enter your choice (1/2): " ;
@@ -112,6 +138,10 @@ elif [ "$which_function_to_run" == "2" ]; then
     echo ">> Your chosen step is: $which_function_to_run" ;
     echo ">> Hence, this will run => FUNCTION_step2_rename_steps_thumbnails" ;
     FUNCTION_step2_rename_steps_thumbnails ;
+elif [ "$which_function_to_run" == "3" ]; then
+    echo ">> Your chosen step is: $which_function_to_run" ;
+    echo ">> Hence, this will run => FUNCTION_step3_download_videos_using_youtube-dl_program" ;
+    FUNCTION_step3_download_videos_using_youtube-dl_program ;
 else 
     echo ">> Your chosen step is INVALID = $which_function_to_run // TRY AGAIN." ;
 fi
