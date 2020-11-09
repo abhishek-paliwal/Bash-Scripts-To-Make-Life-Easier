39119924f9306c8eb847c0c99c3d9763c72f948c
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2020-11-09 14:37:41 +0200

    Update 505A-mggk-custom-newsletter-creation-from-hyperlinks-text-file.sh

--- mggk_bash_scripts/505-mggk-rss-to-email-using-xml-feed/505A-mggk-custom-newsletter-creation-from-hyperlinks-text-file.sh
+++ mggk_bash_scripts/505-mggk-rss-to-email-using-xml-feed/505A-mggk-custom-newsletter-creation-from-hyperlinks-text-file.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 THIS_SCRIPT_NAME="$(basename $0)" ;
+##################################################################################
+## CD to temporary directory on desktop
+PWD="$DIR_Y" ;
+cd $PWD ;
+echo ">>>> PWD is $PWD" ;
+##
+NEWSLETTER_TEMPLATE="$PWD/505A-newsletter-template.txt" ;
+REQUIREMENTS_FILE="$NEWSLETTER_TEMPLATE" ;  ## first text file
+REQUIREMENTS_FILE_BASENAME="$(basename $REQUIREMENTS_FILE)" ; 
+##################################################################################
+
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ## CREATING SCRIPT USAGE FUNCION AND CALLING IT VIA '--help'
 usage()
@@ -7,13 +18,17 @@ usage()
 cat <<EOM
 USAGE: $(basename $0)
 	###############################################################################
-	## THIS PROGRAM READS TEXT FILES IN PWD CONTAINING HYPERLINKS AND CREATES 
-  ## CUSTOM NEWSLETTER BY CREATING A HTML FILE READY TO BE SENT THRU EMAIL TO
-  ## EMAIL SUBSCRIBERS, AFTER COPYING AND PASTING ITS SOURCE CODE INTO ANY
-  ## EMAIL PROGRAM (SENDY, MAILCHIMP, ETC.)
+	## THIS PROGRAM READS A NEWSLETTER TEMPLATE FILE IN PWD CONTAINING HYPERLINKS AND
+  ## TEXT CONTENT. IF THIS FILE IS NOT FOUND, THE IT AUTOCREATES IT FOR THE 
+  ## USER TO FILL, AND THEN THE USER NEEDS TO RUN THIS SCRIPT AGAIN. 
+  #### IT THEN CREATES A 
+  #### CUSTOM NEWSLETTER HTML FILE READY TO BE SENT THRU EMAIL TO
+  #### EMAIL SUBSCRIBERS, AFTER COPYING AND PASTING ITS SOURCE CODE INTO ANY
+  #### EMAIL PROGRAM (SENDY, MAILCHIMP, ETC.)
   ##############################################################################
-  ## REQUIREMENTS => 
-  ## THIS SCRIPT NEEDS A TEXT FILE IN PWD, CONTAINING HYPERLINKS.
+  ## REQUIREMENTS => $REQUIREMENTS_FILE
+  ## THIS SCRIPT NEEDS A NEWSLETTER TEMPLATE FILE IN PWD, CONTAINING HYPERLINKS
+  ## AND TEXT CONTENT. 
 	##############################################################################
 	## MADE BY: PALI
 	## DATE: NOV 01, 2020
@@ -27,18 +42,40 @@ if [ "$1" == "--help" ] ; then usage ; fi
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 ##################################################################################
-## CD to temporary directory on desktop
-PWD="$HOME_WINDOWS/Desktop/Y"
-cd $PWD ;
-echo ">>>> PWD is $PWD" ;
 
+## CHECK WHETHER THE REQUIRED NEWSLETTER TEMPLATE FILE ALREADY EXISTS
+## IF NOT, AUTOMATICALLY CREATE IT.
+if [ -f "$NEWSLETTER_TEMPLATE" ]; then
+    echo ">> SUCCESS: NEWSLETTER TEMPLATE FILE EXISTS => $NEWSLETTER_TEMPLATE"
+else
+    echo ">> FAILURE: NEWSLETTER TEMPLATE FILE DOES NOT EXIST. IT HAS BEEN CREATED FOR YOU." ;
+    echo ">>>> NEXT STEP: Edit this file with your TEXT CONTENT and LINKS, then run this program again. => $NEWSLETTER_TEMPLATE" ;
+    ## CREATING AN EMPTY NEWSLETTER TEMPLATE TO BE FILLED IN
+echo "##begin_links
+https://www.mygingergarlickitchen.com/LINK-1/
+https://www.mygingergarlickitchen.com/LINK-2/
+https://www.mygingergarlickitchen.com/LINK-3/
+##end_links
+##begin_content
+CONTENT LINE 1
+CONTENT LINE 2
+CONTENT LINE 3
+##end_content" > $NEWSLETTER_TEMPLATE ;
+    ## EXIT THIS SCRIPT IN THIS CASE
+    exit 1 ;
+fi
+
+###############################################################################
 ## DEFINE SOME VARIABLE FILENAMES
-REQUIREMENTS_FILE="$(ls -1 $PWD/*.txt | head -1)" ;  ## first text file
-REQUIREMENTS_FILE_BASENAME="$(basename $REQUIREMENTS_FILE)" ; 
 LINKS_FILE_OUTPUT="$PWD/_TMP_MYLINKS.txt" ;
 OUTPUT_HTML_FILE="$PWD/_TMP_505A_NEWSLETTER_FINAL_OUTPUT-$REQUIREMENTS_FILE_BASENAME.html"
 TMP_CURL_FILE="$PWD/_TMP_mycurlfile.txt" ;
 MD_FILENAME="$PWD/_TMP-$REQUIREMENTS_FILE_BASENAME.md";
+##
+## Using the power of sed to find all text between two phrases
+echo ">> Dumping all text found between the required headings" ;
+sed -n "/##begin_links/,/##end_links/p" $REQUIREMENTS_FILE | grep -v '#' > $REQUIREMENTS_FILE_BASENAME-links.txt
+sed -n "/##begin_content/,/##end_content/p" $REQUIREMENTS_FILE | grep -v '#' > $REQUIREMENTS_FILE_BASENAME-content.txt
 ###############################################################################
 
 ########### CREATING THE TMP LINKS FILES #############
@@ -46,7 +83,7 @@ MD_FILENAME="$PWD/_TMP-$REQUIREMENTS_FILE_BASENAME.md";
 ## CHANGING THE INPUT FILE AND REMOVING SOME CONTENT AND SAVING TO OUTPUT FILE
 ## ONLY THOSE LINES ARE SAVED WHICH HAVE THE WORD 'HTTPS' IN THEM, CONSIDERING ONLY
 ## UNIQUE LINES. UNIQ ONLY WORKS WHEN THE LINES ARE SORTED FIRST. SO WE WILL SORT FIRST.
-cat $REQUIREMENTS_FILE | sed 's/\[.*\]//g' | sed 's/(//g' | sed 's/)//g' | grep -i 'https' | uniq > $LINKS_FILE_OUTPUT
+cat $REQUIREMENTS_FILE_BASENAME-links.txt | sed 's/\[.*\]//g' | sed 's/(//g' | sed 's/)//g' | grep -i 'https' | uniq > $LINKS_FILE_OUTPUT
 
 ## PRINTING URLs BEFORE AND AFTER REMOVING DUPLICATES
 echo; echo ">> PRINTING URLs BEFORE REMOVING DUPLICATES >>" ;
@@ -62,20 +99,19 @@ echo "<!doctype html>
     <!-- Required meta tags -->
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
-
     <!-- Bootstrap CSS -->
     <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' crossorigin='anonymous'>
 		<title>Latest delicious recipes from My Ginger Garlic Kitchen</title>
 	</head>
-	<body>
-  
+	<body>  
   <div class='container'>" > $OUTPUT_HTML_FILE ; ## Initializing the HTML file
 
-echo "<p style='font-size: 18px'; >Hello [Name,fallback=]</p>
-<p style='font-size: 18px'; ><strong>🍴Here are the latest recipes from <a href='https://www.mygingergarlickitchen.com/' target='_blank'>our blog</a> and recipe videos from <a href='https://www.youtube.com/user/anupamabhishek/' target='_blank'>our youtube channel</a> directly to your inbox.</strong></p>
-<p style='font-size: 18px'; >Simply click the buttons below to watch the how-to videos and to download recipes.</p>" >> $OUTPUT_HTML_FILE ;
+  echo "<p style='font-size: 18px'; >Hello [Name,fallback=]</p>" >> $OUTPUT_HTML_FILE ;
 
-echo "<p style='font-size: 18px;'>I hope you will love them.<br><br>Best wishes,</p>
+## INSERTING TEXT CONTENT TO THE OUTPUT
+cat $REQUIREMENTS_FILE_BASENAME-content.txt | grep -v '^$' | sed "s+^+<p style='font-size: 18px'; >+g" | sed 's+$+</p>+g' >> $OUTPUT_HTML_FILE ;
+
+echo "<p style='font-size: 18px;'>Best wishes,</p>
 <p style='font-size: 18px;'><img align='none' height='35' src='https://gallery.mailchimp.com/126a7c0d8271b7ec930c15e67/images/623b3f2b-7d5f-426d-857a-2c3175fdd285.png' style='width: 110px; height: 35px;'></p>" >> $OUTPUT_HTML_FILE ;
 
 echo "<p style='font-size: 18px; text-align: center; color: #cd1c62; '>&bull; &bull; &bull; &bull; &bull; &bull; &bull; &bull;</p>" >> $OUTPUT_HTML_FILE ;
@@ -170,8 +206,7 @@ echo "<!-- Optional JavaScript -->
 <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' crossorigin='anonymous'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' crossorigin='anonymous'></script>
 <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' crossorigin='anonymous'></script>
-</body>
-</html>" >> $OUTPUT_HTML_FILE ;
+</body></html>" >> $OUTPUT_HTML_FILE ;
 
 ##########################################
 ## Replacing strange characters to formal html symbols and saving to final html file
