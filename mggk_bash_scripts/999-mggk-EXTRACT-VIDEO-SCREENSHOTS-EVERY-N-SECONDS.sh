9f1fee5b78aeb29308ca546e8791fb17457261dd
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2020-11-30 14:01:37 +0200

    Update 999-mggk-EXTRACT-VIDEO-SCREENSHOTS-EVERY-N-SECONDS.sh

--- mggk_bash_scripts/999-mggk-EXTRACT-VIDEO-SCREENSHOTS-EVERY-N-SECONDS.sh
+++ mggk_bash_scripts/999-mggk-EXTRACT-VIDEO-SCREENSHOTS-EVERY-N-SECONDS.sh
@@ -3,8 +3,8 @@
 ##################################################################################
 ## SOME VARIABLES (CHANGE THE DIRs IF NEEDED)
 WORKDIR="$HOME_WINDOWS/Desktop/Y" ;
-#DIR_WHICH_YOUTUBE_VIDEOS="/home/ubuntu/GitHub/2020-LEELA-RECIPES/content" ;
-DIR_WHICH_YOUTUBE_VIDEOS="$DIR_Y" ;
+DIR_WHICH_YOUTUBE_VIDEOS="/Users/abhishek/GitHub/2019-HUGO-MGGK-WEBSITE-OFFICIAL/content/_FIXED/top-101-150" ;
+#DIR_WHICH_YOUTUBE_VIDEOS="$DIR_Y" ;
 ##################################################################################
 
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
