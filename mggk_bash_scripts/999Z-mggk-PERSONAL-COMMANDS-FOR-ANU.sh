8eb8e6e1d3fa91f2a4d766a2a0b5400abf6ca019
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2021-06-04 13:12:24 +0300

    1x updated

--- mggk_bash_scripts/999Z-mggk-PERSONAL-COMMANDS-FOR-ANU.sh
+++ mggk_bash_scripts/999Z-mggk-PERSONAL-COMMANDS-FOR-ANU.sh
@@ -153,7 +153,6 @@ leelasrecipes_calculate_length_of_titles
 leelasrecipes_calculate_length_of_urls
 999-mggk-EXTRACT-VIDEO-SCREENSHOTS-EVERY-N-SECONDS
 leelasrecipes_make_20_directories
-pali_imagemagick_fixed_resize_images_by_width_or_height
 2000-collage-maker-using-vendor-script-leelasrecipes
 2000-python-image-resize) ;
 
