ca35abd462a97a306de0c6946db8b4cccbceab28
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2021-06-17 00:04:13 +0300

    Create 999-mggk-CREATE-PDF-FILES-FROM-RECIPE-MD-FILES.py

---
+++ mggk_bash_scripts/999-mggk-CREATE-PDF-FILES-FROM-RECIPE-MD-FILES.py
@@ -0,0 +1,323 @@
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## USAGE FOR PYTHON
+## Print this help as >> this_script_name --help
+## CREATING SCRIPT USAGE FUNCION AND CALLING IT VIA '--help'
+import sys
+####
+def usage():
+    print('## USAGE: ' + sys.argv[0])
+    HELP_TEXT = """
+    ##############################################################################
+    ## THIS PYTHON SCRIPT PARSES THE YAML FRONTMATTER METADATA FROM HUGO MARKDOWN FILES.
+    ## FOUND BY PARSING ALL THE MARKDOWN FILES IN ROOTDIR
+    ## THEN CREATES PDF FILES FOR ALL THE RECIPE VALID RECIPE FILES AND SAVES THEM
+    ## TO APPROPRIATE DIRECTORY.
+    #######################################
+    ## IMPORTANT NOTE:
+    ## This program needs these python packages => python-frontmatter + fpdf
+    ## FRONTMATTER: Install: from: https://github.com/eyeseast/python-frontmatter
+    ## FPDF: Tutorial: https://pyfpdf.readthedocs.io/en/latest/Tutorial/index.html
+    #######################################
+    ## MADE ON: JUNE 16 2021
+    ## BY: PALI
+    ##############################################################################
+    """
+    print(HELP_TEXT)
+####
+## Calling the usage function
+## First checking if there are more than one argument on CLI .
+print()
+if (len(sys.argv) > 1) and (sys.argv[1] == "--help"):
+    print('## USAGE HELP IS PRINTED BELOW. SCRIPT WILL EXIT AFTER THAT.')
+    usage()
+    ## EXITING IF ONLY USAGE IS NEEDED
+    quit()
+else:
+    print('## USAGE HELP IS PRINTED BELOW. NORMAL PROGRAM RUN WILL CONTINE AFTER THAT.')
+    usage()  # Printing normal help and continuing script run.
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+##############################################################################
+##############################################################################
+
+import frontmatter
+import io
+import os
+from os.path import basename, splitext
+import glob
+import sys
+## Tutorial here: https://pyfpdf.readthedocs.io/en/latest/Tutorial/index.html
+from fpdf import FPDF
+
+
+##############################################################################
+## WHERE ARE THE FILES TO MODIFY
+MYHOME = os.environ['HOME'] ## GETTING THE ENVIRONMENT VALUE FOR HOME
+ROOTDIR = MYHOME + "/GitHub/2019-HUGO-MGGK-WEBSITE-OFFICIAL/content/"
+PDFDIR = MYHOME + "/GitHub/2019-HUGO-MGGK-WEBSITE-OFFICIAL/static/PDF-PRINTS/"
+## printing all filenames found in ROOTDIR
+for filename in glob.iglob(ROOTDIR + '**/*.md', recursive=True):
+    print(filename)
+##############################################################################
+
+## CREATING A STRING WHICH WILL BE APPENDED LATER
+## FIRST INITIALIZING THAT STRING WITH THE COLUMNS NAME HEADERS
+FINAL_ALL_RECIPE_ROWS=str( "URL,RECIPE_TITLE,RECIPE_AUTHOR,RECIPE_DATE,RECIPE_FEATURED_IMAGE,RECIPE_FILENAME,RECIPE_DESCRIPTION" )
+
+print("\n\n////////////////////// REAL MAGIC HAPPENS BELOW //////////////////\n\n" )
+
+# LOOPING THROUGH ALL MARKDOWN FILES
+for fname in glob.iglob(ROOTDIR + '**/*.md', recursive=True):
+    with io.open(fname, 'r') as f:
+        ####################################
+        # Parse file's front matter
+        post = frontmatter.load(f)
+        ####################################
+        ## CHECK WHETHER THIS FILE IS A RECIPE FILE
+        ## WE'LL CHECK IF PREPTIME TAG IS PRESENT IN ALL YAML TAGS THUS FOUND
+        if 'prepTime' not in str(sorted(post.keys())) :
+            print('=> NOT A VALID RECIPE FILE ... Skipping this, and moving onto the next file ...')
+            print() ;
+            continue;
+        ####################################
+        ######################################################################
+        ## ASSIGN NEW VALUES IF NO VALUES FOR KEYS ARE FOUND
+        #MYKEYS = ["title","author","date","url","featured_image","yoast_description","youtube_video_id","mggk_json_recipe","tags","categories"]
+
+        MYKEYS = ['aggregateRating', 'author', 'categories', 'cookTime', 'date', 'featured_image', 'first_published_on', 'mggk_json_recipe', 'my_custom_variable', 'nutrition', 'prepTime', 'recipeCategory', 'recipeCuisine', 'recipeIngredient', 'recipeInstructions', 'recipeNotes', 'recipeYield', 'recipe_code_image', 'recipe_keywords', 'steps_images_present', 'tags', 'title', 'totalTime', 'type', 'url', 'yoast_description', 'youtube_video_id']
+
+        for p in MYKEYS:
+            if post.get(p) == None:
+                post[p] = ['ZZZZ - NOTHING FOUND for ' + p]
+        ######################################################################
+
+        ## PRINTING PRIMARY STUFF
+        print("\n#########################\n") ## prints line
+        print("FILENAME: " + fname) ## printing filename
+        print("SORTED-KEYS-FOUND: " + str(sorted(post.keys()))) ## prints all metadata keys
+
+
+        ## ASSIGNING VARIABLES
+        FILENAME=str(fname)        
+        TITLE=str(post['title'])
+        AUTHOR=str(post['author'])
+        DATE=str(post['date'])
+        FEATURED_IMAGE=str(post['featured_image'])
+        YOAST_DESCRIPTION=str(post['yoast_description'])
+        YOUTUBE_VIDEO_ID=str(post['youtube_video_id'])
+        CATEGORIES=str(post['categories'])
+        TAGS=str(post['tags'])
+        MGGK_JSON_RECIPE=str(post['mggk_json_recipe'])
+        
+        ## TIMES
+        PREPTIME=str(post['prepTime'])
+        COOKTIME=str(post['cookTime'])
+        TOTALTIME=str(post['totalTime'])
+        
+        ## NUTRITION INFO
+        NUTRITION=post['nutrition']
+        RECIPE_YIELD=str(post['recipeYield'])
+        RECIPE_CUISINE=str(post['recipeCuisine'])   
+        RECIPE_CATEGORY=str(post['recipeCategory'])
+        
+        ## INGREDIENTS AND INSTRUCTIONS
+        RECIPE_INGREDIENTS=post['recipeIngredient']
+        RECIPE_INSTRUCTIONS=post['recipeInstructions']
+        RECIPE_NOTES=post['recipeNotes']
+
+        ## URL FORMATTING
+        URL = str(post['url'])
+        URL_NO_SLASHES = URL.replace("/", "") ## replace all slashes
+        URL_MGGK = str("https://www.mygingergarlickitchen.com" + URL )
+
+
+
+        ## PRINTING METADATA VALUES + OTHER STUFF
+        print("TITLE: " + TITLE ) ## Prints title metadata value from yaml frontmatter
+        print("AUTHOR: " + AUTHOR ) ## Prints author
+        print("DATE: " + DATE ) ## Prints date
+        print("URL: " + URL ) ## Prints url
+        print("FEATURED_IMAGE: " + FEATURED_IMAGE ) ## Prints featured_image
+        print("YOAST_DESCRIPTION: " + YOAST_DESCRIPTION ) ## Prints yoast_description
+        print("YOUTUBE_VIDEO_ID: " + YOUTUBE_VIDEO_ID ) ## Prints youtube_video_id
+        print("CATEGORIES: " + CATEGORIES ) ## printing cagtegories found in frontmatter
+        print("TAGS: " + TAGS ) ## printing tags found in frontmatter
+        #print("MGGK_JSON_RECIPE: " + MGGK_JSON_RECIPE ) ## Prints mggk_json_recipe
+        ##
+        print("PREPTIME: " + PREPTIME )
+        print("COOKTIME: " + COOKTIME )
+        print("TOTALTIME: " + TOTALTIME )
+        ##
+        print("NUTRITION: " + str(NUTRITION) )
+        print("RECIPE_YIELD: " + RECIPE_YIELD )
+        print("RECIPE_CUISINE: " + RECIPE_CUISINE )
+        print("RECIPE_CATEGORY: " + RECIPE_CATEGORY )
+        ##
+        print("RECIPE_INGREDIENTS: " + str(RECIPE_INGREDIENTS) )
+        print("RECIPE_INSTRUCTIONS: " + str(RECIPE_INSTRUCTIONS) )
+        print("RECIPE_NOTES: " + str(RECIPE_NOTES) ) 
+    
+        ##
+        print()
+        print()
+        print(type(RECIPE_INGREDIENTS)) ## should be list containing dictionary
+        print(type(RECIPE_INSTRUCTIONS)) ## should be list containing dictionary
+        print(type(RECIPE_NOTES)) ## should be a list
+
+        ##
+        print()
+        for ingr_group in RECIPE_INGREDIENTS:
+            print(type(ingr_group)) ## should be dictionary
+            print(list(ingr_group.keys())) ## printing all keys in dictionary, as list
+            print()
+            print(ingr_group.get("recipeIngredientTitle"))
+            list_ingredients= ingr_group.get("recipeIngredientList")
+            for ingr in ingr_group.get("recipeIngredientList"):
+                print(ingr)
+
+        ##
+        print()
+        for instr_group in RECIPE_INSTRUCTIONS:
+            print(type(instr_group)) ## should be dictionary
+            print(list(instr_group.keys())) ## printing all keys in dictionary, as list
+            print()
+            print(instr_group.get("recipeInstructionsTitle"))
+            list_instructions= instr_group.get("recipeInstructionsList")
+            for instr in instr_group.get("recipeInstructionsList"):
+                print(instr)
+
+        ##
+        print()
+        for single_note in RECIPE_NOTES:
+            print(single_note)
+
+        ##------------------------------------------------------------------------------
+        ## PRINTING ALL DATA TO A PDF FILE
+        ##------------------------------------------------------------------------------
+        PDF_FILENAME = 'PDF-' + URL_NO_SLASHES + '.pdf'
+        pdf = FPDF('P', 'mm', 'A4')
+        effective_page_width = pdf.w - 2*pdf.l_margin ; ## in mm
+        chosen_box_height = 7 ; ## in mm
+        #print(effective_page_width)
+
+        pdf.add_page()
+        pdf.set_font('Arial', 'B', 16)
+
+        ##
+        #print RECIPE URL:
+        pdf.set_font('Arial', '', 12)
+        pdf.cell(effective_page_width, chosen_box_height*2, 'RECIPE Link: ' + URL_MGGK, 1, 1, 'L')
+        pdf.ln(chosen_box_height)
+
+        #print TITLE:
+        pdf.set_font('Times', 'B', 30)
+        ## replace some unreadable characters to question marks
+        TITLE = TITLE.encode('latin-1', 'replace').decode('latin-1')
+        ##
+        pdf.multi_cell(effective_page_width, chosen_box_height*2, TITLE+' [RECIPE]', 0, 1, 'L')
+
+        #print description
+        #through multi_cell => it requires declaring the height of the cell.
+        pdf.set_font('Arial', '', 16)
+        ## replace some unreadable characters to question marks
+        YOAST_DESCRIPTION = YOAST_DESCRIPTION.encode('latin-1', 'replace').decode('latin-1')
+        ##
+        pdf.multi_cell(effective_page_width, chosen_box_height, YOAST_DESCRIPTION, 0, 1, 'L')
+        pdf.ln(chosen_box_height)
+
+        #print PREPTIME, COOKTIME, TOTALTIME
+        pdf.set_font('Arial', 'B', 12)
+        time_fulltext='Prep Time = ' + PREPTIME + ' // Cook Time = ' + COOKTIME + ' // Total Time = ' + TOTALTIME ;
+        pdf.cell(effective_page_width, chosen_box_height, time_fulltext, 0, 1, 'R')
+
+        #print category, cuisine, serves
+        pdf.set_font('Arial', 'B', 12)
+        category_fulltext='Category = ' + RECIPE_CATEGORY + ' // Cuisine = ' + RECIPE_CUISINE + ' // Serves = ' + RECIPE_YIELD ;
+        pdf.cell(effective_page_width, chosen_box_height, category_fulltext, 0, 1, 'R')
+
+        #print calories, serving_size
+        pdf.set_font('Arial', 'B', 12)
+        nutrition_fulltext='Nutrition Info = ' + str( NUTRITION.get("calories") ) + ' // Serving Size = ' + str( NUTRITION.get("servingSize") ) ;
+        pdf.cell(effective_page_width, chosen_box_height, nutrition_fulltext, 0, 1, 'R')
+    
+        ##------------------------------------------------------------------------------
+
+        #print recipe ingredients
+        pdf.ln(chosen_box_height)
+        pdf.set_font('Arial', '', 14)
+        pdf.cell(effective_page_width, chosen_box_height, "RECIPE INGREDIENTS //", 1, 1, 'L')
+        ##
+        count=1
+        for ingr_group in RECIPE_INGREDIENTS:
+            print(type(ingr_group)) ## should be dictionary
+            print(list(ingr_group.keys())) ## printing all keys in dictionary, as list
+            print()
+            ingr_group_title = ingr_group.get("recipeIngredientTitle")
+            print(ingr_group_title)
+            pdf.set_font('Arial', 'B', 13)
+            ## replace some unreadable characters to question marks
+            ingr_group_title = ingr_group_title.encode('latin-1', 'replace').decode('latin-1')
+            ##
+            pdf.multi_cell(effective_page_width, chosen_box_height, '=> ' + ingr_group_title, 0, 1, 'L')
+            list_ingredients= ingr_group.get("recipeIngredientList")
+            for ingr in list_ingredients:
+                print(ingr)
+                pdf.set_font('Arial', '', 12)
+                ## replace some unreadable characters to question marks
+                ingr = ingr.encode('latin-1', 'replace').decode('latin-1')
+                ##
+                pdf.multi_cell(effective_page_width, chosen_box_height, str(count) + ') ' + ingr, 0, 1, 'L') 
+                count=count+1   
+
+        #print recipe instructions
+        pdf.ln(chosen_box_height)
+        pdf.set_font('Arial', '', 14)
+        pdf.cell(effective_page_width, chosen_box_height, "RECIPE INSTRUCTIONS //", 1, 1, 'L')
+        ##
+        count=1
+        for ingr_group in RECIPE_INSTRUCTIONS:
+            print(type(ingr_group)) ## should be dictionary
+            print(list(ingr_group.keys())) ## printing all keys in dictionary, as list
+            print()
+            ingr_group_title = ingr_group.get("recipeInstructionsTitle")
+            print(ingr_group_title)
+            pdf.set_font('Arial', 'B', 13)
+            ## replace some unreadable characters to question marks
+            ingr_group_title = ingr_group_title.encode('latin-1', 'replace').decode('latin-1')
+            ##
+            pdf.multi_cell(effective_page_width, chosen_box_height, '=> ' + ingr_group_title, 0, 1, 'L')
+            list_ingredients= ingr_group.get("recipeInstructionsList")
+            for ingr in list_ingredients:
+                print(ingr)
+                pdf.set_font('Arial', '', 12)
+                ## replace some unreadable characters to question marks
+                ingr = ingr.encode('latin-1', 'replace').decode('latin-1')
+                ##
+                pdf.multi_cell(effective_page_width, chosen_box_height, str(count) + ') ' + ingr, 0, 1, 'L') 
+                count=count+1   
+        
+        #print recipe notes
+        pdf.ln(chosen_box_height)
+        count=1
+        pdf.set_font('Arial', '', 12)
+        pdf.cell(effective_page_width, chosen_box_height, "RECIPE NOTES //", 1, 1, 'L')
+        for single_note in RECIPE_NOTES:
+            ## replace some unreadable characters to question marks
+            single_note = single_note.encode('latin-1', 'replace').decode('latin-1')
+            ##
+            pdf.multi_cell(effective_page_width, chosen_box_height, str(count) + ') ' + single_note, 0, 1, 'L')
+            count=count+1
+
+
+        ##------------------------------------------------------------------------------
+        ## FINAL PDF OUTPUT FOR THIS RECIPE 
+        pdf.output(PDFDIR + PDF_FILENAME, 'F')
+
+
+
+
+
+
+
+###############################################################################
+############################# PROGRAM ENDS ####################################
