84ad5a165cfec28028c9dd00fdf9d464a249d281
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2021-06-25 23:59:21 +0300

    1x updated

--- 101-pali-change-modification-time-of-files.sh
+++ 101-pali-change-modification-time-of-files.sh
@@ -20,30 +20,38 @@ if [ "$1" == "--help" ] ; then usage ; fi
 
 
 
-PWD=`pwd`;
+PWD=$(pwd);
 cd $PWD ; ## CD to present working directory
 
 for filename in *.* ;
-do
-  echo "\n"; ## Inserts blank line
-
-  ## Cuts the filename text with first 8 characters, and assigns that as a variable
-  new_time=`echo "$filename" | cut -c 1-8` ;
-
-  echo "CURRENT FILE: "$filename ;
-  echo "NEW-TIME: "$new_time ;
-
-      ## IF the new variable is only digits, such as date, etc. then do following:
-      if [[ $new_time =~ ^[0-9]+$ ]]
-          then
-              echo "COMMENT: FILENAME OK. Starts with a number. So CREATION TIME changed." ;
-              ## Now modifying the creation time
-              touch -t $new_time"1234" $filename
-          else
-              echo "COMMENT: FILENAME NOT OK. Does not start with a number. So CREATION TIME NOT changed. <============= " ;
-      fi
-
+do 
+    ####
+    echo;
+    ## Cuts the filename text with first 8 characters, and assigns that as a variable
+    new_time=$(echo "$filename" | cut -c 1-8) ;
+    ##
+    echo "CURRENT FILE: $filename" ;
+    echo "NEW-TIME: $new_time" ;
+    ##
+    ## IF the new variable is only digits, such as date, etc. then do following:
+    if [[ $new_time =~ ^[0-9]+$ ]]
+        then
+            echo "COMMENT: FILENAME OK. Starts with a number. So CREATION TIME changed." ;
+            ## Now modifying the creation time
+            touch -t $new_time"1234" $filename
+        else
+            echo "COMMENT: FILENAME NOT OK. Does not start with a number. So CREATION TIME NOT changed. <============= " ;
+    fi
+    ####
 done
 
-## FINALLY OPEN pwd
-open $PWD ;
+## Listing ...
+echo; echo; echo ">> Listing files ..." ; ls -al ;
+
+## IF on MAC, open working directory
+if [ "$USER" == "abhishek" ] ; then 
+    open $PWD ;
+else 
+	echo ;
+fi 
+
