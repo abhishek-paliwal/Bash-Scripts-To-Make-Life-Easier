2cf3f3240281d04e9971af02c111792c9bd8adef
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2021-07-18 17:57:47 +0300

    Update 999-mggk-CREATE-RESPONSIVE-IMAGES.sh

--- mggk_bash_scripts/999-mggk-CREATE-RESPONSIVE-IMAGES.sh
+++ mggk_bash_scripts/999-mggk-CREATE-RESPONSIVE-IMAGES.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#IMAGES_ROOTDIR="$REPO_MGGK/static/wp-content/youtube_video_cover_image"
 IMAGES_ROOTDIR="$REPO_MGGK/content/" ;
+RESPONSIVE_IMAGES_ROOTDIR="$DIR_Y" ;
 
+##
 tmp1="$DIR_Y/tmp1.txt" ;
 tmp2="$DIR_Y/tmp2.txt" ;
 echo "## Created by script: " > $tmp1
@@ -14,7 +15,7 @@ do
     grep -i "{{< figure" $x | sd ' ' '\n' | grep 'src' | sd '"' '' | sd 'src=' '' >> $tmp1
 done
 
-##
+## Converting urls to local file paths
 cat $tmp1 | sd "https://www.mygingergarlickitchen.com" "$REPO_MGGK/static" > $tmp2
 
 ##################################################################################
@@ -24,7 +25,10 @@ function FUNC_create_responsive_images_for_each_line () {
     ## FUNCTION TO CREATE RESPONSIVE IMAGES
     imagePath="$1" ;
     imagePath_basename=$(basename $imagePath) ;
-    echo "$imagePath // BASENAME = $imagePath_basename)" ;
+    echo "Image = $imagePath"
+    echo "BASENAME = $imagePath_basename" ;
+    originalImageDir="$RESPONSIVE_IMAGES_ROOTDIR/original" ;
+    mkdir $originalImageDir ;
     ## 
     myarray=(300px 425px 550px 675px 800px)
     ##
@@ -33,7 +37,9 @@ function FUNC_create_responsive_images_for_each_line () {
         echo "#### CURRENT SIZE => $i" ;
         imageRes="$i" ;
         resizeTo="$(echo $i | sed 's/px//g')"
-        resizeDir="$DIR_Y/$imageRes" ;
+        resizeDir="$RESPONSIVE_IMAGES_ROOTDIR/$imageRes" ;
+        outputImage="$resizeDir/$imageRes-$imagePath_basename"
+        CopiedOriginalImage="$originalImageDir/$imagePath_basename"
         ##
         if [[ -d "$resizeDir" ]] ; then
             echo "Directory already exists => $resizeDir"
@@ -42,10 +48,16 @@ function FUNC_create_responsive_images_for_each_line () {
             mkdir $resizeDir ;
         fi
         ##
-        ## final resizing of jpg images
-        echo "// $i => resizing is about to begin ..." ;
-        # fdfind --search-path="$IMAGES_ROOTDIR" -a -e jpg -x convert {} -resize "$resizeTo" -quality 80 "$resizeDir/$imageRes-{/.}.jpg" ;
-        convert $imagePath -resize "$resizeTo" -quality 80 "$resizeDir/$imageRes-$imagePath_basename" ;
+        ## Copy original image + only create responsive image if it does not exist
+        if [[ -f "$outputImage" ]] ; then
+            echo "Image already exists => $outputImage" ; 
+        else
+            echo "($imageRes) Image will be created (+ original image copied)" ; 
+            echo "Output image = $outputImage" ; 
+            echo "Copied original image = $CopiedOriginalImage" ; 
+            cp "$imagePath" "$CopiedOriginalImage" ;
+            convert $imagePath -resize "$resizeTo" -quality 80 "$outputImage" ;
+        fi
     done
 }
 
@@ -56,9 +68,9 @@ while read -r line;
 do
     echo "$line" ;
     if [[ -f "$line" ]] ; then 
+        echo "##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"; 
         echo "OK = Image Found" ; 
-        ## Call function
-        FUNC_create_responsive_images_for_each_line "$line"
+        FUNC_create_responsive_images_for_each_line "$line" ; ## Call function
     else 
         echo "NOT OK = Image Not Found" ;
     fi
