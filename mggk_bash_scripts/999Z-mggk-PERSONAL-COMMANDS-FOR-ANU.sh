915ee1311a9c2c71f1c1baad54d57586f5269814
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2021-07-21 18:40:51 +0300

    1x edited

--- mggk_bash_scripts/999Z-mggk-PERSONAL-COMMANDS-FOR-ANU.sh
+++ mggk_bash_scripts/999Z-mggk-PERSONAL-COMMANDS-FOR-ANU.sh
@@ -109,7 +109,7 @@ video_cutting_in_1_minute_equal_parts_using_ffmpeg
 999B-mggk-CREATE-SEARCH-ENGINE-LINKS-FOR-ALL-URLS
 999-mggk-CREATE-PDF-FILES-FROM-RECIPE-MD-FILES
 999-mggk-REPLACE-RECIPE-STEPS-WHITE-SQUARE-IMAGES-WITH-WIDE-ONES
-999-mggk-CREATE-RESPONSIVE-IMAGES-for-all-figure-hugo-shortcodes
+999-mggk-CREATE-RESPONSIVE-IMAGES-FOR-MGGK-SITE
 mggk_shortcode_collection_maker
 mggk_calculate_length_of_yoast_description
 mggk_calculate_length_of_titles
