cce9a6b712fd9881061adfc79a2d611895ded7f5
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2021-07-22 23:51:17 +0300

    2x updated

--- 9999-digitalocean-scripts/9999-download-latest-scripts-from-github-repo-for-crontab.sh
+++ 9999-digitalocean-scripts/9999-download-latest-scripts-from-github-repo-for-crontab.sh
@@ -11,9 +11,11 @@ cat <<EOM
 USAGE: $(basename $0)
   ################################################################################
   ## THIS SCRIPT DOWNLOADS THE LATEST VERSIONS OF SCRIPTS FROM GITHUB REPOS.
-  ## Add more functions as needed.
+  ## Add more scripts as needed.
   ################################################################################
-  ## Created on: Feb 17, 2021
+  ## IMPORTANT NOTE: Please dont include this script to download itself.
+  ################################################################################
+  ## Created on: July 21, 2021
   ## Coded by: Pali
   ###############################################################################
   # USAGE (run the following command):
@@ -28,19 +30,22 @@ if [ "$1" == "--help" ] ; then usage ; fi
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ################################################################################
 
-################################################################################
-function FUNCTION_DOWNLOAD_LATEST_SCRIPT_1 () {
-  DIR_TO_MOVE="/home/ubuntu/scripts-made-by-pali" ;
-  GIT_REPO_PATH="https://raw.githubusercontent.com/abhishek-paliwal/Bash-Scripts-To-Make-Life-Easier/master/9999-digitalocean-scripts" ;
-  BASH_SCRIPT_FILE="9999-show-files-to-delete-for-space-cleanup-digitalocean.sh" ;
-  ## Deleting existing version of this script
-  rm $DIR_TO_MOVE/$BASH_SCRIPT_FILE ;
-  ## Downloading ...
-  wget $GIT_REPO_PATH/$BASH_SCRIPT_FILE ;
-  ## Moving THIS SCRIPT to our desired directory
-  mv $BASH_SCRIPT_FILE $DIR_TO_MOVE/$BASH_SCRIPT_FILE ;
-}
-## RUN THE FUNCTION
-FUNCTION_DOWNLOAD_LATEST_SCRIPT_1
-################################################################################
+WORKDIR="/home/ubuntu/scripts-made-by-pali" ; 
+#WORKDIR="$DIR_Y" ; 
+echo ">> WORKDIR = $WORKDIR" ;
 
+## ADD FULL GITHUB SCRIPT PATH TO FOLLOWING ARRAY (one on each line)
+ARRAY_download_these_scripts=(https://raw.githubusercontent.com/abhishek-paliwal/Bash-Scripts-To-Make-Life-Easier/master/9999-digitalocean-scripts/9999-show-files-to-delete-for-space-cleanup-digitalocean.sh
+https://raw.githubusercontent.com/abhishek-paliwal/Bash-Scripts-To-Make-Life-Easier/master/mggk_bash_scripts/901-mggk-GET-CLOUDFLARE-CACHE-STATUS.sh) ;
+
+for i in "${ARRAY_download_these_scripts[@]}"; do
+    echo "#################################################" ;
+    echo ">> Current Script => $i" ;
+    script_basename=$(basename $i) ;
+    ## Deleting existing version of this script
+    rm $WORKDIR/$script_basename ;
+    ## Downloading ...
+    wget $i ;
+    ## Moving THIS SCRIPT to our desired directory
+    mv $script_basename $WORKDIR/$script_basename ;
+done
