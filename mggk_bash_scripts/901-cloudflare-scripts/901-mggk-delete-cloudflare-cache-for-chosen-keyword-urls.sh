b579e283bf156b5b22354d5af79597e7138e0f11
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2021-08-04 00:45:07 +0300

    Update 901-mggk-delete-cloudflare-cache-for-chosen-keyword-urls.sh

--- mggk_bash_scripts/901-cloudflare-scripts/901-mggk-delete-cloudflare-cache-for-chosen-keyword-urls.sh
+++ mggk_bash_scripts/901-cloudflare-scripts/901-mggk-delete-cloudflare-cache-for-chosen-keyword-urls.sh
@@ -1,15 +1,17 @@
-################################################################################ 
-## mggk delete cloudflare cache for these urls in array
-prefixFileName="_OUTPUT_9999_cloudflare_delete_cache_for_custom_keyword_urls" ;
+#/bin/bash
 
+## mggk delete cloudflare cache for these urls in array
+
+################################################################################ 
 WORKDIR="$DIR_Y" ;
 echo ">> CURRENT WORKDIR = $WORKDIR" ;
 
+prefixFileName="_OUTPUT_9999_cloudflare_delete_cache_for_custom_keyword_urls" ;
 tmpFile="$WORKDIR/_tmp.txt" ; 
 step0File="$WORKDIR/$prefixFileName-step0.txt" ; 
 step1File="$WORKDIR/$prefixFileName-step1.txt" ; 
 step2File="$WORKDIR/$prefixFileName-step2.txt" ;
-step4FileHTML="$WORKDIR/$prefixFileName-step4.html" ;
+step3FileHTML="$WORKDIR/$prefixFileName-step4.html" ;
 
 ##------------------------------------------------------------------------------
 echo "Enter your keyword to find images for: " ; 
@@ -56,34 +58,51 @@ function step2_FUNC_cloudflare_format_urls_for_api () {
     echo ">> DONE = step2_FUNC_cloudflare_format_urls_for_api " ;
 }
 
-function step3_FUNC_cloudflare_delete_cache_for_keyword_urls () {
-    ## function takes no arguments 
-    inFile="$step1File" ; ## step1File
-    for line in $(cat $inFile) ; do
-        final_data='{\"files\":[ \"$line\" ]}' ;
-        echo "FINAL DATA = $final_data" ; 
-        curl -X POST "https://api.cloudflare.com/client/v4/zones/53b0327844e25ed872863f33e465bca0/purge_cache" -H "X-Auth-Email:abhishek+cloudflare@concepro.com" -H "X-Auth-Key:d9072603dee58a63ebac3e3b81fdf9c15d8f8" -H "Content-Type:application/json" --data "$final_data" ;
-    done
-    ##
-    echo ">> DONE = step3_FUNC_cloudflare_delete_cache_for_keyword_urls " ;
-}
-
-function step4_FUNC_cloudflare_create_html_page_with_keyword_urls () {
+function step3_FUNC_cloudflare_create_html_page_with_keyword_urls () {
     ## function takes no arguments 
     #### mggk create a html page showing all images with names with given keyword
     inFile="$step1File" ;
-    outFile="$step4FileHTML" ;
+    outFile="$step3FileHTML" ;
+    echo "<h1>Images for this keyword: $myKeyword</h1>" > $outFile
     for x in $(cat $inFile) ; do 
-        echo "<div style='display: inline-block; width: 100px; height: 150px;'><img src='$x' width='100%'><br>$(basename $x)</div>" ; 
-    done > $outFile
+        echo "<div style='display: inline-block; width: 100px; height: 150px;'><img src='$x' width='100%'><br>$(basename $x)</div>"  >> $outFile ;
+    done
     ##
-    echo ">> DONE = step4_FUNC_cloudflare_create_html_page_with_keyword_urls " ;
+    echo ">> DONE = step3_FUNC_cloudflare_create_html_page_with_keyword_urls " ;
+}
+
+function step4_FUNC_cloudflare_delete_cache_for_keyword_urls () {
+    ## function takes no arguments 
+    inFile="$step1File" ; ## step1File
+    echo ">> Deleting cache keyword urls ..." ;
+    for line in $(cat $inFile) ; do
+        final_data='{ "files":[ "'"$line"'" ]}' ;
+        echo "FINAL DATA = $final_data" ; 
+        curl -X POST "https://api.cloudflare.com/client/v4/zones/53b0327844e25ed872863f33e465bca0/purge_cache" -H "X-Auth-Email:$EMAIL_CLOUDFLARE" -H "X-Auth-Key:$API_KEY_CLOUDFLARE_PURGE" -H "Content-Type:application/json" --data "$final_data" ;
+    done
+    ##
+    echo ">> DONE = step4_FUNC_cloudflare_delete_cache_for_keyword_urls " ;
 }
 ################################################################################
 ################################################################################
 
+## CALLING FUNCTIONS
 step0_FUNC_cloudflare_search_image_paths_containing_keyword
 step1_FUNC_cloudflare_search_image_urls_containing_keyword ;
 step2_FUNC_cloudflare_format_urls_for_api ;
-#step3_FUNC_cloudflare_delete_cache_for_keyword_urls; 
-step4_FUNC_cloudflare_create_html_page_with_keyword_urls ; 
+step3_FUNC_cloudflare_create_html_page_with_keyword_urls ;
+
+## SUMMARY OF OUTPUTS
+fd "$prefixFileName" -t f --search-path="$WORKDIR" -x wc -l {} ;
+
+## ASK USER TO DELETE CACHE OR NOT
+echo ;
+echo ">> ALSO WANT TO DELETE CLOUDFLARE CACHE, ENTER y OR n : " ;
+read CacheDelete ;
+if [ "$CacheDelete" == "y" ]; then
+    echo ">> Alright, cache WILL BE DELETED." ;
+    step4_FUNC_cloudflare_delete_cache_for_keyword_urls ;
+else 
+    echo ">> Alright, cache WILL NOT BE DELETED." ;
+fi
+
