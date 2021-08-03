c642a31c7dc601bf482a2deeb4d9c004b111602b
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2021-08-04 02:01:15 +0300

    Update 901-mggk-delete-cloudflare-cache-for-chosen-keyword-urls.sh

--- mggk_bash_scripts/901-cloudflare-scripts/901-mggk-delete-cloudflare-cache-for-chosen-keyword-urls.sh
+++ mggk_bash_scripts/901-cloudflare-scripts/901-mggk-delete-cloudflare-cache-for-chosen-keyword-urls.sh
@@ -8,6 +8,7 @@ echo ">> CURRENT WORKDIR = $WORKDIR" ;
 
 prefixFileName="_OUTPUT_9999_cloudflare_delete_cache_for_custom_keyword_urls" ;
 tmpFile="$WORKDIR/_tmp.txt" ; 
+tmpFile1="$WORKDIR/_tmp1.txt" ; 
 step0File="$WORKDIR/$prefixFileName-step0.txt" ; 
 step1File="$WORKDIR/$prefixFileName-step1.txt" ; 
 step2File="$WORKDIR/$prefixFileName-step2.txt" ;
@@ -47,18 +48,18 @@ function step1_FUNC_cloudflare_search_image_urls_containing_keyword () {
 
 function step2_FUNC_cloudflare_format_urls_for_api () {
     ## function takes no arguments 
-    inFile="$step1File" ; ## step1File
-    outFile="$step2File" ; ## step2File
+    inFile="$1" ;
+    outFile="$tmpFile1" ; 
     echo "##" > $tmpFile ; 
     for x in $(cat $inFile) ; do 
         echo "\"$x\"," >> $tmpFile ; 
     done ; 
     echo "\"$(head -1 $inFile)\"" >> $tmpFile ;
     cat $tmpFile | grep -iv '#' | sd '\n' '' > $outFile ;
-    echo ">> DONE = step2_FUNC_cloudflare_format_urls_for_api " ;
+    echo ">> DONE = step2_FUNC_cloudflare_format_urls_for_api (=> for this FILE: $inFile )" ;
 }
 
-function step3_FUNC_cloudflare_create_html_page_with_keyword_urls () {
+function step2_FUNC_cloudflare_create_html_page_with_keyword_urls () {
     ## function takes no arguments 
     #### mggk create a html page showing all images with names with given keyword
     inFile="$step1File" ;
@@ -68,7 +69,7 @@ function step3_FUNC_cloudflare_create_html_page_with_keyword_urls () {
         echo "<div style='display: inline-block; width: 100px; height: 150px;'><img src='$x' width='100%'><br>$(basename $x)</div>"  >> $outFile ;
     done
     ##
-    echo ">> DONE = step3_FUNC_cloudflare_create_html_page_with_keyword_urls " ;
+    echo ">> DONE = step2_FUNC_cloudflare_create_html_page_with_keyword_urls " ;
 }
 
 function step4_FUNC_cloudflare_delete_cache_for_keyword_urls () {
@@ -78,7 +79,7 @@ function step4_FUNC_cloudflare_delete_cache_for_keyword_urls () {
     for line in $(cat $inFile) ; do
         final_data='{ "files":[ "'"$line"'" ]}' ;
         echo "FINAL DATA = $final_data" ; 
-        curl -X POST "https://api.cloudflare.com/client/v4/zones/53b0327844e25ed872863f33e465bca0/purge_cache" -H "X-Auth-Email:$CLOUDFLARE_EMAIL" -H "X-Auth-Key:$API_KEY_CLOUDFLARE_PURGE" -H "Content-Type:application/json" --data "$final_data" ;
+        #curl -X POST "https://api.cloudflare.com/client/v4/zones/53b0327844e25ed872863f33e465bca0/purge_cache" -H "X-Auth-Email:$CLOUDFLARE_EMAIL" -H "X-Auth-Key:$API_KEY_CLOUDFLARE_PURGE" -H "Content-Type:application/json" --data "$final_data" ;
     done
     ##
     echo ">> DONE = step4_FUNC_cloudflare_delete_cache_for_keyword_urls " ;
@@ -89,8 +90,7 @@ function step4_FUNC_cloudflare_delete_cache_for_keyword_urls () {
 ## CALLING FUNCTIONS
 step0_FUNC_cloudflare_search_image_paths_containing_keyword
 step1_FUNC_cloudflare_search_image_urls_containing_keyword ;
-step2_FUNC_cloudflare_format_urls_for_api ;
-step3_FUNC_cloudflare_create_html_page_with_keyword_urls ;
+step2_FUNC_cloudflare_create_html_page_with_keyword_urls ;
 
 ## SUMMARY OF OUTPUTS
 echo; echo ">> PRINTING WORD COUNTS ... " ;
@@ -106,9 +106,18 @@ if [ "$CacheDelete" == "y" ]; then
     inFile="$step1File" ; ## step1File
     inFile_base=$(basename $inFile) ; 
     echo ">> Splitting file (= $inFile_base ) into multiple files with 30 lines each ..." ;
-    fd 'xa' -t f --split-path="$WORKDIR" -x rm {} ; ## Deleting already present xa files
-    split -l 30 $inFile ;
-    fd 'xa' -t f --split-path="$WORKDIR" -x step4_FUNC_cloudflare_delete_cache_for_keyword_urls {} ;
+    fd 'xa' -t f --search-path="$WORKDIR" -x rm {} ; ## Deleting already present xa files
+    split -l 30 $inFile ; ## Actual splitting
+    ## SUMMARY OF OUTPUTS
+    echo; echo ">> PRINTING WORD COUNTS ... " ;
+    fd 'xa' -t f --search-path="$WORKDIR" -x wc {} ;
+    ####
+    ## Actual deletion of cache
+    for myFile in $(fd 'xa' -t f --search-path="$WORKDIR"); do 
+        step2_FUNC_cloudflare_format_urls_for_api "$myFile" ;
+        step4_FUNC_cloudflare_delete_cache_for_keyword_urls "$tmpFile1" ;
+    done
+    ####
 else 
     echo ">> Alright, cache WILL NOT BE DELETED." ;
 fi
