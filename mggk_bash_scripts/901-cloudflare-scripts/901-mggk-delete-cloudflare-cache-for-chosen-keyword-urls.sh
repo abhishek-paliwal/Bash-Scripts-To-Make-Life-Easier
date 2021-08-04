2ef8bcfddc550a08dbe9084d49b049643933c215
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2021-08-04 03:18:27 +0300

    Update 901-mggk-delete-cloudflare-cache-for-chosen-keyword-urls.sh

--- mggk_bash_scripts/901-cloudflare-scripts/901-mggk-delete-cloudflare-cache-for-chosen-keyword-urls.sh
+++ mggk_bash_scripts/901-cloudflare-scripts/901-mggk-delete-cloudflare-cache-for-chosen-keyword-urls.sh
@@ -1,9 +1,35 @@
-#/bin/bash
+#!/bin/bash
+THIS_SCRIPT_NAME="$(basename $0)" ;
+THIS_SCRIPT_NAME_SANS_EXTENSION="$(echo $THIS_SCRIPT_NAME | sed 's/\.sh//g')" ;
 
-## mggk delete cloudflare cache for these urls in array
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## CREATING SCRIPT USAGE FUNCION AND CALLING IT VIA '--help'
+usage()
+{
+cat <<EOM
+USAGE: $(basename $0)
+    ################################################################################
+    ## USAGE:
+    #### > bash $THIS_SCRIPT_NAME
+    ################################################################################
+    ## THIS SCRIPT ASKS FOR USER INPUT FOR KEYWORDS TO SEARCH FOR ALL IMAGES CONTAINING 
+    ## THAT KEYWORD. IT THEN DELETES CLOUDFLARE CACHE CORRESPONDING TO THOSE IMAGE URLS
+    ################################################################################
+    ## CREATED BY: PALI
+    ## CREATED ON: 2021-08-04
+    ################################################################################
+EOM
+
+exit 0 ## EXITING IF ONLY USAGE IS NEEDED
+}
+## Calling the usage function
+if [ "$1" == "--help" ] ; then usage ; fi
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 ################################################################################ 
-WORKDIR="$DIR_Y" ;
+WORKDIR="$DIR_Y/_TMP_$THIS_SCRIPT_NAME_SANS_EXTENSION" ;
+mkdir -p "$WORKDIR" ;
+cd $WORKDIR; 
 echo ">> CURRENT WORKDIR = $WORKDIR" ;
 
 prefixFileName="_OUTPUT_9999_cloudflare_delete_cache_for_custom_keyword_urls" ;
@@ -12,7 +38,7 @@ tmpFile1="$WORKDIR/_tmp1.txt" ;
 step0File="$WORKDIR/$prefixFileName-step0.txt" ; 
 step1File="$WORKDIR/$prefixFileName-step1.txt" ; 
 step2File="$WORKDIR/$prefixFileName-step2.txt" ;
-step3FileHTML="$WORKDIR/$prefixFileName-step4.html" ;
+step3FileHTML="$WORKDIR/$prefixFileName-step3.html" ;
 
 ##------------------------------------------------------------------------------
 echo "Enter your keyword to find images for: " ; 
@@ -27,7 +53,7 @@ function step0_FUNC_cloudflare_search_image_paths_containing_keyword () {
     ## function takes no arguments 
     outFile="$step0File" ; ## step0File
     echo "#" > $tmpFile ;
-    fd -I -t f --search-path="$REPO_MGGK/static/wp-content/" "$myKeyword" | sort >> $tmpFile
+    fd -I -t f -e jpg -e png -e jpeg -e gif --search-path="$REPO_MGGK/static/wp-content/" "$myKeyword" | sort >> $tmpFile
     cat $tmpFile | grep -iv '#' > $outFile ;
     echo ">> DONE = step0_FUNC_cloudflare_search_image_paths_containing_keyword " ;
 }
@@ -66,7 +92,7 @@ function step2_FUNC_cloudflare_create_html_page_with_keyword_urls () {
     outFile="$step3FileHTML" ;
     echo "<h1>Images for this keyword: $myKeyword</h1>" > $outFile
     for x in $(cat $inFile) ; do 
-        echo "<div style='display: inline-block; width: 100px; height: 150px;'><img src='$x' width='100%'><br>$(basename $x)</div>"  >> $outFile ;
+        echo "<div style='display: inline-block; width: 65px; height: auto; border-style: solid; border-color: black; border-width: 1px; padding: 2px; '><img src='$x' width='100%'><br>$(basename $x)</div>"  >> $outFile ;
     done
     ##
     echo ">> DONE = step2_FUNC_cloudflare_create_html_page_with_keyword_urls " ;
@@ -75,12 +101,11 @@ function step2_FUNC_cloudflare_create_html_page_with_keyword_urls () {
 function step4_FUNC_cloudflare_delete_cache_for_keyword_urls () {
     ## function takes one argument as texfile with less than 30 urls
     inFile="$1" ;
-    echo ">> Deleting cache keyword urls ... only 30 urls are allowed in one api purge call ..." ;
-    for line in $(cat $inFile) ; do
-        final_data='{ "files":[ "'"$line"'" ]}' ;
-        echo "FINAL DATA = $final_data" ; 
-        #curl -X POST "https://api.cloudflare.com/client/v4/zones/53b0327844e25ed872863f33e465bca0/purge_cache" -H "X-Auth-Email:$CLOUDFLARE_EMAIL" -H "X-Auth-Key:$API_KEY_CLOUDFLARE_PURGE" -H "Content-Type:application/json" --data "$final_data" ;
-    done
+    echo ">> Deleting cache keyword urls ... max 30 urls are allowed per api purge call ..." ;
+    url_array=$(cat $inFile) ;
+    final_data="{ "'"files"'":[ $url_array ]}" ;
+    #echo "FINAL DATA = $final_data" ; 
+    curl -X POST "https://api.cloudflare.com/client/v4/zones/53b0327844e25ed872863f33e465bca0/purge_cache" -H "X-Auth-Email:$CLOUDFLARE_EMAIL" -H "X-Auth-Key:$API_KEY_CLOUDFLARE_PURGE" -H "Content-Type:application/json" --data "$final_data" ;
     ##
     echo ">> DONE = step4_FUNC_cloudflare_delete_cache_for_keyword_urls " ;
 }
@@ -105,9 +130,9 @@ if [ "$CacheDelete" == "y" ]; then
     ##
     inFile="$step1File" ; ## step1File
     inFile_base=$(basename $inFile) ; 
-    echo ">> Splitting file (= $inFile_base ) into multiple files with 30 lines each ..." ;
+    echo ">> Splitting file (= $inFile_base ) into multiple files with 28 lines each ..." ;
     fd 'xa' -t f --search-path="$WORKDIR" -x rm {} ; ## Deleting already present xa files
-    split -l 30 $inFile ; ## Actual splitting
+    split -l 28 $inFile ; ## Actual splitting
     ## SUMMARY OF OUTPUTS
     echo; echo ">> PRINTING WORD COUNTS ... " ;
     fd 'xa' -t f --search-path="$WORKDIR" -x wc {} ;
@@ -122,3 +147,7 @@ else
     echo ">> Alright, cache WILL NOT BE DELETED." ;
 fi
 
+##------------------------------------------------------------------------------
+## SUMMARY
+echo; 
+echo ">> CHECK OUT THE HTML OUTPUT: $step3FileHTML " ;
