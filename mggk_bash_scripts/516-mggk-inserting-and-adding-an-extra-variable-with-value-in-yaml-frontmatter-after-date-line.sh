6f34a809311c8e0aad93f391324dedb26f664585
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2021-08-26 23:05:55 +0300

    Update 516-mggk-inserting-and-adding-an-extra-variable-with-value-in-yaml-frontmatter-after-date-line.sh

--- mggk_bash_scripts/516-mggk-inserting-and-adding-an-extra-variable-with-value-in-yaml-frontmatter-after-date-line.sh
+++ mggk_bash_scripts/516-mggk-inserting-and-adding-an-extra-variable-with-value-in-yaml-frontmatter-after-date-line.sh
@@ -14,9 +14,7 @@ cat <<EOM
 USAGE: $(basename $0)
   ###############################################################################
   ## This program adds (inserts) extra yaml frontmatter variable(s) of your 
-  ## choice, in markdown files.
-  ## ALSO PRESENT => ONE LINE VERSION TO ADD EXTRA FRONTMATTER VARIABLE(s) 
-  ## (if you simply want to add new variable(s) above my_custom_variable variable name)
+  ## choice in mggk markdown files.
   ###############################################################################
   ## CREATED ON: 2021-08-26
   ## CREATED BY: PALI
@@ -29,22 +27,33 @@ exit 0 ## EXITING IF ONLY USAGE IS NEEDED
 if [ "$1" == "--help" ] ; then usage ; fi
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+mydir="$REPO_MGGK/content/"
+#mydir="$REPO_ZZMGGK/content/blog"
+
 ################################################################################
+function func_create_new_frontmatter_variables () {
+    ## Add frontmatter variables to those files where seo_title_value is not found 
+    ## Adds three new variable before title tag: toc, seo_title_value, my_custom_variable. 
+    for mdFile in $(ag -L "seo_title_value" "$mydir") ; do 
+        sed -i '' "s|^title|toc: true\n\nseo_title_value: \"demo title demo title\"\n\nmy_custom_variable: \"custom_variable_value\"\n\ntitle|g" $mdFile ;
+    done 
+}
+########################################
+function func_insert_value_to_seo_title_value () {
+    ## Replace exiting frontmatter variable value from another variable
+    ## Replaces value of seo_title_value from exiting title value.
+    for x in $(ag -l "seo_title_value" "$mydir") ; do 
+        echo $x; 
+        ## get the existing title value to be inserted into the new one
+        new_seo_title=$(grep -irh '^title:' $x | sd 'title' 'seo_title_value') ;
+        ##
+        sed -i '' "s%seo_title_value.*$%$new_seo_title%g" $x
+    done
+}
 ################################################################################
-## ONE LINE VERSION TO ADD EXTRA FRONTMATTER VARIABLE(s) 
-## (if you simply want to add new variable(s) above my_custom_variable variable name)
-################################################################################
+
 ## STEP1 = The following is an example command (fully working) // copy-paste after uncommenting
-#fd --search-path="$REPO_MGGK/content/" -e md -x sed -i .bak "s|my_custom_variable|toc: true\n\nseo_title_value: \"demo title demo title\"\n\nmy_custom_variable|g" {} ;
+func_create_new_frontmatter_variables
 
 ## STEP2 = Get the value from the existing title and insert into the newly created variable
-mydir="$REPO_MGGK/content/"
-##
-for x in $(ag -l "seo_title_value" "$mydir") ; do 
-    echo $x; 
-    ## get the existing title value to be inserted into the new one
-    new_seo_title=$(grep -irh '^title:' $x | sd 'title' 'seo_title_value') ;
-    ##
-    sed -i '' "s%seo_title_value.*$%$new_seo_title%g" $x
-done
-
+func_insert_value_to_seo_title_value
