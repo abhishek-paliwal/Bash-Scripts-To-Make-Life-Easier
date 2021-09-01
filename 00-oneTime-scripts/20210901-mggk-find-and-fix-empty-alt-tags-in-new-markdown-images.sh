292b224d56d90fb5b1ad454beaa45405d387d10e
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2021-09-01 20:23:48 +0300

    Update 20210901-mggk-find-and-fix-empty-alt-tags-in-new-markdown-images.sh

--- 00-oneTime-scripts/20210901-mggk-find-and-fix-empty-alt-tags-in-new-markdown-images.sh
+++ 00-oneTime-scripts/20210901-mggk-find-and-fix-empty-alt-tags-in-new-markdown-images.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 
+## THIS PROGRAM FINDS AND FIXES empty image alt tags of new markdown
+## images of ![](image.jpg/jpeg/png) format.
+
 WORKDIR="$DIR_Y" ;
 
 ################################################################################ 
@@ -17,7 +20,7 @@ function func_fix_this_empty_image_link () {
         #echo "$image//"
         myline_replace=$(echo $myline | sed -e 's|\!|\\!|g' -e 's|\[|\\[|g' -e 's|\]|\\]|g') ;
         #echo "$myline_replace" ;
-        #sed -i '' "s|$myline_replace|\!\[$image_basename_words_final\]($image)|g" $mdfile_to_fix ;
+        sed -i '' "s|$myline_replace|\!\[$image_basename_words_final\]($image)|g" $mdfile_to_fix ;
     done 
 }
 ################################################################################ 
@@ -28,11 +31,11 @@ count=0
 ## Fix all files where empty image markdown links found.
 for mdfile_to_fix in $(grep -irl '\!\[\]' "$inDir" ) ; do
     ((count++)) ;
-    echo; echo ">> Finding empty links and fixing // current file => $count" ; 
+    echo; echo ">> Current file => $count // Finding empty links and fixing // $mdfile_to_fix " ; 
     ##
-    find_all_empty_links="$(grep -irh '\!\[\]' $mdfile_to_fix)" ; 
+    find_all_empty_links="$(grep -irh '\!\[\]' $mdfile_to_fix | sd ' ' '' )" ; 
     #echo "$find_all_empty_links" ; 
-    echo "$find_all_empty_links" > "$tmpFile" ;
+    echo "$find_all_empty_links"> "$tmpFile" ;
     ##
     func_fix_this_empty_image_link "$tmpFile" "$mdfile_to_fix" ;
 done
