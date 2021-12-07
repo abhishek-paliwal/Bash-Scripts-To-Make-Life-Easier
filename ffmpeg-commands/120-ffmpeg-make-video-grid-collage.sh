3be46159f34db7b452dc3a65bf75ca5dc0595cda
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2021-12-07 14:02:39 +0200

    1x updated

--- ffmpeg-commands/120-ffmpeg-make-video-grid-collage.sh
+++ ffmpeg-commands/120-ffmpeg-make-video-grid-collage.sh
@@ -59,6 +59,39 @@ ffmpeg -i $inDir/1.mp4 -i $inDir/2.mp4 -i $inDir/3.mp4 -i $inDir/4.mp4 -i $inDir
     [tmp7][eight] overlay=shortest=1:x=640:y=720 [tmp8];
     [tmp8][nine] overlay=shortest=1:x=1280:y=720" -c:v libx264 $inDir/_output_video_collage_1920x1080_3x3.mp4
 }
+########
+function FUNC_CREATE_VIDEO_COLLAGE_3X2 () {
+inDir="$1" ;
+## creating actual collage
+ffmpeg -i $inDir/1.mp4 -i $inDir/2.mp4 -i $inDir/3.mp4 -i $inDir/4.mp4 -i $inDir/5.mp4 -i $inDir/6.mp4 -filter_complex "nullsrc=size=1920x1080 [base];
+    [0:v] setpts=PTS-STARTPTS, scale=640x360 [one];
+    [1:v] setpts=PTS-STARTPTS, scale=640x360 [two];
+    [2:v] setpts=PTS-STARTPTS, scale=640x360 [three];
+    [3:v] setpts=PTS-STARTPTS, scale=640x360 [four];
+    [4:v] setpts=PTS-STARTPTS, scale=640x360 [five];
+    [5:v] setpts=PTS-STARTPTS, scale=640x360 [six];
+    [base][one] overlay=shortest=1:x=0:y=0 [tmp1];
+    [tmp1][two] overlay=shortest=1:x=640:y=0 [tmp2];
+    [tmp2][three] overlay=shortest=1:x=1280:y=0 [tmp3];
+    [tmp3][four] overlay=shortest=1:x=0:y=360 [tmp4];
+    [tmp4][five] overlay=shortest=1:x=640:y=360 [tmp5];
+    [tmp5][six] overlay=shortest=1:x=1280:y=360" -c:v libx264 $inDir/_output_video_collage_1920x1080_3x2.mp4
+}
+########
+function FUNC_CREATE_VIDEO_COLLAGE_2X2 () {
+inDir="$1" ;
+## creating actual collage
+ffmpeg -i $inDir/1.mp4 -i $inDir/2.mp4 -i $inDir/3.mp4 -i $inDir/4.mp4 -filter_complex "nullsrc=size=1920x1080 [base];
+    [0:v] setpts=PTS-STARTPTS, scale=960x540 [one];
+    [1:v] setpts=PTS-STARTPTS, scale=960x540 [two];
+    [2:v] setpts=PTS-STARTPTS, scale=960x540 [three];
+    [3:v] setpts=PTS-STARTPTS, scale=960x540 [four];
+    [base][one] overlay=shortest=1:x=0:y=0 [tmp1];
+    [tmp1][two] overlay=shortest=1:x=960:y=0 [tmp2];
+    [tmp2][three] overlay=shortest=1:x=0:y=540 [tmp3];
+    [tmp3][four] overlay=shortest=1:x=960:y=540" -c:v libx264 $inDir/_output_video_collage_1920x1080_2x2.mp4
+}
+
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 ## Copying and then renaming all mp4 files to working dir
@@ -66,3 +99,5 @@ c=1; for x in *.mp4 ; do cp $x $WORKDIR/$c.mp4 ; ((c++)); done
 
 ## Calling all functions
 FUNC_CREATE_VIDEO_COLLAGE_3X3 "$WORKDIR" ;
+FUNC_CREATE_VIDEO_COLLAGE_3X2 "$WORKDIR" ;
+FUNC_CREATE_VIDEO_COLLAGE_2X2 "$WORKDIR" ;
