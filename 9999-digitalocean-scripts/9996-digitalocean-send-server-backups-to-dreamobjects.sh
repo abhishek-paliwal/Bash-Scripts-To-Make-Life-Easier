c4c4863fc0a34c90b0da0c3c08c4a57f4a240610
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-02-11 21:43:54 +0200

    new server backup program added

---
+++ 9999-digitalocean-scripts/9996-digitalocean-send-server-backups-to-dreamobjects.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+THIS_SCRIPT_NAME="$(basename $0)" ;
+THIS_SCRIPT_NAME_SANS_EXTENSION="$(echo $THIS_SCRIPT_NAME | sed 's/\.sh//g')" ;
+
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## CREATING SCRIPT USAGE FUNCION AND CALLING IT VIA '--help'
+usage()
+{
+cat <<EOM
+USAGE: $(basename $0)
+    ################################################################################
+    ## USAGE:
+    #### > bash $THIS_SCRIPT_NAME
+    ################################################################################
+    ## This program copies all backup zip files to a dedicated dreamobjects bucket.
+    ## This program uses rclone too. Install it via linuxbrew.
+    #######################################
+    ## IMPORTANT NOTE: This program should only be run on DIGITALOCEAN SERVER.
+    ################################################################################
+    ## CREATED BY: PALI
+    ## CREATED ON: 2022-02-11
+    ################################################################################
+EOM
+
+exit 0 ## EXITING IF ONLY USAGE IS NEEDED
+}
+## Calling the usage function
+if [ "$1" == "--help" ] ; then usage ; fi
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+##############################################################################
+## SETTING VARIABLES
+WORKDIR="$DIR_Y/_OUTPUT_$THIS_SCRIPT_NAME_SANS_EXTENSION" ;
+mkdir -p $WORKDIR ; ## create dir if not exists
+echo "##########################################" ; 
+echo "## PRESENT WORKING DIRECTORY = $WORKDIR" ;
+echo "##########################################" ; 
+
+DIR_LOCAL_BACKUP="/home/00-BACKUPS-BY-PALI/" ; 
+BUCKET_REMOTE_BACKUP="dreamobjects:private-digitalocean-backups" ;
+
+## COPYING ALL ZIP FILES ONE BY ONE TO REMOTE BUCKET
+for myfile in $DIR_LOCAL_BACKUP/*.zip ; do
+    rclone copy -i "$myfile" "$BUCKET_REMOTE_BACKUP" ; 
+done 
+## LISTING REMOTE DIRECTORY STRUCTURE TO CHECK
+rclone lsd "$BUCKET_REMOTE_BACKUP" ;
+##############################################################################
+
