bf32cd0a6998d1af8b67957ed36f7b24c3a6510a
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-03-04 13:57:21 +0200

    Updated on AP-MBP - Fri Mar  4 13:57:21 EET 2022

--- 002-mini-scripts/00207_pali_print_video_and_image_dimensions_using_ffprobe.sh
+++ 002-mini-scripts/00207_pali_print_video_and_image_dimensions_using_ffprobe.sh
@@ -1,3 +1,7 @@
 #/bin/bash
 
-for x in $(find . -type f) ; do myvar=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 $x) ; echo "$myvar <== $x" ; done | sort -n
+echo ">> Printing dimensions of videos (mp4/mkv/webm) and photos (jpg/png/jpeg) in current directory ..." ; 
+echo; 
+echo "##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" ;
+for x in $(fd -I -t f -e jpg -e png -e jpeg -e mp4 -e mkv -e webm) ; do myvar=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 $x) ; echo "$myvar <== $x" ; done | sort -n 
+echo "##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" ;
