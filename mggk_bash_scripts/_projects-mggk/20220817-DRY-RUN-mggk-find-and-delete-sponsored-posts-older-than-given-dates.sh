b95c178649d626dc4467508c86e2ea34f7e9ad54
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-08-18 14:09:09 +0300

    Update 20220817-DRY-RUN-mggk-find-and-delete-sponsored-posts-older-than-given-dates.sh

--- mggk_bash_scripts/_projects-mggk/20220817-DRY-RUN-mggk-find-and-delete-sponsored-posts-older-than-given-dates.sh
+++ mggk_bash_scripts/_projects-mggk/20220817-DRY-RUN-mggk-find-and-delete-sponsored-posts-older-than-given-dates.sh
@@ -17,6 +17,7 @@ USAGE: $(basename $0)
     ## This program finds and moves the sponsored posts mdfiles which are first published before 
     ## given dates. To change those dates, open this program and change the 
     ## variable known as DONT_DELETE_POSTS_WITH_THESE_DATES
+    ## This program only moves those posts which are not to be kept forever. Hence, it's safe.
     ################################################################################
     ## CREATED BY: PALI
     ## CREATED ON: 2022-08-17
@@ -38,29 +39,29 @@ echo "## PRESENT WORKING DIRECTORY = $WORKDIR" ;
 echo "##########################################" ; 
 ####
 INDIR="$DIR_GITHUB/2019-HUGO-MGGK-WEBSITE-OFFICIAL/content/blog/99_uncategorized" ;
+INITIAL_NUMBER_OF_FILES_FOUND_IN_INDIR=$(ls $INDIR | wc -l) ; 
 TMPFILE1="$WORKDIR/_tmpSpon1.txt" ; 
 TMPFILE2="$WORKDIR/_tmpSpon2.txt" ; 
 TMPFILE3="$WORKDIR/_tmpSpon3.txt" ;
-TMPFILE4="$WORKDIR/_tmpSpon4.txt" ; 
-##  
-DONT_DELETE_POSTS_WITH_THESE_DATES="(2022-|2021-|2020-12|2020-11|2020-10|2020-09)" ; ## Change this accordingly as you please
-##
 
 ################################################################################
-## finding the first published dates for the posts other than DONT_DELETE_POSTS_WITH_THESE_DATES
+## Finding the first published dates for the posts other than DONT_DELETE_POSTS_WITH_THESE_DATES
+DONT_DELETE_POSTS_WITH_THESE_DATES="(2022-|2021-|2020-12|2020-11|2020-10|2020-09)" ; ## Change this accordingly as you please
+##
 echo; echo "################################################################################" ;  
 echo ">> FINDING THE FIRST PUBLISHED DATES FOR THE POSTS OTHER THAN GIVEN DATES ..." ; 
-echo > $TMPFILE1 ; 
-for x in $(fd --search-path="$INDIR") ; do 
+rm $TMPFILE1 ; ## remove if already exists
+## Loop through all the posts which are not to be kept forever
+for x in $(grep -irL 'keep_this_post_forever: yes' $INDIR/*.md) ; do 
     ## echo $x ; 
     ack 'first_published_on' $x | grep -ivE "$DONT_DELETE_POSTS_WITH_THESE_DATES" >> $TMPFILE1 ; 
 done
-## sorting the output
-sort -n $TMPFILE1 | grep -iv '^$' > $TMPFILE2 ; 
+## Sorting the output
+sort -nr $TMPFILE1 | grep -iv '^$' > $TMPFILE2 ; 
 ################################################################################
 
-## reading the file line by line and finding the corresponding md files locations
-echo > $TMPFILE3 ; 
+## Reading the file line by line and finding the corresponding md files locations
+rm $TMPFILE3 ; ## remove if already exists
 count=1; 
 while read line ; do 
     echo "##------------------------------------------------------------------------------" ; 
@@ -75,36 +76,31 @@ echo ">> MOVING FILES FROM ORIGINAL DIRECTORY TO THE WORKDIR ..." ;
 countA=1;
 while read line_filepath ; do 
     echo "--------"  ;
-    echo "$countA >>>> CURRENT FILEPATH = $line_filepath" ;  
-    cp "$line_filepath" $WORKDIR/ ;
+    echo "$countA >>>> CURRENT FILEPATH = $line_filepath" ; 
+  ####
+    ## FOR DRY RUN, USE cp command. FOR WET RUN, USE mv command. Comment-out the other one.
+    cp "$line_filepath" $WORKDIR/ ; #Dry run
+    #mv "$line_filepath" $WORKDIR/ ; #Wet run
+  ####
     ((countA++)) ;  
 done < $TMPFILE3
-
-################################################################################
-## finding posts to keep forever because we would need to move them back (meaning undelete them)
-echo; echo "################################################################################" ;  
-echo > $TMPFILE4 ; 
-ag 'keep_this_post_forever: yes' -l $WORKDIR >> $TMPFILE4 ; 
-echo ">> DISPLAYING POSTS WITH KEEP FOREVER TAG BECAUSE THEY NEEDED TO BE MOVED BACK (MEANING UNDELETE THEM) ..." ; 
-cat $TMPFILE4 ; 
-echo "################################################################################" ;  
 
 ################################################################################
 ## SUMMARY
 echo; echo "################################################################################" ;  
 echo ">> SUMMARY ==>" ;
-echo "  $(ls $INDIR | wc -l) = TOTAL_FILES_FOUND in DIR $INDIR (before running the program)" ; 
+echo "  $INITIAL_NUMBER_OF_FILES_FOUND_IN_INDIR = TOTAL_FILES_FOUND BEFORE in DIR $INDIR (before running the program)" ; 
 echo "  $(cat $TMPFILE3 | grep -iv '^$' | wc -l) = VALID_FILES_FOUND (older than the dates given)" ; 
-echo "  $(cat $TMPFILE4 | grep -iv '^$' | wc -l) =  FILES_MOVED_BACK (with keep forever tag)" ; 
-echo "  $(ls $INDIR | wc -l) = TOTAL_FILES_FOUND in DIR => $INDIR (after running the program)" ; 
+echo "  $(ls $INDIR | wc -l) = TOTAL_FILES_FOUND AFTER in DIR => $INDIR (after running the program)" ; 
 
 ################################################################################
 ## FINAL IMPORTANT NOTE
 echo;
 echo "################################################################################" ;  
 echo "################################################################################" ;  
-echo ">> IMPORTANT NOTE: " ;
+echo ">> IMPORTANT NOTES: " ;
+echo ">>>> This is the dry run of the program. It will only copy the found mdfiles." ; 
+echo ">>>> For the wet run, meaning to actually move the files, comment-out the dry-run cp command on line-82, and uncomment the wet-run mv command on line-83." ; 
 echo ">>>> Immediately after running this program, check the Github Desktop in the MGGK HUGO DIR that no unnecessary files have been deleted or moved. This is very important."
-echo ">>>> This is the dry run of the program." ; 
 echo "################################################################################" ;  
 echo "################################################################################" ;  
