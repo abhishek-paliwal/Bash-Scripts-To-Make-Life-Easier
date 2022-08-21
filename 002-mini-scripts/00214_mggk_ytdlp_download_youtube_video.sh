cf01eb0f9ac2f4324e4fa640d4c1da33dbc7ff48
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-08-21 13:19:57 +0300

    Updated on AP-MBP.local - Sun Aug 21 13:19:57 EEST 2022

--- 002-mini-scripts/00214_mggk_ytdlp_download_youtube_video.sh
+++ 002-mini-scripts/00214_mggk_ytdlp_download_youtube_video.sh
@@ -3,7 +3,7 @@
 myfunc () { 
     echo "[VIDEO DOWNLOAD] Enter youtube/facebook video URL or ID:" ; 
     read videoURL ; 
-    yt-dlp --merge-output-format mp4/mkv $videoURL 
+    yt-dlp --merge-output-format mp4 $videoURL 
 } ; 
     
 myfunc
