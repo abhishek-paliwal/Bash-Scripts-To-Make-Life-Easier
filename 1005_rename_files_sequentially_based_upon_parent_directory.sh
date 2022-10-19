6e262119786dd81478dc18954edd0df4c18d8c26
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-10-19 20:27:30 +0300

    Updated on LAPTOP-F0AJ6LBG - Wed Oct 19 20:27:30 EEST 2022

---
+++ 1005_rename_files_sequentially_based_upon_parent_directory.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+##############################################################################
+## rename files based upon their parent directory
+function FUNC_RENAME_FILES_SEQUENTIALLY () {
+    c=$1 ;
+    c1=$( printf '%03d' $c) ; 
+
+    myfullpath="$(dirname $x)" ;
+    myDirname="$(basename $myfullpath)" ;  
+
+    x_base="$(basename $x)" ;
+    x_newname="$myDirname-$c1.jpg" ; 
+
+    echo "  ORIG = $x_base" ; 
+    echo "  NEW = $x_newname" ; 
+
+    echo ; echo ; echo ">>>> MOVING ... ($x_base => $x_newname)" ; 
+    mv $x_base $x_newname ; 
+}
+##############################################################################
+##############################################################################
+## RENAME ALL JPG FILES
+count=0;
+for x in $(fd -e jpg -t f --search-path="$(pwd)" | grep -iv 'publish'); do
+    ((count++))
+    FUNC_RENAME_FILES_SEQUENTIALLY $count ;
+done
