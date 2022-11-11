9a8ea759b6ec744949f898bb8a1f9367fd2e18fa
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-11-11 15:57:45 +0200

    Updated on LAPTOP-F0AJ6LBG - Fri Nov 11 15:57:45 EET 2022

---
+++ mggk_bash_scripts/_projects-mggk/20221111-properly-rename-jpg-images-in-onedrive-with-directory-names.sh
@@ -0,0 +1,35 @@
+#/bin/bash
+
+#############################################################################
+## THIS PROGRAM RENAMES ALL JPG IMAGES PRESENT IN SUB-DIRECTORIES IN A GIVEN 
+## DIRECTORY BASED UPON THEIR PARENT DIRECTORY NAME. 
+#############################################################################
+
+DIR="/mnt/d/OneDrive/00-MGGK-ARCHIVED-VIDEOS-PHOTOS/2014-2018-Recipe-Photos/Baking" ; 
+INFILE="$DIR_Y/tmp.txt" ; 
+
+## listing dirs with the desired images names
+fd -tf 'img|dsc|anu-' --search-path="$DIR"  ; 
+fd -tf 'img|dsc|anu-' --search-path="$DIR" -x dirname {} | sort -u > $INFILE ; 
+
+echo "##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" ; 
+cat $INFILE ; 
+echo "##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" ; 
+
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function FUNC_ACTUALLY_RENAME_FILES () {
+####
+    for x in $(cat $INFILE | head -27) ; do 
+        echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>";  
+        cd $x ; 
+        ls $x/* ; 
+        bash $REPO_SCRIPTS/1005_rename_jpg_png_images_sequentially_based_upon_parent_directory.sh ; 
+        ls $x/* ; 
+        cd $DIR ; 
+        sleep 2; 
+done
+####
+}
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Call function
+FUNC_ACTUALLY_RENAME_FILES
