391b8ca9ee68d3e5ee6098157c504bb6a52b7874
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2023-01-14 22:47:31 +0200

    Update 00215-extract_parent_urls_from_linkchecker_html_RESULTS_page_on_VPS.sh

--- 002-mini-scripts/00215-extract_parent_urls_from_linkchecker_html_RESULTS_page_on_VPS.sh
+++ 002-mini-scripts/00215-extract_parent_urls_from_linkchecker_html_RESULTS_page_on_VPS.sh
@@ -1,3 +1,9 @@
 #/bin/bash
 
-cdy; linkchecker_results_url="https://vps.abhishekpaliwal.com//scripts-html-outputs/20221207-linkchecker-out-mggk.html" ; curl -k "$linkchecker_results_url" | grep -i 'parent' | grep -ioE 'http.*"' | sd '"' ''  > _OUTPUT_00215-extract_parent_urls_from_linkchecker_html_RESULTS_page_on_VPS.txt
+cdy; 
+
+linkchecker_results_url_page="https://vps.abhishekpaliwal.com//scripts-html-outputs/20221228-linkchecker-out-mggk.html" ; 
+
+echo ">> CURRENT RESULTS URL PAGE IS: $linkchecker_results_url_page" ; 
+
+curl -k "$linkchecker_results_url_page" | grep -i 'parent' | grep -ioE 'http.*"' | sd '"' ''  > $DIR_Y/_OUTPUT_00215-extract_parent_urls_from_linkchecker_html_RESULTS_page_on_VPS.txt
