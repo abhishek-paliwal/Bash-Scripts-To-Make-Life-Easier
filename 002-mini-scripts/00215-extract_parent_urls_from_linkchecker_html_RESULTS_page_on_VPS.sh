698e609c75ad32ec03aeb7766368a2b4f7ee707a
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2023-01-14 23:06:00 +0200

    Update 00215-extract_parent_urls_from_linkchecker_html_RESULTS_page_on_VPS.sh

--- 002-mini-scripts/00215-extract_parent_urls_from_linkchecker_html_RESULTS_page_on_VPS.sh
+++ 002-mini-scripts/00215-extract_parent_urls_from_linkchecker_html_RESULTS_page_on_VPS.sh
@@ -1,9 +1,24 @@
 #/bin/bash
+################################################################################
+## THIS SCRIPT EXTRACTS PARENT URLS FROM LINKCHECKER RESULTS PAGE PRESENT ON THE VPS
+################################################################################
 
-cdy; 
+cd $DIR_Y ; 
 
-linkchecker_results_url_page="https://vps.abhishekpaliwal.com//scripts-html-outputs/20221228-linkchecker-out-mggk.html" ; 
+## SELECT THE RESULTS PAGE FROM THE FZF DROPDOWN
+#linkchecker_results_url_page="https://vps.abhishekpaliwal.com//scripts-html-outputs/20221228-linkchecker-out-mggk.html" ; 
+linkchecker_results_url_page=$(curl -k https://vps.abhishekpaliwal.com/tree.html |  grep -i 'linkchecker' | grep -io 'https.*.html"' | sd '"' '' | fzf) ; 
 
-echo ">> CURRENT RESULTS URL PAGE IS: $linkchecker_results_url_page" ; 
+## FINALLY EXTRACTING PARENT URLS FROM CHOSEN RESULTS PAGE
+echo; echo; 
+echo "##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"; 
+echo ">> CURRENT RESULTS URL PAGE IS: $linkchecker_results_url_page" ;
+echo "##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"; 
+OUTPUT_FILE="$DIR_Y/_OUTPUT_00215-extract_parent_urls_from_linkchecker_html_RESULTS_page_on_VPS.txt" ; 
+curl -k "$linkchecker_results_url_page" | grep -i 'parent' | grep -ioE 'http.*"' | sd '"' ''  > "$OUTPUT_FILE"
+echo "##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"; 
+echo ">> OUTPUT FILE SAVED AT: $OUTPUT_FILE" ;
+echo "##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"; 
 
-curl -k "$linkchecker_results_url_page" | grep -i 'parent' | grep -ioE 'http.*"' | sd '"' ''  > $DIR_Y/_OUTPUT_00215-extract_parent_urls_from_linkchecker_html_RESULTS_page_on_VPS.txt
+echo ">> OUTPUT IS ALSO PRINTED BELOW ..." ; 
+cat "$OUTPUT_FILE"  ;
