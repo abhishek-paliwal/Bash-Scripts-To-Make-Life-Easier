e7433292775a4fa7772a849a8e8362eb335f229e
Abhishek <abhishek@concepro.com>
2023-02-06 17:01:32 +0000

    Updated on LAPTOP-UDJH3V8M - Mon Feb  6 19:01:32 EET 2023

---
+++ 002-mini-scripts/00217_join_mp3_files_sequentially_using_ffmpeg.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+################################################################################
+## THIS SCRIPT JOINS MP3 FILES FOUND IN PWD, USING FFMPEG
+################################################################################
+
+echo "################################################################################" ; 
+WORKING_DIR="$(pwd)" ; 
+echo ">> PRESENT WORKING DIR = $WORKING_DIR" ; 
+echo ">> ARE YOU IN THE DIRECTORY WHERE THE MP3 FILES ARE PRESENT? IF yes, then ..." ;
+echo "################################################################################" ; 
+
+TMPLIST_FILE="$WORKING_DIR/_TMP_MP3_LIST_ALL.TXT" ; 
+rm $TMPLIST_FILE ; ## if present already
+
+## Listing mp3 files
+for f in $(fd -e mp3 -e MP3 | sort -V) ;
+    do
+    echo "file '$f'" >> $TMPLIST_FILE ;
+    done
+
+##
+echo ">> THESE mp3 FILES WILL BE JOINED IN THIS ORDER ..." ; 
+cat  $TMPLIST_FILE ; 
+
+## FINALLY SOME FFMPEG CONCATENATE MAGIC, AND CREATING FINAL FILE
+mkdir _TMP_DIR ;
+how_many_mp3=$(fd -e mp3 -e MP3 | wc -l | tr ' ' '') ; 
+ffmpeg -f concat -safe 0 -i $TMPLIST_FILE -c copy _TMP_DIR/joined-$how_many_mp3-files-FULL-FINAL-AUDIO.mp3
+
