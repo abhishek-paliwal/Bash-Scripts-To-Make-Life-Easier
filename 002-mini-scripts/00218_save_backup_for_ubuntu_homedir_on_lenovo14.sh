ae9366e18ec91709bd224819cf4ef939d247dff4
Abhishek <abhishek@concepro.com>
2023-02-07 21:11:59 +0000

    Updated on LAPTOP-UDJH3V8M - Tue Feb  7 23:11:59 EET 2023

--- 002-mini-scripts/00218_save_backup_for_ubuntu_homedir_on_lenovo14.sh
+++ 002-mini-scripts/00218_save_backup_for_ubuntu_homedir_on_lenovo14.sh
@@ -9,5 +9,13 @@ mkdir -p "$DIR_TODAY" ;
 
 ## RSYNC COMMAND TO RUN
 echo ">> TIME_START: $(date)" ; 
-rsync -a --info=progress2 --exclude="lost+found" --exclude=".cache" --exclude="GitHub" --exclude=".vscode-server" /home/ubuntu/ $DIR_TODAY/ ;
+rsync -a --info=progress2 --exclude-from="/home/ubuntu/rsync_exclusion_file.txt" /home/ubuntu/ $DIR_TODAY/ ;
 echo ">> TIME_END: $(date)" ;
+
+## CREATING A ZIP FILE FROM THIS ARCHIVE
+echo ">> CREATING A ZIP FILE ..." ; 
+zip -r $DIR_TODAY.zip $DIR_TODAY
+
+## LIST CONTENTS OF BACKUP DIR
+echo ">> LISTING CONTENTS OF BACKUP DIR ..." ; 
+ls -al $DIR_BACKUP ; 
