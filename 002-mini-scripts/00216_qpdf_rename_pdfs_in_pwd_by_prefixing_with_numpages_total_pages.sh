2d4ee4f1684c0216013a6dc6b286b0b6659adc7e
Abhishek <abhishek@concepro.com>
2023-02-10 17:52:24 +0000

    Updated on LAPTOP-UDJH3V8M - Fri Feb 10 19:52:24 EET 2023

--- 002-mini-scripts/00216_qpdf_rename_pdfs_in_pwd_by_prefixing_with_numpages_total_pages.sh
+++ 002-mini-scripts/00216_qpdf_rename_pdfs_in_pwd_by_prefixing_with_numpages_total_pages.sh
@@ -4,16 +4,34 @@
 ## THIS SCRIPT USES qpdf program, can be installed by > brew install qpdf
 
 
+## Exit if no pdf found 
+pdfarray=( $(ls *.pdf) ) ; 
+if [[ ${#pdfarray[@]} -gt 0 ]] ; then 
+    echo ">> SUCCESS: PDFs found. The program will continue ..." ; 
+else
+    echo ">> FAILURE: No PDFs found. The program will exit now." ; 
+    exit ; 
+fi 
+
+#####################################################################
+
+## SET VARIABLES
 DIR_RENAMED="_TMP_RENAMED_PDFS" ;
-mkdir -p $DIR_RENAMED ; 
+DIR_ORIGINAL="_TMP_ORIGINAL_PDFS" ;
+mkdir -p $DIR_RENAMED $DIR_ORIGINAL ; 
 
 for x in *.pdf ; do 
-echo "##============================================" ; 
-totalPages=$(qpdf --show-npages $x) ; 
-echo "$totalPages =  Total pages in // $x" ;
-echo "	>> RENAMING PDF = > $x" ;  
-mv "$x" "$DIR_RENAMED/$(printf "NumPages_" "%03i_%s" "$totalPages" "$x")" ; 
-done ; 
+    echo "##============================================" ; 
+    totalPages=$(qpdf --show-npages $x) ; 
+    echo "$totalPages =  Total pages in // $x" ;
+    ##
+    echo "  >> Copying original (just in case)..." ; 
+    cp "$x" $DIR_ORIGINAL/ ; 
+    ##
+    echo "	>> RENAMING PDF = > $x" ;  
+    NAME_PREFIX="Numpages-$(printf "%03i%s" "$totalPages")" ; 
+    cp "$x" "$DIR_RENAMED/$NAME_PREFIX-$(basename $x)" ; 
+done 
 
 echo; 
 echo ">> File tree list = >" ; 
