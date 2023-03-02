156c8eec7f71cf9037a5c2e2d60a6b41738112a3
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2023-03-02 17:58:54 +0200

    Updated on AP-MBP.local - Thu Mar  2 17:58:54 EET 2023

--- 10D_create_OUR_LOGOS_indexes.sh
+++ 10D_create_OUR_LOGOS_indexes.sh
@@ -50,6 +50,8 @@ echo "<html lang='en'><head>
 <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
 
     <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+    <meta name='robots' content='noindex'>
+    
     <link rel='icon' href='https://downloads.concepro.com/favicon.ico'>
 
     <title>Index of All Our Logos // Concepro Agency</title>
