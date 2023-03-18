15f29253d4dd246665eb5a02be0fbf8881f432c2
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2023-03-18 21:20:31 +0200

    awgp project updated

--- mggk_bash_scripts/_projects-mggk/20230314-awgp-gayatri-pariwar-data_extraction_to_download_pdfs_and_htmls-project/_002_miller_input_outputs/002_run_this_miller_script_to_convert_plain_links_csv_to_proper_hyperlinks_csv.sh
+++ mggk_bash_scripts/_projects-mggk/20230314-awgp-gayatri-pariwar-data_extraction_to_download_pdfs_and_htmls-project/_002_miller_input_outputs/002_run_this_miller_script_to_convert_plain_links_csv_to_proper_hyperlinks_csv.sh
@@ -1,10 +1,11 @@
 #/bin/bash
 
+
 echo ">> THIS PROGRAM'S JOB IS DONE. SO IT WILL SIMPLY EXIT NOW WITHOUT DOING ANYTHING FURTHER. REMOVE THE EXIT BLOCK IF NEEDED." ;
 exit ; ## remove this line to run again.
 
-input_csv="awgp_merged_full_book_catalog.csv" ; 
-output_csv="awgp_merged_full_book_catalog_with_hyperlinks.csv" ; 
+input_csv="v0_awgp_merged_full_book_catalog.csv" ; 
+output_csv="v0_awgp_merged_full_book_catalog_with_hyperlinks.csv" ; 
 
 ## Calculate : put new columns based upon old columns
 ## convert normal link column values to proper html hyperlinks
@@ -21,6 +22,25 @@ mlr --csv --ifs ';' --ofs ';' --from _tmp01.csv cut -f BOOK_SERIAL_NUMBER,BOOK_N
 
 ## rename the new calculated column names to same as the old names
 mlr --csv --ifs ';' --ofs ';' --from _tmp02.csv rename BOOK_URL_NEW,BOOK_URL,BOOK_URL_HOME_NEW,BOOK_URL_HOME,PDF_URLS_NEW,PDF_URLS,NUM_MAX_PAGES_NEW,NUM_MAX_PAGES > "$output_csv" ; 
+
+
+##############################################################################
+## Adding extra columns to csv files = google translated english booknames ( => 99_english_translated_booknames.csv)
+##############################################################################
+mlr --csv --ifs ';' --ofs ';' join -u -j BOOK_SERIAL_NUMBER -f $input_csv 99_english_translated_booknames.csv > _tmp_n1.csv
+mlr --csv --ifs ';' --ofs ';' join -u -j BOOK_SERIAL_NUMBER -f $output_csv 99_english_translated_booknames.csv > _tmp_n2.csv
+
+## reordering columns
+mlr --csv --ifs ';' --ofs ';' reorder -f BOOK_SERIAL_NUMBER,BOOK_TYPE,BOOK_NAME,BOOK_NAME_TRANSLATED_ENGLISH _tmp_n1.csv > _tmp_n1_1.csv
+mlr --csv --ifs ';' --ofs ';' reorder -f BOOK_SERIAL_NUMBER,BOOK_TYPE,BOOK_NAME,BOOK_NAME_TRANSLATED_ENGLISH _tmp_n2.csv > _tmp_n2_1.csv
+
+## Renaming
+v1_input_csv="v1_awgp_merged_full_book_catalog.csv" ; 
+v1_output_csv="v1_awgp_merged_full_book_catalog_with_hyperlinks.csv" ; 
+mv _tmp_n1_1.csv _dropbox_files/$v1_input_csv ; 
+mv _tmp_n2_1.csv _dropbox_files/$v1_output_csv ;
+##############################################################################
+
 
 ################################################################################
 ## Make html tables from all csv files in pwd
