8203cbb2b3f5e165daa15156463f4540a7e79c41
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2023-03-19 20:33:52 +0200

    Updated on LAPTOP-F0AJ6LBG - Sun Mar 19 20:33:52 EET 2023

--- 002-mini-scripts/00205_add_date_to_bash_aliases.sh
+++ 002-mini-scripts/00205_add_date_to_bash_aliases.sh
@@ -2,13 +2,15 @@
 
 func_add_date_to_BA () {
     myTMPFILE="/tmp/tmp1.txt" ;     
-    BA_FILE="$HOME/.bash_aliases" ;     
+    BA_FILE="$REPO_DOTFILES/_00_common_files/.bash_aliases" ;     
     BACKUP_BA_FILE="$HOME/.bash_aliases.BACKUP" ;     
     echo "###### FILE LAST EDITED: $(date) ######" > $myTMPFILE  ;     
     cat $myTMPFILE $BA_FILE > $BACKUP_BA_FILE ;     
-    rm $BA_FILE ;     
+    #rm $BA_FILE ;     
     cp $BACKUP_BA_FILE $BA_FILE ;    
     echo "BA_FILE = $BA_FILE // BACKUP = $BACKUP_BA_FILE" ; 
 } ; 
     
 func_add_date_to_BA ; 
+
+
