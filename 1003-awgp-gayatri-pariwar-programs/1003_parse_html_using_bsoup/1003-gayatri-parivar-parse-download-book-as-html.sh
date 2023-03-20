1e1ad02fd66f0aa4ae84dae14bacf9a047c155c0
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2023-03-20 23:08:59 +0200

    Update 1003-gayatri-parivar-parse-download-book-as-html.sh

--- 1003-awgp-gayatri-pariwar-programs/1003_parse_html_using_bsoup/1003-gayatri-parivar-parse-download-book-as-html.sh
+++ 1003-awgp-gayatri-pariwar-programs/1003_parse_html_using_bsoup/1003-gayatri-parivar-parse-download-book-as-html.sh
@@ -2,20 +2,20 @@
 ################################################################################
 ## THIS PROGRAM DOWNLOAD WEBPAGES AND PARSES THEM INTO A COMBINED HTML FILE RESUTING IN A SINGLE PAGE BOOK
 ################################################################################
-
 THIS_SCRIPT_NAME="$(basename $0)" ;
 THIS_SCRIPT_NAME_SANS_EXTENSION="$(echo $THIS_SCRIPT_NAME | sed 's/\.sh//g')" ;
 ####
+
 WORKDIR="$DIR_Y/_output_$THIS_SCRIPT_NAME_SANS_EXTENSION" ; 
 mkdir -p $WORKDIR ; 
 cd $WORKDIR ; 
 ####
 inFileCSV="$CSV_AWGP" ## get from env variable
 searchResultCSV="$WORKDIR/_tmp_searchResult.csv" ;
+HTML_OUTPUT="$WORKDIR/_FINAL_RESULT.html" ;
 DIR_PROJECT="$REPO_SCRIPTS_AWGP/1003_parse_html_using_bsoup" ; 
 DIR_OUTPUT="$WORKDIR" ; 
 tmpConfigFile="$WORKDIR/_tmp_config_file.txt" ; 
-echo > $tmpConfigFile ; ## initializing
 ####
 
 ##------------------------------------------------------------------------------
@@ -31,18 +31,45 @@ echo > $tmpConfigFile ; ## initializing
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ## FUNCITON DEFINITIONS
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+FUNC_PRINT_FANCY_DIVIDER () {
+    source "$REPO_SCRIPTS_MINI/00200_print_fancy_divider.sh" ; 
+}
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 FUNC_SEARCH_VARS_BASED_UPON_NUM_MAXPAGES_URL () {
+    FUNC_PRINT_FANCY_DIVIDER ; 
     ## USER INPUT:
-    echo;  echo ">>>> PLEASE PROVIDE THE NUM MAXPAGES URL (OR press ENTER key to exit) =>" ;
-    read search_term ; 
-    echo;
-    ##
+    ########
+    echo ">> CONFIG FILE = $tmpConfigFile" ; 
+    ## DO NOT ASK FOR USER INPUT IF CONFIG FILE ALREADY EXISTS
+    if [[ -f "$tmpConfigFile" ]]; then
+        ## CHOOSE URL FROM CONFIG FILE
+        source "$tmpConfigFile" ## firstly, source the config file
+        echo ">> CONFIG FILE exists. URL will be chosen from the config file ..." ; 
+        search_url="$NUM_MAX_PAGES_URL" ## from the sourced config file
+    else
+        ## ASK FOR USER INPUT
+        echo; echo ">> CONFIG FILE does not exist. User will be asked for the URL ..." ; 
+        echo ">>>> PLEASE PROVIDE THE NUM MAXPAGES URL (OR press ENTER key to exit) =>" ;
+        read search_url ; echo;
+    fi
+    ########
+    echo ">> CHOSEN SEARCH URL IS = $search_url";
+    ## Exit the program if search url is empty.
+    if [ -z "$search_url" ]
+    then
+        echo "\$search_url is empty. Program will exit now." ; exit ; 
+    else
+        echo "\$search_url is NOT empty. Program will continue." ; 
+    fi
+    ########
+    echo "##+++++++++++++++++++++++++++++++++++++++" ; 
+    ########
     TMPFILE1="$WORKDIR/_tmp1.txt" ; 
     TMPFILE2="$WORKDIR/_tmp2.txt" ; 
     OUTFILE="$searchResultCSV" ; 
     ##
     head -1 $inFileCSV > $TMPFILE1
-    grep -i "$search_term" $inFileCSV > $TMPFILE2
+    grep -i "$search_url" $inFileCSV > $TMPFILE2
     cat $TMPFILE1 $TMPFILE2 > $OUTFILE
     ## Using miller command (mlr) to print the result in xtab format (c2x =  csv to xtab)
     echo ">>PRINTING THE FOUND BOOK DETAILS ..." ; 
@@ -62,9 +89,11 @@ FUNC_SEARCH_VARS_BASED_UPON_NUM_MAXPAGES_URL () {
 #NUM_MAX_PAGES
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 FUNC_GET_SOME_PARAM_VALUES () {
+    FUNC_PRINT_FANCY_DIVIDER ; 
+    echo; echo ">> RUNNING FUNCTION => FUNC_GET_SOME_PARAM_VALUES ..."  ; 
     inFile="$searchResultCSV" ;
-    ## consider first result line only
     ## PRIMARY VARIABLES
+    ## consider first result line only
     BOOK_SERIAL_NUMBER=$(mlr --csv --ifs ';' --headerless-csv-output cut -f BOOK_SERIAL_NUMBER then head -n 1 $inFile) ; 
     BOOK_TYPE=$(mlr --csv --ifs ';' --headerless-csv-output cut -f BOOK_TYPE then head -n 1 $inFile) ; 
     BOOK_NAME=$(mlr --csv --ifs ';' --headerless-csv-output cut -f BOOK_NAME then head -n 1 $inFile) ; 
@@ -76,24 +105,32 @@ FUNC_GET_SOME_PARAM_VALUES () {
     NUMPAGES_TO_EXTRACT=$(echo "$NUM_MAX_PAGES_URL" | awk -F '.' '{print $NF}') ; 
     BOOK_URL_PREFIX_WITH_DOT_IN_END=$(echo "$NUM_MAX_PAGES_URL" | awk -F '.' '{$NF=""; print} ' | sd ' ' '.') ; 
     LOCALPATH_PREFIX_WITH_DOT=$(echo "$NUM_MAX_PAGES_URL" | awk -F '.' '{$NF=""; print}' | awk -F '/' '{print "file://$DIR_OUTPUT/" $NF "."}' | sd ' ' '') ; 
+    URL_BOOK_PART=$(echo "$NUM_MAX_PAGES_URL" | awk -F '/' '{print $(NF-1)}') ; 
     ####
-    echo ">> PRINTING SECONDARY INPUT VARIABLES FOR PYTHON ... " ; 
-    echo $BOOKNAME // $NUMPAGES_TO_EXTRACT // $BOOK_URL_PREFIX_WITH_DOT_IN_END // $LOCALPATH_PREFIX_WITH_DOT ;
+    echo; echo ">> PRINTING SECONDARY INPUT VARIABLES FOR PYTHON ... " ; 
+    echo $BOOKNAME // $NUMPAGES_TO_EXTRACT // $BOOK_URL_PREFIX_WITH_DOT_IN_END // $LOCALPATH_PREFIX_WITH_DOT // $URL_BOOK_PART // $BOOK_SERIAL_NUMBER ;
+    echo; echo; 
 ####
 ####
 ## WRITE A TMP CONFIG FILE TO BE USED BY PYTHON FUNCTIONS
+echo > $tmpConfigFile ; ## initializing
 cat << EOF | tee -a "$tmpConfigFile"
-DIR_PROJECT="$REPO_SCRIPTS_AWGP/1003_parse_html_using_bsoup" ; 
-DIR_OUTPUT="$WORKDIR" ; 
+DIR_PROJECT="$REPO_SCRIPTS_AWGP/1003_parse_html_using_bsoup"
+DIR_OUTPUT="$WORKDIR"
+BOOK_SERIAL_NUMBER="$BOOK_SERIAL_NUMBER"
+NUM_MAX_PAGES_URL="$NUM_MAX_PAGES_URL" 
 BOOKNAME="Audiobook - $BOOKNAME"
 BOOK_URL_PREFIX_WITH_DOT_IN_END="$BOOK_URL_PREFIX_WITH_DOT_IN_END"
-LOCALPATH_PREFIX_WITH_DOT="$LOCALPATH_PREFIX_WITH_DOT" ;
-NUMPAGES_TO_EXTRACT=$NUMPAGES_TO_EXTRACT ;
+LOCALPATH_PREFIX_WITH_DOT="$LOCALPATH_PREFIX_WITH_DOT"
+NUMPAGES_TO_EXTRACT=$NUMPAGES_TO_EXTRACT
+URL_BOOK_PART="$URL_BOOK_PART"
+BOOKNAME_HTML_MD_FINAL="${BOOK_SERIAL_NUMBER}_XX_${URL_BOOK_PART}"
 EOF
 ####
 }
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 FUNC_DOWNLOAD_ALL_PAGES_LOCALLY () {
+    FUNC_PRINT_FANCY_DIVIDER ; 
     ## Firstly, sourcing the config file
     source "$tmpConfigFile" ; 
     ## download all pages one by one
@@ -103,6 +140,7 @@ FUNC_DOWNLOAD_ALL_PAGES_LOCALLY () {
 }
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 FUNC_RUN_PYTHON_PROGRAMS_FOR_OUTPUTS () {
+    FUNC_PRINT_FANCY_DIVIDER ; 
     ## Firstly, sourcing the config file
     source "$tmpConfigFile" ; 
     ## run the python bsoup program to extract the desired data from locally present html files
@@ -110,14 +148,15 @@ FUNC_RUN_PYTHON_PROGRAMS_FOR_OUTPUTS () {
     ## Check output
     python3 $python_program_path "$BOOKNAME" "$NUMPAGES_TO_EXTRACT" "$LOCALPATH_PREFIX_WITH_DOT" ; 
     ## Then run again to save output to html
-    python3 $python_program_path "$BOOKNAME" "$NUMPAGES_TO_EXTRACT" "$LOCALPATH_PREFIX_WITH_DOT" > $WORKDIR/_FINAL_RESULT.html ; 
+    python3 $python_program_path "$BOOKNAME" "$NUMPAGES_TO_EXTRACT" "$LOCALPATH_PREFIX_WITH_DOT" > "$HTML_OUTPUT" ; 
 }
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-FUNC_CALL_STEP01_FUNCTIONS () {
-    FUNC_DOWNLOAD_ALL_PAGES_LOCALLY ; 
-}
-FUNC_CALL_STEP02_FUNCTIONS () {
-    FUNC_RUN_PYTHON_PROGRAMS_FOR_OUTPUTS ; 
+FUNC_PANDOC_CONVERT_HTML_TO_MARKDOWN () {
+    FUNC_PRINT_FANCY_DIVIDER ; 
+    echo ">> RUNNING ... FUNC_PANDOC_CONVERT_HTML_TO_MARKDOWN" ; 
+    ## Firstly, sourcing the config file
+    source "$tmpConfigFile" ; 
+    pandoc --from=html --to=markdown_strict "$HTML_OUTPUT" -o "$BOOKNAME_HTML_MD_FINAL.md" ;
 }
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -133,12 +172,14 @@ while true
 do
     echo "1. Download HTMLs locally"
     echo "2. Parse local HTMLs using python"
-    echo "3. Exit prompt"
+    echo "3. Convert result HTML to markdown using pandoc"
+    echo "4. Exit prompt"
    read Input
    case "$Input" in
-        1) FUNC_CALL_STEP01_FUNCTIONS ;;
-        2) FUNC_CALL_STEP02_FUNCTIONS  ;;
-        3) exit
+        1) FUNC_DOWNLOAD_ALL_PAGES_LOCALLY ;;
+        2) FUNC_RUN_PYTHON_PROGRAMS_FOR_OUTPUTS  ;;
+        3) FUNC_PANDOC_CONVERT_HTML_TO_MARKDOWN  ;;
+        4) exit
    esac
 done
 echo "##+++++++++++++++++++++++++++++++++++++++" ; 
