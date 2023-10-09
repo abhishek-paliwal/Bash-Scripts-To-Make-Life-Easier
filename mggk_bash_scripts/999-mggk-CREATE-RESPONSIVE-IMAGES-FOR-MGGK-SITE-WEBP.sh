1ac10a57dd6ee489f190dc32877309c344cbb4ac
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2023-10-10 01:09:11 +0300

    Create 999-mggk-CREATE-RESPONSIVE-IMAGES-FOR-MGGK-SITE-WEBP.sh

---
+++ mggk_bash_scripts/999-mggk-CREATE-RESPONSIVE-IMAGES-FOR-MGGK-SITE-WEBP.sh
@@ -0,0 +1,91 @@
+#/bin/bash
+################################################################################
+## THIS PROGRAM READS READS JPG/PNG IMAGES FROM ORIGINAL CDN FOLDER AND
+## CREATES CORRESPONDING MISSING WEBP IMAGES IN ORIGINAL_WEBP DIRECTORY, AND ALSO
+## IN VARIOUS OTHER RESOLUTIONS.
+## NOTE: THIS PROGRAM USES COMMAND LINE TOOL: cwebp (INFO: https://developers.google.com/speed/webp/docs/cwebp)
+## DATE: 2023-10-10
+## CREATED_BY: PALI
+################################################################################
+
+ROOTDIR="$REPO_CDN/cdn.mygingergarlickitchen.com" ;
+ROOTDIR_WEBP="$ROOTDIR/images_webp" ; 
+DIR_IM="$ROOTDIR/images/original" ; 
+DIR_IM_WEBP="$ROOTDIR/images_webp/original" ; 
+##
+tmpfile1="$DIR_Y/tmpfile1.txt" ;
+tmpfile2="$DIR_Y/tmpfile2.txt" ;   
+tmpfile_compare="$DIR_Y/tmpfile_compare.txt"
+
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function FUNC_PRINTDIVIDER () {
+    echo "##------------------------------------------------------------------------------" ; 
+}
+####
+function FUNC1_WEBP_COMPARE_AND_COPY_IMAGES_TO_WEBP_DIRECTORY () {
+    echo ">> RUNNING FUNCTION : $FUNCNAME" ; 
+    fd -HIt f -e png -e jpg --search-path="$DIR_IM" -x echo {/.} | sort -u > "$tmpfile1" ; 
+    fd -HIt f -e webp --search-path="$DIR_IM_WEBP" -x echo {/.}  | sort -u > "$tmpfile2" ; 
+    ## comparing and listing differences 
+    diff "$tmpfile1" "$tmpfile2"  | grep -i '<' | sd '< ' '' > "$tmpfile_compare" ;
+    ## 
+    echo ">> NOTE: These images will be converted to WEBP ... (will be blank if no image differences are found.)" ; 
+    cat -n "$tmpfile_compare" ; 
+}
+####
+function FUNC2_WEBP_CREATE_ORIGINAL_WEBP_IMAGES_FROM_ORIGINAL_JPG_PNG () {
+    echo ">> RUNNING FUNCTION : $FUNCNAME" ;
+    INFILE="$tmpfile_compare" ; 
+    INDIR="$DIR_IM" ; 
+    dir2make="$DIR_IM_WEBP" ;
+    mkdir -p "${dir2make}" ; 
+    echo "Created directory, if does not exist already ... (for webp images): $dir2make" ;
+    ####
+    for filename_sans_extn in $(cat $INFILE) ; do 
+        echo ; 
+        fd -HIt f "${filename_sans_extn}" --search-path="$INDIR" -x cwebp -short -progress {} -o "$dir2make/${file%.*}.webp" ;
+        #current_file="$INDIR/${filename_sans_extn}.webp" ; 
+        #file=$(basename $current_file) ; 
+        #cwebp -short -progress "$current_file" -o "$dir2make/${dir2make}-${file%.*}.webp" ; 
+    done
+    ####
+}
+####
+function FUNC3_WEBP_RESIZE_ORIGINAL_WEBP_IMAGES_TO_VARIOUS_SIZES () {
+    echo ">> RUNNING FUNCTION : $FUNCNAME" ;
+    ## resizing webp images to various resolutions using 'cwebp' command
+    # Declare an array with directory names
+    directories=("350" "425" "550" "675" "800") ; 
+
+    # Loop through the array and create the directories
+    for dir in "${directories[@]}"; do
+        INFILE="$tmpfile_compare" ; 
+        INDIR="$DIR_IM_WEBP" ; 
+        dir2make="$ROOTDIR_WEBP/${dir}px" ;
+        mkdir -p "${dir2make}" ; 
+        echo "Created directory, if does not exist already ... (for webp images): $dir2make" ;
+        ####
+        #for filename in $(fd -HIt f -e webp --search-path="$INDIR") ; do 
+        for filename_sans_extn in $(cat $INFILE) ; do 
+            echo ; 
+            current_file="$INDIR/${filename_sans_extn}.webp" ; 
+            file=$(basename $current_file) ; 
+            # Example command: cwebp input_file -o output_file -resize 600 0 ; ## 0 is needed to resize to 600px wide keeping aspect ratio intact.
+            cwebp -short -progress "$current_file" -o "$dir2make/${dir}px-${file%.*}.webp" -resize "$dir" 0 ; 
+        done
+        ####
+    done
+}
+####
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+## RUN FUNCTIONS IN SEQUENCE
+FUNC_PRINTDIVIDER ; 
+FUNC1_WEBP_COMPARE_AND_COPY_IMAGES_TO_WEBP_DIRECTORY ;
+FUNC_PRINTDIVIDER ; 
+FUNC2_WEBP_CREATE_ORIGINAL_WEBP_IMAGES_FROM_ORIGINAL_JPG_PNG ; 
+FUNC_PRINTDIVIDER ; 
+FUNC3_WEBP_RESIZE_ORIGINAL_WEBP_IMAGES_TO_VARIOUS_SIZES ; 
+FUNC_PRINTDIVIDER ; 
