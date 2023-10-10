a8881573b875066b5a6d78170198c69e98b04d02
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2023-10-10 14:35:24 +0300

    Update 00225_print_images_dimensions_and_filesizes_jpg_png_using_imagemagick_in_pwd.sh

--- 002-mini-scripts/00225_print_images_dimensions_and_filesizes_jpg_png_using_imagemagick_in_pwd.sh
+++ 002-mini-scripts/00225_print_images_dimensions_and_filesizes_jpg_png_using_imagemagick_in_pwd.sh
@@ -7,12 +7,14 @@ tmpfile0="$WORKDIR/_tmp00_images_dimensions_and_filesizes.txt" ;
 tmpfile1="$WORKDIR/_tmp01_images_dimensions.txt" ; 
 tmpfile2="$WORKDIR/_tmp02_images_filesizes.txt" ; 
 
+echo ">> CURRENT DIR = $(pwd)" ; 
+
 ## initializing tmpfiles
 echo | tee $tmpfile0 $tmpfile1 $tmpfile2 ;
 echo "## FILE SIZE => SORTED FROM SMALLER TO LARGER" | tee -a $tmpfile0 $tmpfile1 $tmpfile2 ; 
 echo "##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" | tee -a $tmpfile0 $tmpfile1 $tmpfile2 ; 
 
-for x in $(fd -e jpg -e png -e jpeg -e gif -e JPG -e PNG -e GIF -e webp -e WEBP--search-path="$(pwd)") ; do 
+for x in $(fd -HIt f -e jpg -e png -e jpeg -e gif -e JPG -e PNG -e GIF -e webp -e WEBP --search-path="$(pwd)") ; do 
     dimensions=$(identify -format "%wx%h\n" "$x")  ;  
     filesize=$(du -skh "$x")  ;  
     echo "$dimensions $filesize" >> $tmpfile0 ; 
