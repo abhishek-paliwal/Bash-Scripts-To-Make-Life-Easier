e8dfaa66cca8263d4d5b174d1e5bd48f1d14eeff
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2023-10-11 20:09:59 +0300

    Update 00209-upload_all_images_to_mggk_webserver.sh

--- 002-mini-scripts/00209-upload_all_images_to_mggk_webserver.sh
+++ 002-mini-scripts/00209-upload_all_images_to_mggk_webserver.sh
@@ -9,14 +9,13 @@ echo ">> Present working directory: $WORKDIR" ;
 echo "##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" ;
 ##
 echo ">>>> BEGIN: RSYNC ... <<<<" ; 
-figlet -t -f cybermedium "Enter password" | lolcat ;
-#rsync -azq --delete $REPO_MGGK/static/wp-content/ $DREAMHOST_USER_MGGK@$DREAMHOST_SERVER:/home/$DREAMHOST_USER_MGGK/mygingergarlickitchen.com/wp-content/ ; 
+figlet -f cybermedium "Enter password" | lolcat ;
 rsync -azq $REPO_MGGK/static/wp-content/ $DREAMHOST_USER_MGGK@$DREAMHOST_SERVER:/home/$DREAMHOST_USER_MGGK/mygingergarlickitchen.com/wp-content/ ; 
 echo ; 
 ####
-figlet -t -f cybermedium "Enter password again" | lolcat ;
-#rsync -azq --delete $REPO_CDN/$CDN_MGGK_IMAGES/ $DREAMHOST_USER_MGGK@$DREAMHOST_SERVER:/home/$DREAMHOST_USER_MGGK/$CDN_MGGK_IMAGES/ ; 
-rsync -azq $REPO_CDN/$CDN_MGGK_IMAGES/ $DREAMHOST_USER_MGGK@$DREAMHOST_SERVER:/home/$DREAMHOST_USER_MGGK/$CDN_MGGK_IMAGES/ ; 
-rsync -azq $REPO_CDN/$CDN_MGGK_IMAGES_WEBP/ $DREAMHOST_USER_MGGK@$DREAMHOST_SERVER:/home/$DREAMHOST_USER_MGGK/$CDN_MGGK_IMAGES_WEBP/ ; 
+figlet -f cybermedium "Enter password again" | lolcat ;
+CDN_DIR_MGGK="cdn.mygingergarlickitchen.com" ; 
+#rsync -azq --delete $REPO_CDN/$CDN_DIR/ $DREAMHOST_USER_MGGK@$DREAMHOST_SERVER:/home/$DREAMHOST_USER_MGGK/$CDN_DIR/ ; 
+rsync -azq $REPO_CDN/$CDN_DIR_MGGK/ $DREAMHOST_USER_MGGK@$DREAMHOST_SERVER:/home/$DREAMHOST_USER_MGGK/$CDN_DIR_MGGK/ ; 
 echo ">>>> END: RSYNC ... <<<<" ; 
-figlet -t "Image upload done." ; 
+figlet "Image upload done." ; 
