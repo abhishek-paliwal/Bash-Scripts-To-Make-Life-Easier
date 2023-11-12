d27c09794759fc50690493872b04366a290163db
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2023-11-12 22:33:40 +0200

    Update 00228_ffmpeg_print_mp3_files_duration_in_hour_mins_secs_as_CSV.sh

--- 002-mini-scripts/00228_ffmpeg_print_mp3_files_duration_in_hour_mins_secs_as_CSV.sh
+++ 002-mini-scripts/00228_ffmpeg_print_mp3_files_duration_in_hour_mins_secs_as_CSV.sh
@@ -11,7 +11,7 @@ THIS_SCRIPT_NAME_SANS_EXTENSION="$(echo $THIS_SCRIPT_NAME | sed 's/\.sh//g')" ;
 ## SETTING VARIABLES
 WORKDIR="$DIR_Y/_OUTPUT_$THIS_SCRIPT_NAME_SANS_EXTENSION" ;
 CURRENTDIR="$(pwd)" ; 
-TMPCSV="$WORKDIR/mp3_durations.csv" ; 
+TMPCSV="$WORKDIR/mp3_durations_in_$(basename $CURRENTDIR).csv" ; 
 echo "DURATION,MP3_FILEPATH" > $TMPCSV ; ## initializing columns in csv file
 mkdir -p $WORKDIR ; ## create dir if not exists
 echo "##########################################" ; 
@@ -23,6 +23,7 @@ echo "##########################################" ;
 function FUNC_PRINT_MP3_DURATION_IN_PWD () {
     # Replace "yourfile.mp3" with the actual filename
     file="$1" ## mp3 filepath
+    echo ">> CURRENT MP3 FILE = $file" ; 
     # Use ffprobe to get the duration
     duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$file")
     # Convert seconds to hours, minutes, and seconds using bc
@@ -31,6 +32,7 @@ function FUNC_PRINT_MP3_DURATION_IN_PWD () {
     seconds_tmp=$(echo "$duration % 60" | bc) ; 
     seconds=$(echo "$seconds_tmp / 1" | bc) ;  ## rounding 
     # Print duration
+    echo "${hours}h_${minutes}m_${seconds}s" ; 
     echo "${hours}h_${minutes}m_${seconds}s,$file" >>  $TMPCSV ; 
 }
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -39,8 +41,13 @@ for mp3file in $(fd -e mp3 --search-path="$(pwd)") ; do
     FUNC_PRINT_MP3_DURATION_IN_PWD "$mp3file"  
 done 
 ##
+echo "################################################################################" ;
 echo ; echo ; 
 echo ">> CSV file generated. Now printing it ( $TMPCSV )" ; 
 echo ; echo ; 
 cat $TMPCSV ; 
-##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo ; echo ; 
+echo "##------------------------------------------------------------------------------" ; 
+echo ">> CHECK FINAL CSV FILE = $TMPCSV " ; 
+echo "##------------------------------------------------------------------------------" ; 
+
