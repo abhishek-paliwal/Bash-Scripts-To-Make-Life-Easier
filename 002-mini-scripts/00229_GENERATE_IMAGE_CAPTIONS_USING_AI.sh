c6605710a359868f14cff3abdafc98eb0aa4170b
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2023-12-21 00:06:08 +0200

    Updated on AP-MBP.local - Thu Dec 21 00:06:08 EET 2023

--- 002-mini-scripts/00229_GENERATE_IMAGE_CAPTIONS_USING_AI.sh
+++ 002-mini-scripts/00229_GENERATE_IMAGE_CAPTIONS_USING_AI.sh
@@ -17,29 +17,34 @@ echo "1. FASTEST   = h003_create_image_captions_from_inference_api_from_someones
 echo "2. FAST      = h002_create_image_captions_from_inference_api"
 echo "3. VERYSLOW  = h001_run_this_to_create_image_captions_in_pwd_custom_dir"
 echo "4. FAST      = googlecloud_create_image_captions_from_vertexai_vision"
-echo "5. Quit"
+echo "5. FAST      = googlecloud_use_gemini_pro_models_with_text_and_images_captions"
+echo "99. Quit"
 echo "+++++++++++++++++++++++++++++++++++++++" ; 
 
 read -p "Enter your choice [Press ENTER to use default choice - 1]: " choice
 
 case $choice in
     1)
-        echo "You chose Option One."
+        echo "You chose Option 1."
         eval h003_create_image_captions_from_inference_api_from_someones_space  ;
         ;;
     2)
-        echo "You chose Option Two."
+        echo "You chose Option 2."
         eval h002_create_image_captions_from_inference_api  ;
         ;;
     3)
-        echo "You chose Option Three."
+        echo "You chose Option 3."
         eval h001_run_this_to_create_image_captions_in_pwd_custom_dir  ;
         ;;
     4)
-        echo "You chose Option Four."
+        echo "You chose Option 4."
         eval googlecloud_create_image_captions_from_vertexai_vision ;
         ;;
     5)
+        echo "You chose Option 5."
+        eval googlecloud_use_gemini_pro_models_with_text_and_images_captions ;
+        ;;
+    99)
         echo "Exiting the program."
         exit 0
         ;;
