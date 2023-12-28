abc5f503dbbdedd68c194b614964583b4eefd95f
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2023-12-28 21:30:06 +0200

    updated

--- 002-mini-scripts/00229_GENERATE_IMAGE_CAPTIONS_USING_AI.sh
+++ 002-mini-scripts/00229_GENERATE_IMAGE_CAPTIONS_USING_AI.sh
@@ -13,11 +13,11 @@ shopt -s expand_aliases ## for BASH: This has to be done, else, aliases are not 
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 echo "+++++++++++++++++++++++++++++++++++++++" ; 
 echo ">> Choose an option:" ;
-echo "1. FASTEST   = h003_create_image_captions_from_inference_api_from_someones_space"
-echo "2. FAST      = h002_create_image_captions_from_inference_api"
-echo "3. VERYSLOW  = h001_run_this_to_create_image_captions_in_pwd_custom_dir"
-echo "4. FAST      = googlecloud_create_image_captions_from_vertexai_vision"
-echo "5. FAST      = googlecloud_use_gemini_pro_models_with_text_and_images_captions"
+echo "1. FAST              = h002_create_image_captions_from_inference_api"
+echo "2. FAST (FREE)       = googlecloud_use_gemini_pro_models_with_text_and_images_captions"
+echo "3. FAST (PAID)       = googlecloud_create_image_captions_from_vertexai_vision"
+#echo "4. FAST              = h003_create_image_captions_from_inference_api_from_someones_space"
+#echo "5. VERYSLOW (local)  = h001_run_this_to_create_image_captions_in_pwd_custom_dir"
 echo "99. Quit"
 echo "+++++++++++++++++++++++++++++++++++++++" ; 
 
@@ -25,33 +25,28 @@ read -p "Enter your choice [Press ENTER to use default choice - 1]: " choice
 
 case $choice in
     1)
-        echo "You chose Option 1."
-        eval h003_create_image_captions_from_inference_api_from_someones_space  ;
+        eval h002_create_image_captions_from_inference_api 
         ;;
     2)
-        echo "You chose Option 2."
-        eval h002_create_image_captions_from_inference_api  ;
+        figlet 'USA VPN on ?' 
+        eval googlecloud_use_gemini_pro_models_with_text_and_images_captions
         ;;
     3)
-        echo "You chose Option 3."
-        eval h001_run_this_to_create_image_captions_in_pwd_custom_dir  ;
+        eval googlecloud_create_image_captions_from_vertexai_vision
         ;;
     4)
-        echo "You chose Option 4."
-        eval googlecloud_create_image_captions_from_vertexai_vision ;
+        #eval h003_create_image_captions_from_inference_api_from_someones_space 
         ;;
     5)
-        echo "You chose Option 5." ; 
-        figlet 'USA VPN on ?' ; 
-        eval googlecloud_use_gemini_pro_models_with_text_and_images_captions ;
+        #eval h001_run_this_to_create_image_captions_in_pwd_custom_dir 
         ;;
     99)
         echo "Exiting the program."
         exit 0
         ;;
     *)
-        echo "Invalid choice. Please enter a number between 1 and 5. Default command will run..." ;
-        eval h003_create_image_captions_from_inference_api_from_someones_space ;
+        echo "No option selected. Default command [option 1] will run..." ;
+        eval h002_create_image_captions_from_inference_api ;
         ;;
 esac
 ##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
