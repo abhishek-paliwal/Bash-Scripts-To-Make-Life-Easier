99114e95b3aaa1496aa23347ca55e09420686e15
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2024-02-20 17:07:49 +0200

    Update 00237_mggk_format_and_replace_tags_categories_to_lowercase_in_hyphens_in_original_mdfiles.sh

--- 002-mini-scripts/00237_mggk_format_and_replace_tags_categories_to_lowercase_in_hyphens_in_original_mdfiles.sh
+++ 002-mini-scripts/00237_mggk_format_and_replace_tags_categories_to_lowercase_in_hyphens_in_original_mdfiles.sh
@@ -20,26 +20,33 @@ function FUNC_PROCESS_EACH_MDFILE_FOR_EACH_CATEGORY_OR_TAG () {
     echo ">> RUNNING FUNCTION : $FUNCNAME" ;
     ##
     file2use="$1" ; 
+    TOTAL_CATEGORIES_OR_TAGS=$(cat $file2use | wc -l ) ; 
     #IFS="\n" # Set IFS to comma
+    COUNT1=0;
     while read myCategoryOrTag
     do
-        #lowercase=${myCategoryOrTag,,} # Convert to lowercase
-        #newCategoryOrTag=${lowercase// /-} # Replace spaces with hyphens
+        ((COUNT1++)) ;
         lowercase=$(echo "$myCategoryOrTag" | tr '[:upper:]' '[:lower:]') # Convert to lowercase
         newCategoryOrTag=$(echo "$lowercase" | tr ' ' '-') # Replace spaces with hyphens
         ##
         # Find mdfiles and process each of the found files 
         tmpfile="$DIR_Y/mdfilelist.txt" ; 
         grep -irEl "^.*- $myCategoryOrTag" "$REPO_MGGK/content/allrecipes" > $tmpfile ; 
+        TOTAL_FILES_FOUND=$(cat $tmpfile | wc -l ) ; 
+        ##
         while IFS="\n" read mdfilepath ; do 
             ## replace the formatted text in original (with various space levels)
             sed -i '' "s/^ - *$myCategoryOrTag *$/ - $newCategoryOrTag/" "$mdfilepath" ;
             sed -i '' "s/^  - *$myCategoryOrTag *$/  - $newCategoryOrTag/" "$mdfilepath" ;
         done < $tmpfile
+        ##
+        echo ">> CURRENT CATEGORY/TAG = $COUNT1 of $TOTAL_CATEGORIES_OR_TAGS (= $myCategoryOrTag) // TOTAL MDFILES FOUND FOR THIS CATEGORY/TAG = $TOTAL_FILES_FOUND" ; 
     done < "$file2use"
 }
 ##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-## CALL FUNCTION
+## CALL FUNCTIONS FOR CATEGORY AND TAGS
+echo ">> IMPORTANT NOTE: This program takes about 9 minutes to run completely." ; 
+##
 FUNC_PROCESS_EACH_MDFILE_FOR_EACH_CATEGORY_OR_TAG "$file_category" ; 
 FUNC_PROCESS_EACH_MDFILE_FOR_EACH_CATEGORY_OR_TAG "$file_tags" ; 
