49712e292dc1e64ebba11a0653e124ab3cb044cc
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2024-09-03 21:31:28 +0300

    Update 9993-01-digitalocean-parse-aurora-image.sh

--- 9999-digitalocean-scripts/9993-digitalocean-aurora/9993-01-digitalocean-parse-aurora-image.sh
+++ 9999-digitalocean-scripts/9993-digitalocean-aurora/9993-01-digitalocean-parse-aurora-image.sh
@@ -10,10 +10,12 @@
 ## BY: PALI 
 ################################################################################
 
+OUTDIR="/home/ubuntu/Desktop/Y" ; 
+cd $OUTDIR ; 
+
 URL_AURORA_IMAGE="https://cdn.fmi.fi/weather-observations/products/magnetic-disturbance-observations/map-latest-en.png" ; 
 PROG_ROOTDIR="/home/GitHub/Bash-Scripts-To-Make-Life-Easier/9999-digitalocean-scripts/9993-digitalocean-aurora" ; 
 PYTHON3_VENV_PATH="/home/GitHub/My-Pro-Python-Programs/my_python_virtual_environments/venv3/bin/python3" ; ## on digitalocean server
-OUTDIR="/home/ubuntu/Desktop/Y" ; 
 IMAGE_INPUT="$OUTDIR/9993-digitalocean-map-latest-en.png" ; 
 IMAGE_CROPPED="$OUTDIR/9993-digitalocean-cropped_image_result.png" ; 
 EMAIL_FROM="info@mygingergarlickitchen.com" ; 
