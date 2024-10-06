d97afb208711bba1b045743a4e050c3b1c277a9a
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2024-10-06 13:43:34 +0300

    Updated on AP-MBP.local - Sun Oct  6 13:43:34 EEST 2024

--- 9999-digitalocean-scripts/9993-digitalocean-crop-and-ocr-aurora-image.sh
+++ 9999-digitalocean-scripts/9993-digitalocean-crop-and-ocr-aurora-image.sh
@@ -40,19 +40,31 @@ wget -O "$IMAGE_INPUT" "$URL_AURORA_IMAGE" ;
 /home/linuxbrew/.linuxbrew/bin/tesseract "$IMAGE_CROPPED" "$TMPFILE_BASE" -l fin 
 
 ## create final message
-myTextMessage=$(cat $TMPFILE | tr '\n' ' ') ; 
+myTextMessage=$(cat $TMPFILE | tr '\n' ' ') ;
+
+# Extract just the number from message using regular expression
+myNumber=$(echo "$myTextMessage" | grep -o -E '[0-9]+')
 
 ## sending email (use aws full path)
 #/home/linuxbrew/.linuxbrew/bin/aws ses send-email --from "$EMAIL_FROM" --to "$EMAIL_TO" --subject "$myTextMessage (AURORA) $(date)" --text "Aurora numbers / $myTextMessage" ;
 
-## sending message to telegram bot
-## get variable values from environment variables
-echo ">> Sending message to telegram bot ..." ; 
-echo;
-/home/linuxbrew/.linuxbrew/bin/curl -X POST -H "Content-Type:multipart/form-data" -F "chat_id=${TELEGRAM_CHATID}" -F text="${myTextMessage} -$(date +%H:%M/%b%d)" "https://api.telegram.org/bot${TELEGRAM_BOTTOKEN}/sendMessage"
-## also send an image to telegram bot
-echo "===================="; 
-/home/linuxbrew/.linuxbrew/bin/curl -X POST -H "Content-Type:multipart/form-data" -F chat_id=${TELEGRAM_CHATID} -F photo=@"${IMAGE_INPUT}" "https://api.telegram.org/bot${TELEGRAM_BOTTOKEN}/sendPhoto" 
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## ONLY SEND THE MESSAGE WHEN THE NUMBER IS MORE THAN 99
+# Check if the number is greater than 99
+if [[ $myNumber -gt 99 ]]; then
+    echo "The Aurora number $myNumber is greater than 99."
+    ## sending message to telegram bot
+    ## get variable values from environment variables
+    echo ">> Sending message to telegram bot ..." ; 
+    echo;
+    /home/linuxbrew/.linuxbrew/bin/curl -X POST -H "Content-Type:multipart/form-data" -F "chat_id=${TELEGRAM_CHATID}" -F text="${myTextMessage} -$(date +%H:%M/%b%d)" "https://api.telegram.org/bot${TELEGRAM_BOTTOKEN}/sendMessage" ;
+    ## also send an image to telegram bot
+    echo "===================="; 
+    /home/linuxbrew/.linuxbrew/bin/curl -X POST -H "Content-Type:multipart/form-data" -F chat_id=${TELEGRAM_CHATID} -F photo=@"${IMAGE_INPUT}" "https://api.telegram.org/bot${TELEGRAM_BOTTOKEN}/sendPhoto" ;
+    echo "===================="; 
+else
+    echo "The Aurora number $myNumber is less than or equal to 99."
+fi
 
-echo "===================="; 
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
